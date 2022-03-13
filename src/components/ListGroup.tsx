762d453dc295cce85713e8af0018a19e2fa4e60b
Nesrine Sghaier <nesrinesghaier10@gmail.com>
2022-03-13 12:00:04 +0100

    chore: sort class names

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -9,18 +9,20 @@ export type ListGroupItem = {
   link?: string;
   icon?: FC<ComponentProps<'svg'>>;
   active?: boolean;
+  onClick?: () => void;
   disabled?: boolean;
 };
 
 export const ListGroup: FC<ListGroupProps> = ({ items }) => {
   return (
-    <div className="list-none w-48 text-sm font-medium text-gray-900 bg-white rounded-lg divide-y divide-gray-200 dark:divide-gray-600 border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+    <div className="w-48 list-none divide-y divide-gray-200 rounded-lg border border-gray-200 bg-white text-sm font-medium text-gray-900 dark:divide-gray-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white">
       {items.map((item, index) => (
         <Fragment key={index}>
           {!item.link ? (
             <button
+              onClick={item.onClick}
               className={classNames(
-                'flex gap-2 items-center text-sm font-medium py-2 px-4 w-full text-left first:rounded-t-lg last:rounded-b-lg',
+                'flex w-full items-center gap-2 py-2 px-4 text-left text-sm font-medium first:rounded-t-lg last:rounded-b-lg',
                 {},
               )}
               type="button"
@@ -33,9 +35,9 @@ export const ListGroup: FC<ListGroupProps> = ({ items }) => {
               href={item.link}
               aria-current="false"
               className={classNames(
-                'block py-2 px-4 w-full cursor-pointer focus:outline-none dark:bg-gray-800 first:rounded-t-lg last:rounded-b-lg',
+                'block w-full cursor-pointer py-2 px-4 first:rounded-t-lg last:rounded-b-lg focus:outline-none dark:bg-gray-800',
                 {
-                  'text-white bg-blue-700': item.active,
+                  'bg-blue-700 text-white': item.active,
                   'hover:bg-gray-100 hover:text-blue-700 dark:hover:bg-gray-600 dark:hover:text-white': !item.active,
                 },
               )}
