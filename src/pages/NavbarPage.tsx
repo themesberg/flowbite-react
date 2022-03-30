670da6bc9fb644b6026c0f5cdc18a66449090db6
Joesph Chrysanthopoulos <joeyc@Joesphs-Mac-mini.local>
2022-03-29 23:21:33 -0500

    feat: Create Navbar example page

---
+++ src/pages/NavbarPage.tsx
@@ -0,0 +1,131 @@
+import { FC } from 'react';
+import { CodeExample, DemoPage } from './DemoPage';
+import { Navbar } from '../components';
+import { Button } from '../components';
+
+const NavbarPage: FC = () => {
+  const examples: CodeExample[] = [
+    {
+      title: 'Default Navbar',
+      code: (
+        <Navbar>
+          <Navbar.Logo href="https://www.joeysapps.com/">
+            <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+          </Navbar.Logo>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <ul className="mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium">
+              <li>
+                <a
+                  href="#/navbars"
+                  className="block rounded bg-blue-700 py-2 pr-4 pl-3 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#/navbars"
+                  className="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#/navbars"
+                  className="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                >
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#/navbars"
+                  className="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                >
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#/navbars"
+                  className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </Navbar.Collapse>
+        </Navbar>
+      ),
+      codeClassName: 'dark:!bg-gray-900',
+    },
+    {
+      title: 'Navbar with CTA button',
+      code: (
+        <Navbar>
+          <Navbar.Logo>
+            <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+          </Navbar.Logo>
+          <div className="flex md:order-2">
+            <Button>Get started</Button>
+            <Navbar.Toggle />
+          </div>
+          <Navbar.Collapse>
+            <ul className="mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium">
+              <li>
+                <a
+                  href="#/navbars"
+                  className="block rounded bg-blue-700 py-2 pr-4 pl-3 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#/navbars"
+                  className="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#/navbars"
+                  className="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                >
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#/navbars"
+                  className="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                >
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#/navbars"
+                  className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </Navbar.Collapse>
+        </Navbar>
+      ),
+      codeClassName: 'dark:!bg-gray-900',
+    },
+  ];
+  return <DemoPage examples={examples} />;
+};
+
+export default NavbarPage;
