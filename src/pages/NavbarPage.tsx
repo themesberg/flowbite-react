02f930b50599bf6b36611464ef4de2bd8b809854
Joesph Chrysanthopoulos <joeyc@Joesphs-Mac-mini.local>
2022-03-30 20:52:59 -0500

    added new props to Navbar examples page

--- src/pages/NavbarPage.tsx
+++ src/pages/NavbarPage.tsx
@@ -7,7 +7,7 @@ const NavbarPage: FC = () => {
     {
       title: 'Default Navbar',
       code: (
-        <Navbar>
+        <Navbar fluid menuOpen rounded>
           <Navbar.Brand href="https://flowbite.com/">
             <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
             <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
@@ -65,7 +65,7 @@ const NavbarPage: FC = () => {
     {
       title: 'Navbar with CTA button',
       code: (
-        <Navbar>
+        <Navbar fluid>
           <Navbar.Brand href="https://flowbite.com/">
             <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
             <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
