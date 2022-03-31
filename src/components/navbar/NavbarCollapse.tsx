7fe9e3e8041f30a411626b87868eea30be4f663f
Joesph Chrysanthopoulos <joeyc@Joesphs-Mac-mini.local>
2022-03-31 18:01:20 -0500

    fix: removed linter ignore for hidden className

--- src/components/navbar/NavbarCollapse.tsx
+++ src/components/navbar/NavbarCollapse.tsx
@@ -1,7 +1,8 @@
 import { FC } from 'react';
+import classNames from 'classnames';
 import { useNavbarContext } from './NavbarContext';
 
 export const NavbarCollapse: FC = ({ children }) => {
   const { isOpen } = useNavbarContext();
-  return <div className={isOpen ? 'w-full md:block md:w-auto' : 'hidden w-full md:block md:w-auto'}>{children}</div>;
+  return <div className={classNames('w-full md:block md:w-auto', { hidden: !isOpen })}>{children}</div>;
 };
