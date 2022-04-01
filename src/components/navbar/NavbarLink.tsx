d3fcd0f5ca7e52a5ecf187120fde54175332013c
Joesph Chrysanthopoulos <joeyc@Joesphs-Mac-mini.local>
2022-03-31 19:55:50 -0500

    fix: removed typo around a className

--- src/components/navbar/NavbarLink.tsx
+++ src/components/navbar/NavbarLink.tsx
@@ -13,7 +13,7 @@ export const NavbarLink: FC<NavbarLinkProps> = ({ active, disabled, href, childr
       <a
         href={href}
         className={classNames('block py-2 pr-4 pl-3 md:p-0', {
-          'md:text-blue-700" bg-blue-700 text-white dark:text-white md:bg-transparent': active,
+          'bg-blue-700 text-white dark:text-white md:bg-transparent md:text-blue-700': active,
           'border-b border-gray-100  text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white':
             !active,
         })}
