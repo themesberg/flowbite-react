c0f3d188a40f0579cb15a5e22b351d64c806674c
Joesph Chrysanthopoulos <joeyc@Joesphs-Mac-mini.local>
2022-04-02 00:22:40 -0500

    chore: add disabled classNames to Navbar.Link

--- src/components/navbar/NavbarLink.tsx
+++ src/components/navbar/NavbarLink.tsx
@@ -15,7 +15,8 @@ export const NavbarLink: FC<NavbarLinkProps> = ({ active, disabled, href, childr
         className={classNames('block py-2 pr-4 pl-3 md:p-0', {
           'bg-blue-700 text-white dark:text-white md:bg-transparent md:text-blue-700': active,
           'border-b border-gray-100  text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white':
-            !active,
+            !active && !disabled,
+          'text-gray-400 hover:cursor-not-allowed dark:text-gray-600': disabled,
         })}
       >
         {children}
