5f82950a17be6aad011810a878f38a2a36eda3ce
Ricardo Lüders <rluders@redhat.com>
2022-04-12 15:26:42 +0200

    chore: fix storybook import

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,7 +1,7 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
 import React from 'react';
 
-import { Button, ButtonProps } from './Button';
+import { Button, ButtonProps } from '.';
 
 export default {
   title: 'Components/Buttons',
