a951c06023ccc155cedb951b487f61355856a8b0
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2022-04-13 11:28:20 +0200

    chore: add avatar user dropdown example

--- src/pages/AvatarPage.tsx
+++ src/pages/AvatarPage.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 
-import { Avatar } from '../components';
+import { Avatar, Dropdown } from '../components';
 import { CodeExample, DemoPage } from './DemoPage';
 
 const AvatarPage: FC = () => {
@@ -61,6 +61,26 @@ const AvatarPage: FC = () => {
             <div className="text-sm text-gray-500 dark:text-gray-400">Joined in August 2014</div>
           </div>
         </Avatar>
+      ),
+    },
+    {
+      title: 'User dropdown',
+      code: (
+        <Dropdown
+          label={<Avatar img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />}
+          arrowIcon={false}
+          inline
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
+          </Dropdown.Header>
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item>Earnings</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
       ),
     },
     {
