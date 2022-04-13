74b139df63671c3fa5156c61aec2b7332bbb93c8
Dinesh K <medineshkatwal@gmail.com>
2022-04-13 17:12:51 +0545

    Added more colors, size, label for progressbar

--- src/components/Progressbar.tsx
+++ src/components/Progressbar.tsx
@@ -1,14 +1,77 @@
+import classNames from 'classnames';
 import { FC, PropsWithChildren } from 'react';
 
+type Color = 'gray' | 'blue' | 'red' | 'green' | 'yellow' | 'indigo' | 'purple';
+type Size = 'sm' | 'md' | 'lg' | 'xl';
+type TextSize = 'sm' | 'md' | 'lg' | 'xl';
+type TextColor = 'gray' | 'blue' | 'red' | 'green' | 'yellow' | 'indigo' | 'purple';
+
 export type ProgressbarProps = PropsWithChildren<{
-  progress?: string | '10%' | '20%' | '30%' | '40%' | '50%' | '60%' | '70%' | '80%' | '90%' | '100%';
+  className?: string;
+  progress?: number;
+  color?: Color;
+  size?: Size;
+  label?: string;
+  labelPosition?: 'inside' | 'outside';
+  labelProgress?: boolean;
+  labelSize?: TextSize;
+  labelColor?: TextColor;
 }>;
 
-export const Progressbar: FC<ProgressbarProps> = ({ progress }) => {
+const colorClasses: Record<Color, string> = {
+  blue: 'bg-blue-600',
+  red: 'bg-red-600',
+  purple: 'bg-purple-600',
+  gray: 'bg-gray-600',
+  green: 'bg-green-600',
+  yellow: 'bg-yellow-400',
+  indigo: 'bg-indigo-600',
+};
+const sizeClasses: Record<Size, string> = {
+  sm: 'h-1.5',
+  md: 'h-2.5',
+  lg: 'h-4',
+  xl: 'h-6',
+};
+const labelSizeClasses: Record<Size, string> = {
+  sm: 'text-xs font-medium text-blue-100 text-center p-0.5 leading-none',
+  md: 'text-xs font-medium text-blue-100 text-center p-0.5 leading-none',
+  lg: 'text-xs font-medium text-blue-100 text-center p-0.5 leading-none',
+  xl: 'text-xs font-medium text-blue-100 text-center p-0.5 leading-none',
+};
+
+export const Progressbar: FC<ProgressbarProps> = ({
+  className,
+  progress = 45,
+  color = 'blue',
+  size = 'md',
+  label,
+  labelPosition = 'inside',
+  labelProgress = false,
+  labelSize = 'md',
+  labelColor = 'blue',
+}) => {
   return (
     <>
-      <div className="h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700">
-        <div className="h-2.5 rounded-full bg-blue-600" style={{ width: progress }}></div>
+      {label && labelPosition === 'outside' && (
+        <div className="mb-1 flex justify-between">
+          <span className="text-base font-medium text-blue-700 dark:text-white">{label}</span>
+          {labelProgress && <span className="text-sm font-medium text-blue-700 dark:text-white">{progress}%</span>}
+        </div>
+      )}
+      <div className={classNames('w-full rounded-full bg-gray-200 dark:bg-gray-700 ', sizeClasses[size])}>
+        <div
+          className={classNames(
+            'rounded-full',
+            colorClasses[color],
+            sizeClasses[size],
+            labelSizeClasses[labelSize],
+            className,
+          )}
+          style={{ width: progress + '%' }}
+        >
+          {label && labelPosition == 'inside' && label}
+        </div>
       </div>
     </>
   );
--- src/pages/ProgressbarPage.tsx
+++ src/pages/ProgressbarPage.tsx
@@ -7,7 +7,67 @@ const ProgressbarPage: FC = () => {
   const examples: CodeExample[] = [
     {
       title: 'Default Progressbar',
-      code: <Progressbar progress="10%" />,
+      code: <Progressbar />,
+    },
+    {
+      title: 'Sizing',
+      code: (
+        <>
+          <div className="mb-1 text-base font-medium dark:text-white">Small</div>
+          <Progressbar size="sm" color="gray" />
+          <div className="mb-1 text-base font-medium dark:text-white">Default</div>
+          <Progressbar size="md" color="gray" />
+          <div className="mb-1 text-lg font-medium dark:text-white">Large</div>
+
+          <Progressbar size="lg" color="gray" />
+          <div className="mb-1 text-lg font-medium dark:text-white">Extra Large</div>
+
+          <Progressbar size="xl" color="gray" />
+        </>
+      ),
+    },
+    {
+      title: 'Colors',
+      code: (
+        <>
+          <div className="mb-1 text-base font-medium">Dark</div>
+          <Progressbar color="gray" />
+
+          <div className="mb-1 text-base font-medium text-blue-700">Blue</div>
+          <Progressbar color="blue" />
+
+          <div className="mb-1 text-base font-medium text-red-700">Red</div>
+          <Progressbar color="red" />
+
+          <div className="mb-1 text-base font-medium text-green-700">Green</div>
+          <Progressbar color="green" />
+
+          <div className="mb-1 text-base font-medium text-yellow-700">Yellow</div>
+          <Progressbar color="yellow" />
+
+          <div className="mb-1 text-base font-medium text-indigo-700">Indigo</div>
+          <Progressbar color="indigo" />
+
+          <div className="mb-1 text-base font-medium text-purple-700">Purple</div>
+          <Progressbar color="purple" />
+        </>
+      ),
+    },
+    {
+      title: 'With label inside',
+      code: (
+        <>
+          <Progressbar progress={45} label="flowbite" />
+        </>
+      ),
+    },
+    {
+      title: 'With label outside',
+      code: (
+        <>
+          <Progressbar label="Flowbite" labelPosition="outside" labelProgress={true} />
+        </>
+      ),
     },
   ];
 
