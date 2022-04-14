09cfef49d6a52a01cca814e22ca35eef4ba65992
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2022-04-14 13:40:47 +0200

    fix: add missing dark class names of the progress component

--- src/components/Progress/index.tsx
+++ src/components/Progress/index.tsx
@@ -15,13 +15,13 @@ export type ProgressProps = PropsWithChildren<{
 }>;
 
 const colorClasses: Record<Color, string> = {
+  dark: 'bg-gray-600 dark:bg-gray-300',
   blue: 'bg-blue-600',
-  red: 'bg-red-600',
-  purple: 'bg-purple-600',
-  dark: 'bg-gray-600',
-  green: 'bg-green-600',
+  red: 'bg-red-600 dark:bg-red-500',
+  green: 'bg-green-600 dark:bg-green-500',
   yellow: 'bg-yellow-400',
-  indigo: 'bg-indigo-600',
+  indigo: 'bg-indigo-600 dark:bg-indigo-500',
+  purple: 'bg-purple-600 dark:bg-purple-500',
 };
 const sizeClasses: Record<Size, string> = {
   sm: 'h-1.5',
