c525f6210431fdf7443d1a97ccd53ead29942003
mouracamila <camilapaleo@gmail.com>
2022-04-14 16:18:15 +0200

    chore: fix color

--- src/components/AvatarGroup/AvatarGroupCounter.tsx
+++ src/components/AvatarGroup/AvatarGroupCounter.tsx
@@ -8,7 +8,7 @@ export type AvatarGroupdCounterProps = PropsWithChildren<{
 export const AvatarGroupCounter: React.FC<AvatarGroupdCounterProps> = ({ total, href }) => {
   return (
     <a
-      className="relative flex h-10 w-10 items-center justify-center rounded-full bg-gray-700 text-xs font-medium text-white ring-2 ring-gray-300 hover:bg-gray-600 dark:border-gray-800 "
+      className="relative flex h-10 w-10 items-center justify-center rounded-full bg-gray-700 text-xs font-medium text-white ring-2 ring-gray-300 hover:bg-gray-600  dark:ring-gray-500 "
       href={href}
     >
       +{total}
