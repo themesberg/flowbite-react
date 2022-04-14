e84f86bbbda906b0b1078f6cc3d0cff341b6b078
Joey Chrysanthopoulos <joeyachrys@outlook.com>
2022-04-14 06:32:36 -0500

    chore: added testid to toast toggle

--- src/components/Toast/ToastToggle.tsx
+++ src/components/Toast/ToastToggle.tsx
@@ -16,6 +16,7 @@ export const ToastToggle: FC<ToastToggleProps> = ({ xIcon: XIcon = HiX }) => {
 
   return (
     <button
+      data-testid="toast-toggle-element"
       type="button"
       className="-mx-1.5 -my-1.5 ml-auto inline-flex h-8 w-8 rounded-lg bg-white p-1.5 text-gray-400 hover:bg-gray-100 hover:text-gray-900 focus:ring-2 focus:ring-gray-300 dark:bg-gray-800 dark:text-gray-500 dark:hover:bg-gray-700 dark:hover:text-white"
       onClick={handleClick}
