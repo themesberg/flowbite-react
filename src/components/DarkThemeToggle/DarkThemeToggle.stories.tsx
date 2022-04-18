983d25bc2c83b4f3b4d95766dbe75b31e8570e22
mouracamila <camilapaleo@gmail.com>
2022-04-18 16:47:29 +0200

    chore: dark theme toggle stories

---
+++ src/components/DarkThemeToggle/DarkThemeToggle.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+
+import { DarkThemeToggle } from '.';
+
+export default {
+  title: 'Components/DarkThemeToggle',
+  component: DarkThemeToggle,
+} as Meta;
+
+const Template: Story = (args) => <DarkThemeToggle {...args} />;
+
+export const DefaultDarkThemeToggle = Template.bind({});
+DefaultDarkThemeToggle.storyName = 'Default';
+DefaultDarkThemeToggle.args = {};
