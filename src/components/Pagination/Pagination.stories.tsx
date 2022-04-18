2829d17d62a7c43158e62574d28c558bc2f294e2
Dinesh K <medineshkatwal@gmail.com>
2022-04-18 11:28:41 +0545

    Fixed storybook for pagination component

--- src/components/Pagination/Pagination.stories.tsx
+++ src/components/Pagination/Pagination.stories.tsx
@@ -3,7 +3,7 @@ import { Meta, Story } from '@storybook/react/types-6-0';
 import { Pagination, PaginationProps } from '.';
 
 export default {
-  title: 'Components/Pageination',
+  title: 'Components/Pagination',
   component: Pagination,
 } as Meta;
 
