aef555c5d95129552e569a586857e96dd186f258
Conner Davis <conner@tulup.io>
2022-04-25 03:06:51 -0700

    feat(component): Add `imgAlt` attribute to `Card`s
    Allows `Card`s to pass `imgAlt` to provide `alt` text
    to the underlying `<img/>`, when an `imgSrc` has also
    been provided.
    
    Now, by default, `Card`s with an image will have
    `alt=""`, which browsers interpret appropriately as
    [decorative images](https://www.w3.org/WAI/tutorials/images/decorative/).
    
    To add `alt` text to your `Card` with an image:
    
    ```js
    <Card
      imgAlt="Meaningful alt text for an image that is not purely decorative"
      imgSrc="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
    >
      ..
    </Card>
    ```

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,12 +2,13 @@ import { FC, PropsWithChildren } from 'react';
 import classNames from 'classnames';
 
 export type CardProps = PropsWithChildren<{
+  alt?: string;
   className?: string;
   horizontal?: boolean;
   imgSrc?: string;
 }>;
 
-export const Card: FC<CardProps> = ({ children, className, horizontal, imgSrc }) => {
+export const Card: FC<CardProps> = ({ alt, children, className, horizontal, imgSrc }) => {
   return (
     <div
       className={classNames(
@@ -26,7 +27,7 @@ export const Card: FC<CardProps> = ({ children, className, horizontal, imgSrc })
             'h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg': horizontal,
           })}
           src={imgSrc}
-          alt=""
+          alt={alt ?? ''}
         />
       )}
       <div className="flex h-full flex-col justify-center gap-4 p-6">{children}</div>
