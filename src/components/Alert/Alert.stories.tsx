a99abc9dd192e860949797721b9a56b7aba32ff1
mouracamila <camilapaleo@gmail.com>
2022-04-27 16:08:06 +0200

    feat: adds more alert stories

--- src/components/Alert/Alert.stories.tsx
+++ src/components/Alert/Alert.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
 
 import { Alert, AlertProps } from '.';
+import { HiInformationCircle, HiEye } from 'react-icons/hi';
 
 export default {
   title: 'Components/Alert',
@@ -16,6 +17,83 @@ DefaultAlert.args = {
     <>
       More info about this info alert goes here. This example text is going to run a bit longer so that you can see how
       spacing within an alert works with this kind of content.
+    </>
+  ),
+};
+
+export const AlertWithIcons = Template.bind({});
+AlertWithIcons.storyName = 'Alert with icons';
+AlertWithIcons.args = {
+  Icon: HiInformationCircle,
+  children: (
+    <>
+      More info about this info alert goes here. This example text is going to run a bit longer so that you can see how
+      spacing within an alert works with this kind of content.
+    </>
+  ),
+};
+
+export const AlertDismissible = Template.bind({});
+AlertDismissible.storyName = 'Dismissible alert';
+AlertDismissible.args = {
+  children: (
+    <>
+      More info about this info alert goes here. This example text is going to run a bit longer so that you can see how
+      spacing within an alert works with this kind of content.
+    </>
+  ),
+};
+
+export const AlertRounded = Template.bind({});
+AlertRounded.storyName = 'Do not rounded alert';
+AlertRounded.args = {
+  rounded: false,
+  children: (
+    <>
+      More info about this info alert goes here. This example text is going to run a bit longer so that you can see how
+      spacing within an alert works with this kind of content.
+    </>
+  ),
+};
+
+export const AlertWithBorderAccent = Template.bind({});
+AlertWithBorderAccent.storyName = 'Border accent';
+AlertWithBorderAccent.args = {
+  withBorderAccent: true,
+  children: (
+    <>
+      More info about this info alert goes here. This example text is going to run a bit longer so that you can see how
+      spacing within an alert works with this kind of content.
+    </>
+  ),
+};
+
+export const AlertWithAdditionalContent = Template.bind({});
+AlertWithAdditionalContent.storyName = 'Border accent';
+AlertWithAdditionalContent.args = {
+  withBorderAccent: true,
+
+  children: (
+    <>
+      <div className="mt-2 mb-4 text-sm text-blue-700 dark:text-blue-800">
+        More info about this info alert goes here. This example text is going to run a bit longer so that you can see
+        how spacing within an alert works with this kind of content.
+      </div>
+      <div className="flex">
+        <button
+          type="button"
+          className="mr-2 inline-flex items-center rounded-lg bg-blue-700 px-3 py-1.5 text-center text-xs font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-800 dark:hover:bg-blue-900"
+        >
+          <HiEye className="-ml-0.5 mr-2 h-4 w-4" />
+          View more
+        </button>
+        <button
+          type="button"
+          className="rounded-lg border border-blue-700 bg-transparent px-3 py-1.5 text-center text-xs font-medium text-blue-700 hover:bg-blue-800 hover:text-white focus:ring-4 focus:ring-blue-300 dark:border-blue-800 dark:text-blue-800 dark:hover:text-white"
+        >
+          Dismiss
+        </button>
+      </div>
     </>
   ),
 };
