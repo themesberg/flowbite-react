7a37db32a9c633a826d092e1b2a4af6f6d4510d3
Conner Davis <conner@tulup.io>
2022-04-30 02:24:57 -0700

    test(component): Add stories for `Sidebar`, resolving #56
    The `Sidebar` is not highly configurable at the root
    component, but there are a number of ways to set up
    the contents, so we added those examples.

---
+++ src/lib/components/Sidebar/Sidebar.stories.tsx
@@ -0,0 +1,52 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+
+import { Sidebar } from '.';
+import {
+  CTASidebarComponent,
+  DefaultSidebarComponent,
+  DropdownSidebarComponent,
+  LogoSidebarComponent,
+  SeparatorSidebarComponent,
+} from '../../pages/SidebarPage';
+
+export default {
+  title: 'Components/Sidebar',
+  component: Sidebar,
+} as Meta;
+
+const Template: Story = (args) => <Sidebar {...args} />;
+
+export const DefaultSidebar = Template.bind({});
+DefaultSidebar.storyName = 'Default';
+DefaultSidebar.args = {
+  children: <DefaultSidebarComponent />,
+  collapsed: false,
+};
+
+export const MultiLevelDropdown = Template.bind({});
+MultiLevelDropdown.storyName = 'Multi-level dropdown';
+MultiLevelDropdown.args = {
+  children: <DropdownSidebarComponent />,
+  collapsed: false,
+};
+
+export const ContentSeparator = Template.bind({});
+ContentSeparator.storyName = 'Content separator';
+ContentSeparator.args = {
+  children: <SeparatorSidebarComponent />,
+  collapsed: false,
+};
+
+export const CTAButton = Template.bind({});
+CTAButton.storyName = 'CTA button';
+CTAButton.args = {
+  children: <CTASidebarComponent />,
+  collapsed: false,
+};
+
+export const LogoBranding = Template.bind({});
+LogoBranding.storyName = 'CTA button';
+LogoBranding.args = {
+  children: <LogoSidebarComponent />,
+  collapsed: false,
+};
