38b86bd8b10fb4d5c310f0cf214c5e935021f3cd
tulup-conner <conner@tulup.io>
2022-05-02 08:47:26 +0000

    feat(route): Add `Sidebar` examples page & stories, resolving #53 #56 (#69)
    * feat(route): Add `Sidebar` examples with new API
    
    Adds the following examples for `Sidebar`s:
    
    - [x] Default sidebar
    - [x] Multi-level dropdown
    - [x] Content separator
    - [x] CTA button
    - [x] Logo branding
    
    * test(component): Add stories for `Sidebar`, resolving #56
    
    The `Sidebar` is not highly configurable at the root
    component, but there are a number of ways to set up
    the contents, so we added those examples.
    
    * refactor(story): Fix dupe story name in `Sidebar`
    
    * fix(route): Restore accurate `Sidebar` example markup
    
    Creating a component to reuse the code between
    `storybook` and the page caused the page to display
    the shortcut component instead of the actual
    component's code.
    
    * refactor(route): Use `flowbite-react` components in `Sidebar` example

---
+++ src/docs/pages/SidebarPage.tsx
@@ -0,0 +1,216 @@
+import { FC } from 'react';
+import { BiBuoy } from 'react-icons/bi';
+import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from 'react-icons/hi';
+import { Sidebar, Badge, Button } from '../../lib';
+import { CodeExample, DemoPage } from './DemoPage';
+
+const SidebarPage: FC = () => {
+  const examples: CodeExample[] = [
+    {
+      title: 'Default sidebar',
+      code: (
+        <Sidebar collapsed={false}>
+          <Sidebar.Items>
+            <Sidebar.ItemGroup>
+              <Sidebar.Item href="#" icon={HiChartPie}>
+                Dashboard
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiViewBoards} label="Pro" labelColor="gray">
+                Kanban
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiInbox} label="3">
+                Inbox
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiUser}>
+                Users
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiShoppingBag}>
+                Products
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiArrowSmRight}>
+                Sign In
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiTable}>
+                Sign Up
+              </Sidebar.Item>
+            </Sidebar.ItemGroup>
+          </Sidebar.Items>
+        </Sidebar>
+      ),
+    },
+    {
+      title: 'Multi-level dropdown',
+      code: (
+        <Sidebar collapsed={false}>
+          <Sidebar.Items>
+            <Sidebar.ItemGroup>
+              <Sidebar.Item href="#" icon={HiChartPie}>
+                Dashboard
+              </Sidebar.Item>
+              <Sidebar.Collapse icon={HiShoppingBag} label="E-commerce">
+                <Sidebar.Item href="#">Products</Sidebar.Item>
+              </Sidebar.Collapse>
+              <Sidebar.Item href="#" icon={HiInbox}>
+                Inbox
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiUser}>
+                Users
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiShoppingBag}>
+                Products
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiArrowSmRight}>
+                Sign In
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiTable}>
+                Sign Up
+              </Sidebar.Item>
+            </Sidebar.ItemGroup>
+          </Sidebar.Items>
+        </Sidebar>
+      ),
+    },
+    {
+      title: 'Content separator',
+      code: (
+        <Sidebar collapsed={false}>
+          <Sidebar.Items>
+            <Sidebar.ItemGroup>
+              <Sidebar.Item href="#" icon={HiChartPie}>
+                Dashboard
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiViewBoards}>
+                Kanban
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiInbox}>
+                Inbox
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiUser}>
+                Users
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiShoppingBag}>
+                Products
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiArrowSmRight}>
+                Sign In
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiTable}>
+                Sign Up
+              </Sidebar.Item>
+            </Sidebar.ItemGroup>
+            <Sidebar.ItemGroup>
+              <Sidebar.Item href="#" icon={HiChartPie}>
+                Upgrade to Pro
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiViewBoards}>
+                Documentation
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={BiBuoy}>
+                Help
+              </Sidebar.Item>
+            </Sidebar.ItemGroup>
+          </Sidebar.Items>
+        </Sidebar>
+      ),
+    },
+    {
+      title: 'CTA button',
+      code: (
+        <Sidebar collapsed={false}>
+          <Sidebar.Items>
+            <Sidebar.ItemGroup>
+              <Sidebar.Item href="#" icon={HiChartPie}>
+                Dashboard
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiViewBoards}>
+                Kanban
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiInbox}>
+                Inbox
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiUser}>
+                Users
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiShoppingBag}>
+                Products
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiArrowSmRight}>
+                Sign In
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiTable}>
+                Sign Up
+              </Sidebar.Item>
+            </Sidebar.ItemGroup>
+          </Sidebar.Items>
+          <Sidebar.CTA>
+            <div className="mb-3 flex items-center">
+              <Badge color="yellow">Beta</Badge>
+              <Button
+                aria-label="Close"
+                className="-mx-1.5 -my-1.5 ml-auto !h-6 !w-6 bg-transparent !p-1 text-blue-900 hover:bg-blue-200"
+                data-collapse-toggle="dropdown-cta"
+              >
+                <span className="sr-only">Close</span>
+                <svg className="h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </Button>
+            </div>
+            <p className="mb-3 text-sm text-blue-900 dark:text-blue-400">
+              Preview the new Flowbite dashboard navigation! You can turn the new navigation off for a limited time in
+              your profile.
+            </p>
+            <a
+              className="text-sm text-blue-900 underline hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300"
+              href="#"
+            >
+              Turn new navigation off
+            </a>
+          </Sidebar.CTA>
+        </Sidebar>
+      ),
+    },
+    {
+      title: 'Logo branding',
+      code: (
+        <Sidebar collapsed={false}>
+          <Sidebar.Logo href="#" img="logo192.png" imgAlt="Flowbite logo">
+            Flowbite
+          </Sidebar.Logo>
+          <Sidebar.Items>
+            <Sidebar.ItemGroup>
+              <Sidebar.Item href="#" icon={HiChartPie}>
+                Dashboard
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiViewBoards}>
+                Kanban
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiInbox}>
+                Inbox
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiUser}>
+                Users
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiShoppingBag}>
+                Products
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiArrowSmRight}>
+                Sign In
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiTable}>
+                Sign Up
+              </Sidebar.Item>
+            </Sidebar.ItemGroup>
+          </Sidebar.Items>
+        </Sidebar>
+      ),
+    },
+  ];
+  return <DemoPage examples={examples} />;
+};
+
+export default SidebarPage;
---
+++ src/lib/components/Sidebar/Sidebar.stories.tsx
@@ -0,0 +1,232 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { BiBuoy } from 'react-icons/bi';
+import { HiChartPie, HiViewBoards, HiInbox, HiUser, HiShoppingBag, HiArrowSmRight, HiTable } from 'react-icons/hi';
+
+import { Sidebar } from '.';
+import { Badge } from '../Badge';
+import { Button } from '../Button';
+
+export default {
+  title: 'Components/Sidebar',
+  component: Sidebar,
+} as Meta;
+
+const Template: Story = (args) => <Sidebar {...args} />;
+
+export const DefaultSidebar = Template.bind({});
+DefaultSidebar.storyName = 'Default';
+DefaultSidebar.args = {
+  children: (
+    <Sidebar collapsed={false}>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item href="#" icon={HiChartPie}>
+            Dashboard
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiViewBoards} label="Pro" labelColor="gray">
+            Kanban
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiInbox} label="3">
+            Inbox
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiUser}>
+            Users
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiShoppingBag}>
+            Products
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiArrowSmRight}>
+            Sign In
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiTable}>
+            Sign Up
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  ),
+  collapsed: false,
+};
+
+export const MultiLevelDropdown = Template.bind({});
+MultiLevelDropdown.storyName = 'Multi-level dropdown';
+MultiLevelDropdown.args = {
+  children: (
+    <Sidebar collapsed={false}>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item href="#" icon={HiChartPie}>
+            Dashboard
+          </Sidebar.Item>
+          <Sidebar.Collapse icon={HiShoppingBag} label="E-commerce">
+            <Sidebar.Item href="#">Products</Sidebar.Item>
+          </Sidebar.Collapse>
+          <Sidebar.Item href="#" icon={HiInbox}>
+            Inbox
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiUser}>
+            Users
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiShoppingBag}>
+            Products
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiArrowSmRight}>
+            Sign In
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiTable}>
+            Sign Up
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  ),
+  collapsed: false,
+};
+
+export const ContentSeparator = Template.bind({});
+ContentSeparator.storyName = 'Content separator';
+ContentSeparator.args = {
+  children: (
+    <Sidebar collapsed={false}>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item href="#" icon={HiChartPie}>
+            Dashboard
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiViewBoards}>
+            Kanban
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiInbox}>
+            Inbox
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiUser}>
+            Users
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiShoppingBag}>
+            Products
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiArrowSmRight}>
+            Sign In
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiTable}>
+            Sign Up
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item href="#" icon={HiChartPie}>
+            Upgrade to Pro
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiViewBoards}>
+            Documentation
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={BiBuoy}>
+            Help
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  ),
+  collapsed: false,
+};
+
+export const CTAButton = Template.bind({});
+CTAButton.storyName = 'CTA button';
+CTAButton.args = {
+  children: (
+    <Sidebar collapsed={false}>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item href="#" icon={HiChartPie}>
+            Dashboard
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiViewBoards}>
+            Kanban
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiInbox}>
+            Inbox
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiUser}>
+            Users
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiShoppingBag}>
+            Products
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiArrowSmRight}>
+            Sign In
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiTable}>
+            Sign Up
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+      <Sidebar.CTA>
+        <div className="mb-3 flex items-center">
+          <Badge color="yellow">Beta</Badge>
+          <Button
+            aria-label="Close"
+            className="-mx-1.5 -my-1.5 ml-auto !h-6 !w-6 bg-transparent !p-1 text-blue-900 hover:bg-blue-200"
+            data-collapse-toggle="dropdown-cta"
+          >
+            <span className="sr-only">Close</span>
+            <svg className="h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </Button>
+        </div>
+        <p className="mb-3 text-sm text-blue-900 dark:text-blue-400">
+          Preview the new Flowbite dashboard navigation! You can turn the new navigation off for a limited time in your
+          profile.
+        </p>
+        <a
+          className="text-sm text-blue-900 underline hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300"
+          href="#"
+        >
+          Turn new navigation off
+        </a>
+      </Sidebar.CTA>
+    </Sidebar>
+  ),
+  collapsed: false,
+};
+
+export const LogoBranding = Template.bind({});
+LogoBranding.storyName = 'Logo branding';
+LogoBranding.args = {
+  children: (
+    <Sidebar collapsed={false}>
+      <Sidebar.Logo href="#" img="logo192.png" imgAlt="Flowbite logo">
+        Flowbite
+      </Sidebar.Logo>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item href="#" icon={HiChartPie}>
+            Dashboard
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiViewBoards}>
+            Kanban
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiInbox}>
+            Inbox
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiUser}>
+            Users
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiShoppingBag}>
+            Products
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiArrowSmRight}>
+            Sign In
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiTable}>
+            Sign Up
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  ),
+  collapsed: false,
+};
