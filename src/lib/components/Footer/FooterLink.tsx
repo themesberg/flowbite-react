320236153be26587ec6ae8d8d600ea08bd766758
mouracamila <camilapaleo@gmail.com>
2022-05-03 11:29:38 +0200

    refactore: improvement at the FooterLink  component

--- src/lib/components/Footer/FooterLink.tsx
+++ src/lib/components/Footer/FooterLink.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { FC, PropsWithChildren } from 'react';
 
 export type FooterLinkProps = PropsWithChildren<{
@@ -7,8 +8,8 @@ export type FooterLinkProps = PropsWithChildren<{
 
 export const FooterLink: FC<FooterLinkProps> = ({ href, children, className }) => {
   return (
-    <li className={className}>
-      <a href={href} className="mr-4 hover:underline md:mr-6 ">
+    <li className={classNames('mr-4 last:mr-0 md:mr-6', className)}>
+      <a href={href} className="hover:underline">
         {children}
       </a>
     </li>
