7609730ee7b255c08585f551a5d761ba1078754f
mouracamila <camilapaleo@gmail.com>
2022-05-03 11:31:31 +0200

    refactore: add className in the FooterBrand

--- src/lib/components/Footer/FooterBrand.tsx
+++ src/lib/components/Footer/FooterBrand.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { FC, PropsWithChildren } from 'react';
 
 export type FooterBrandProps = PropsWithChildren<{
@@ -5,10 +6,11 @@ export type FooterBrandProps = PropsWithChildren<{
   src?: string;
   alt?: string;
   name?: string;
+  className?: string;
 }>;
 
-export const FooterBrand: FC<FooterBrandProps> = ({ children, href, src, alt, name }) => (
-  <a href={href} className="mb-4 flex items-center sm:mb-0">
+export const FooterBrand: FC<FooterBrandProps> = ({ children, href, src, alt, name, className }) => (
+  <a href={href} className={classNames('mb-4 flex items-center sm:mb-0', className)}>
     <img src={src} className="mr-3 h-8" alt={alt} />
     <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">{name}</span>
     {children}
