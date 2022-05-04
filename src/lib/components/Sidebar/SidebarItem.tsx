44454017ce9f318a3c655793654e60f0ae6d1355
Conner Davis <conner@tulup.io>
2022-05-03 22:43:23 -0700

    fix(component): Add dark mode for `Sidebar.Item` icons

--- src/lib/components/Sidebar/SidebarItem.tsx
+++ src/lib/components/Sidebar/SidebarItem.tsx
@@ -57,7 +57,12 @@ const SidebarItem: FC<SidebarItemProps> = ({
         {...rest}
       >
         {Icon && (
-          <Icon className="h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" />
+          <Icon
+            className={classNames(
+              'h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white',
+              { 'text-gray-700 dark:text-gray-100': href === pathname },
+            )}
+          />
         )}
         {!collapsed && <span className="ml-3 flex-1 whitespace-nowrap">{children}</span>}
         {!collapsed && label && <Badge color={labelColor}>{label}</Badge>}
