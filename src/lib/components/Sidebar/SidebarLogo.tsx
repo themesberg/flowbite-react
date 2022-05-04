3f537506640d2c71545bb82d72d6e30e88a3f78b
Conner Davis <conner@tulup.io>
2022-05-03 23:58:56 -0700

    fix(a11y): Add label to `Sidebar.Logo` when `Sidebar` is collapsed

--- src/lib/components/Sidebar/SidebarLogo.tsx
+++ src/lib/components/Sidebar/SidebarLogo.tsx
@@ -15,7 +15,9 @@ const SidebarLogo: FC<SidebarLogoProps> = ({ children, className, href, img, img
   return (
     <a href={href} className={classNames('mb-5 flex items-center pl-2.5', className)}>
       <img alt={imgAlt} className="mr-3 h-6 sm:h-7" src={img} />
-      {!collapsed && (
+      {collapsed ? (
+        <span className="sr-only">{children}</span>
+      ) : (
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">{children}</span>
       )}
     </a>
