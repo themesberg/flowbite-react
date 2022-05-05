17c639cd1b023c79856d3ae7b3bc69613ea4bdc7
mouracamila <camilapaleo@gmail.com>
2022-05-05 16:13:04 +0200

    fix: fix the footer copyright

--- src/lib/components/Footer/FooterCopyright.tsx
+++ src/lib/components/Footer/FooterCopyright.tsx
@@ -11,19 +11,11 @@ export type CopyrightProps = PropsWithChildren<{
 export const FooterCopyright: FC<CopyrightProps> = ({ href, by, year, className }) => {
   return (
     <>
-      {by ? (
+      {by && (
         <span className={classNames('text-sm text-gray-500 dark:text-gray-400 sm:text-center', className)}>
           © {year}
           <a href={href} className="ml-1 hover:underline">
             {by}
-          </a>
-          . All Rights Reserved.
-        </span>
-      ) : (
-        <span className="block text-sm text-gray-500 dark:text-gray-400 sm:text-center">
-          © 2022
-          <a href="https://flowbite.com" className="hover:underline">
-            Flowbite™
           </a>
           . All Rights Reserved.
         </span>
