5bb699f8e4393fa3ec5281a13886fb464eec8d15
mouracamila <camilapaleo@gmail.com>
2022-05-05 16:52:59 +0200

    fix: improving flow handling

--- src/lib/components/Footer/FooterCopyright.tsx
+++ src/lib/components/Footer/FooterCopyright.tsx
@@ -3,7 +3,7 @@ import { FC, PropsWithChildren } from 'react';
 
 export type CopyrightProps = PropsWithChildren<{
   href?: string;
-  by?: string;
+  by: string;
   year?: number;
   className?: string;
 }>;
@@ -11,15 +11,17 @@ export type CopyrightProps = PropsWithChildren<{
 export const FooterCopyright: FC<CopyrightProps> = ({ href, by, year, className }) => {
   return (
     <>
-      {by && (
-        <span className={classNames('text-sm text-gray-500 dark:text-gray-400 sm:text-center', className)}>
-          © {year}
+      <span className={classNames('text-sm text-gray-500 dark:text-gray-400 sm:text-center', className)}>
+        © {year}
+        {href ? (
           <a href={href} className="ml-1 hover:underline">
             {by}
           </a>
-          . All Rights Reserved.
-        </span>
-      )}
+        ) : (
+          <span className="ml-1">{by}</span>
+        )}
+        . All Rights Reserved.
+      </span>
     </>
   );
 };
