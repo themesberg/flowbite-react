ee42c98b8bb8d790777f1a9646c33509aad8e7c6
Conner Davis <conner@tulup.io>
2022-05-05 22:45:11 -0700

    feature(component): Accept HTML attributes on `Sidebar.ItemGroup`

--- src/lib/components/Sidebar/SidebarItemGroup.tsx
+++ src/lib/components/Sidebar/SidebarItemGroup.tsx
@@ -2,13 +2,14 @@ import classNames from 'classnames';
 import { FC, HTMLAttributes, PropsWithChildren } from 'react';
 import { SidebarItemContext } from './SidebarItemContext';
 
-const SidebarItemGroup: FC<PropsWithChildren<HTMLAttributes<HTMLUListElement>>> = ({ children }) => {
+const SidebarItemGroup: FC<PropsWithChildren<HTMLAttributes<HTMLUListElement>>> = ({ children, ...rest }) => {
   return (
     <ul
       className={classNames(
         'mt-4 space-y-2 border-t border-gray-200 pt-4 first:border-t-0 first:pt-0 dark:border-gray-700',
       )}
       data-testid="sidebar-item-group"
+      {...rest}
     >
       <SidebarItemContext.Provider value={{ insideCollapse: false }}>{children}</SidebarItemContext.Provider>
     </ul>
