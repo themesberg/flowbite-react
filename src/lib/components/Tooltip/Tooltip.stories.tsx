ae548473278ab75fab394cb9d5226768a5384f26
Conner Davis <conner@tulup.io>
2022-05-09 03:29:45 -0700

    feature(story): Add stories for `Tooltip`
    - [x] Trigger on click
    - [X] No arrow
    - [x] Slow animation

--- src/lib/components/Tooltip/Tooltip.stories.tsx
+++ src/lib/components/Tooltip/Tooltip.stories.tsx
@@ -17,3 +17,30 @@ DefaultTooltip.args = {
   placement: 'bottom',
   children: <Button>Default tooltip</Button>,
 };
+
+export const TriggerOnClick = Template.bind({});
+TriggerOnClick.storyName = 'Trigger on click';
+TriggerOnClick.args = {
+  content: 'Tooltip content',
+  placement: 'bottom',
+  trigger: 'click',
+  children: <Button>Clickable tooltip</Button>,
+};
+
+export const NoArrow = Template.bind({});
+NoArrow.storyName = 'No arrow';
+NoArrow.args = {
+  arrow: false,
+  content: 'Tooltip content',
+  placement: 'bottom',
+  children: <Button>Tooltip with no arrow</Button>,
+};
+
+export const SlowAnimation = Template.bind({});
+SlowAnimation.storyName = 'Slow animation';
+SlowAnimation.args = {
+  animation: 'duration-1000',
+  content: 'Tooltip content',
+  placement: 'bottom',
+  children: <Button>Tooltip with slow animation</Button>,
+};
