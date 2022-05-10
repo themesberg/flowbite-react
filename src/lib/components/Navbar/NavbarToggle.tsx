e2c977b5245184f4d6a87a799684c0a5bdd789bf
Conner Davis <conner@tulup.io>
2022-05-10 04:32:35 -0700

    test(component): Add unit tests for `Navbar`s, resolves #41

--- src/lib/components/Navbar/NavbarToggle.tsx
+++ src/lib/components/Navbar/NavbarToggle.tsx
@@ -19,6 +19,7 @@ export const NavbarToggle: FC<NavbarToggleProps> = ({ barIcon: BarIcon = GoThree
       className={classNames(
         'ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden',
       )}
+      data-testid="navbar-toggle"
       onClick={handleClick}
     >
       <span className="sr-only">Open main menu</span>
