1342924f81eaffd8f2e53f3d094195c261bad491
Conner Davis <conner@tulup.io>
2022-05-10 03:36:48 -0700

    test(component): Add unit tests for `Rating`s, resolves #43

--- src/lib/components/Rating/RatingAdvanced.tsx
+++ src/lib/components/Rating/RatingAdvanced.tsx
@@ -9,7 +9,7 @@ export const RatingAdvanced: FC<RatingAdvancedProps> = ({ percentFilled, childre
     <div className="flex items-center">
       <span className="text-sm font-medium text-blue-600 dark:text-blue-500">{children}</span>
       <div className="mx-4 h-5 w-2/4 rounded bg-gray-200 dark:bg-gray-700">
-        <div className="h-5 rounded bg-yellow-400" style={{ width: `${percentFilled}%` }} />
+        <div className="h-5 rounded bg-yellow-400" data-testid="rating-fill" style={{ width: `${percentFilled}%` }} />
       </div>
       <span className="text-sm font-medium text-blue-600 dark:text-blue-500">{`${percentFilled}%`}</span>
     </div>
