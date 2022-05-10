9b96276cace378cde6860bdfc930bd64f7df0960
Conner Davis <conner@tulup.io>
2022-05-10 04:09:34 -0700

    feature(story): Add missing `Navbar` stories

--- src/lib/components/Navbar/Navbar.stories.tsx
+++ src/lib/components/Navbar/Navbar.stories.tsx
@@ -1,4 +1,7 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
+import { Avatar } from '../Avatar';
+import { Button } from '../Button';
+import { Dropdown } from '../Dropdown';
 
 import { Navbar, NavbarComponentProps } from '../Navbar';
 
@@ -7,7 +10,11 @@ export default {
   component: Navbar,
 } as Meta;
 
-const Template: Story<NavbarComponentProps> = (args) => <Navbar {...args} />;
+const Template: Story<NavbarComponentProps> = (args) => (
+  <div className="w-4/5">
+    <Navbar {...args} />
+  </div>
+);
 
 export const DefaultNavbar = Template.bind({});
 DefaultNavbar.storyName = 'Default';
@@ -19,6 +26,71 @@ DefaultNavbar.args = {
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
       </Navbar.Brand>
       <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Navbar.Link href="/navbars" active>
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="/navbars">About</Navbar.Link>
+        <Navbar.Link href="/navbars">Services</Navbar.Link>
+        <Navbar.Link href="/navbars">Pricing</Navbar.Link>
+        <Navbar.Link href="/navbars">Contact</Navbar.Link>
+      </Navbar.Collapse>
+    </>
+  ),
+};
+
+export const WithCTA = Template.bind({});
+WithCTA.args = {
+  children: (
+    <>
+      <Navbar.Brand href="https://flowbite.com/">
+        <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <Button>Get started</Button>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link href="/navbars" active>
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="/navbars">About</Navbar.Link>
+        <Navbar.Link href="/navbars">Services</Navbar.Link>
+        <Navbar.Link href="/navbars">Pricing</Navbar.Link>
+        <Navbar.Link href="/navbars">Contact</Navbar.Link>
+      </Navbar.Collapse>
+    </>
+  ),
+};
+
+export const WithDropdown = Template.bind({});
+WithDropdown.storyName = 'With dropdown';
+WithDropdown.args = {
+  children: (
+    <>
+      <Navbar.Brand href="https://flowbite.com/">
+        <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={<Avatar img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />}
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
+          </Dropdown.Header>
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item>Earnings</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
+        <Navbar.Toggle />
+      </div>
       <Navbar.Collapse>
         <Navbar.Link href="/navbars" active>
           Home
