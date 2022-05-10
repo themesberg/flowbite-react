1cf3cede89041f638001eed47fb111e32858e067
Conner Davis <conner@tulup.io>
2022-05-10 05:46:54 -0700

    feature(docs): Add missing example to `Breadcrumb` page
    - [x] Solid background

--- src/docs/pages/BreadcrumbPage.tsx
+++ src/docs/pages/BreadcrumbPage.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { HiHome } from 'react-icons/hi';
+import Breadcrumb from '../../lib/components/Breadcrumb';
 
-import { Breadcrumb } from '../../lib';
 import { CodeExample, DemoPage } from './DemoPage';
 
 const BreadcrumbPage: FC = () => {
@@ -9,22 +9,25 @@ const BreadcrumbPage: FC = () => {
     {
       title: 'Default breadcrumb',
       code: (
-        <Breadcrumb
-          items={[
-            {
-              icon: HiHome,
-              label: 'Home',
-              href: '/breadcrumb',
-            },
-            {
-              label: 'Projects',
-              href: '/breadcrumb',
-            },
-            {
-              label: 'Flowbite React',
-            },
-          ]}
-        />
+        <Breadcrumb>
+          <Breadcrumb.Item href="#" icon={HiHome}>
+            Home
+          </Breadcrumb.Item>
+          <Breadcrumb.Item href="#">Projects</Breadcrumb.Item>
+          <Breadcrumb.Item icon={HiHome}>Flowbite React</Breadcrumb.Item>
+        </Breadcrumb>
+      ),
+    },
+    {
+      title: 'Solid background',
+      code: (
+        <Breadcrumb className="bg-gray-50 py-3 px-5 dark:bg-gray-900">
+          <Breadcrumb.Item href="#" icon={HiHome}>
+            Home
+          </Breadcrumb.Item>
+          <Breadcrumb.Item href="#">Projects</Breadcrumb.Item>
+          <Breadcrumb.Item icon={HiHome}>Flowbite React</Breadcrumb.Item>
+        </Breadcrumb>
       ),
     },
   ];
