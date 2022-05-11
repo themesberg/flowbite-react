b55526a5b0372195c32a4a8f9f22d9f938ff846d
Conner Davis <conner@tulup.io>
2022-05-11 04:10:09 -0700

    feature(story): Add `Pagination` stories

--- src/lib/components/Pagination/Pagination.stories.tsx
+++ src/lib/components/Pagination/Pagination.stories.tsx
@@ -1,13 +1,68 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
-
-import { Pagination, PaginationProps } from '.';
+import { useEffect, useState } from 'react';
+import Pagination, { PaginationProps } from '.';
 
 export default {
   title: 'Components/Pagination',
   component: Pagination,
+  decorators: [
+    (Story): JSX.Element => (
+      <div className="flex items-center justify-center text-center">
+        <Story />
+      </div>
+    ),
+  ],
 } as Meta;
 
-const Template: Story<PaginationProps> = (args) => <Pagination {...args} />;
+const Template: Story<PaginationProps> = ({
+  currentPage = 1,
+  layout = 'pagination',
+  totalPages = 100,
+  ...rest
+}): JSX.Element => {
+  const [page, setPage] = useState(currentPage);
 
-export const DefaultProgress = Template.bind({});
-DefaultProgress.storyName = 'Default';
+  const onPageChange = (page: number) => {
+    setPage(page);
+  };
+
+  useEffect(() => {
+    setPage(currentPage);
+  }, [currentPage]);
+
+  return (
+    <Pagination {...rest} currentPage={page} layout={layout} onPageChange={onPageChange} totalPages={totalPages} />
+  );
+};
+
+export const Default = Template.bind({});
+
+export const PaginationWithIcons = Template.bind({});
+PaginationWithIcons.storyName = 'Pagination with icons';
+PaginationWithIcons.args = {
+  showIcons: true,
+};
+
+export const Nav = Template.bind({});
+Nav.args = {
+  layout: 'navigation',
+};
+
+export const NavWithIcons = Template.bind({});
+NavWithIcons.storyName = 'Nav with icons';
+NavWithIcons.args = {
+  layout: 'navigation',
+  showIcons: true,
+};
+
+export const Table = Template.bind({});
+Table.args = {
+  layout: 'table',
+};
+
+export const TableWithIcons = Template.bind({});
+TableWithIcons.storyName = 'Table with icons';
+TableWithIcons.args = {
+  layout: 'table',
+  showIcons: true,
+};
