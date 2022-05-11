c8770742bd2aac752f29a01b15f569ab2058dd19
Conner Davis <conner@tulup.io>
2022-05-10 22:56:59 -0700

    test(component): Add unit tests for `DarkThemeToggle`, resolves #50

---
+++ src/lib/components/DarkThemeToggle/DarkThemeToggle.spec.tsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { DarkThemeToggle } from '.';
+import { Flowbite } from '../Flowbite';
+
+describe('Dark theme toggle', () => {
+  describe('when clicked', () => {
+    it('should toggle dark mode on page', () => {
+      const { getByTestId, queryByTestId } = render(
+        <Flowbite>
+          <DarkThemeToggle />
+        </Flowbite>,
+      );
+
+      const toggle = getByTestId('dark-theme-toggle');
+      userEvent.click(toggle);
+      expect(queryByTestId('dark-theme-toggle-enabled')).not.toBeInTheDocument();
+      expect(getByTestId('dark-theme-toggle-disabled')).toBeInTheDocument();
+    });
+  });
+});
--- src/lib/components/DarkThemeToggle/index.tsx
+++ src/lib/components/DarkThemeToggle/index.tsx
@@ -7,11 +7,16 @@ export const DarkThemeToggle: FC = () => {
 
   return (
     <button
-      type="button"
       className="rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+      data-testid="dark-theme-toggle"
       onClick={toggleMode}
+      type="button"
     >
-      {mode === 'dark' ? <HiSun className="h-5 w-5" /> : <HiMoon className="h-5 w-5" />}
+      {mode === 'dark' ? (
+        <HiSun className="h-5 w-5" data-testid="dark-theme-toggle-disabled" />
+      ) : (
+        <HiMoon className="h-5 w-5" data-testid="dark-theme-toggle-enabled" />
+      )}
     </button>
   );
 };
