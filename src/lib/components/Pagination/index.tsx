190017df01aa0c6294ba7625a5cd2f68cb950ead
Conner Davis <conner@tulup.io>
2022-05-11 23:19:52 -0700

    test(component): Add unit tests for `Pagination`

--- src/lib/components/Pagination/index.tsx
+++ src/lib/components/Pagination/index.tsx
@@ -56,7 +56,7 @@ const Pagination: FC<PaginationProps> = ({
         {layout === 'pagination' &&
           range(firstPage, lastPage).map(
             (page: number): JSX.Element => (
-              <li key={page}>
+              <li aria-current={page === currentPage ? 'page' : undefined} key={page}>
                 <button
                   className={classNames(
                     'w-12 border border-gray-300 bg-white py-2 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white',
