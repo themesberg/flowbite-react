4f1e41adfaa3069defdad20ede2b16031a3f43e9
Conner Davis <conner@tulup.io>
2022-05-12 20:45:18 -0700

    fix(test): Add missing test case to `mergeDeep` helper

--- src/lib/helpers/mergeDeep.spec.ts
+++ src/lib/helpers/mergeDeep.spec.ts
@@ -1,7 +1,63 @@
 import { mergeDeep } from './mergeDeep';
 
 describe('Helper / mergeDeep (Deeply merge two objects)', () => {
-  describe('given two objects', () => {
+  describe('given two unknown values', () => {
+    describe('both containing an array with same key', () => {
+      it('should only keep elements in latter object', () => {
+        const combineMe = {
+          a: {
+            fruit: ['apples', 'bananas'],
+          },
+          b: 2,
+          c: 3,
+        };
+        const withThis = {
+          a: {
+            fruit: ['lemons'],
+          },
+          d: 4,
+          e: 5,
+        };
+
+        expect(mergeDeep(combineMe, withThis)).toEqual({
+          a: {
+            fruit: ['lemons'],
+          },
+          b: 2,
+          c: 3,
+          d: 4,
+          e: 5,
+        });
+      });
+    });
+
+    describe('containing different objects', () => {
+      it('should combine them', () => {
+        const combineMe = {
+          a: {
+            fruit: 'apple',
+          },
+          b: 2,
+        };
+        const withThis = {
+          b: 2,
+          c: {
+            vegetable: 'carrot',
+          },
+          d: 4,
+          e: 5,
+        };
+
+        expect(mergeDeep(combineMe, withThis)).toEqual({
+          a: { fruit: 'apple' },
+          b: 2,
+          c: { vegetable: 'carrot' },
+          d: 4,
+          e: 5,
+        });
+      });
+    });
+
     describe('with identical items', () => {
       it("shouldn't duplicate them", () => {
         const combineMe = {
@@ -70,35 +126,6 @@ describe('Helper / mergeDeep (Deeply merge two objects)', () => {
         c: 3,
         d: 4,
         e: 5,
-      });
-    });
-
-    describe('both containing an array with same key', () => {
-      it('should only keep elements in latter object', () => {
-        const combineMe = {
-          a: {
-            fruit: ['apples', 'bananas'],
-          },
-          b: 2,
-          c: 3,
-        };
-        const withThis = {
-          a: {
-            fruit: ['lemons'],
-          },
-          d: 4,
-          e: 5,
-        };
-
-        expect(mergeDeep(combineMe, withThis)).toEqual({
-          a: {
-            fruit: ['lemons'],
-          },
-          b: 2,
-          c: 3,
-          d: 4,
-          e: 5,
-        });
       });
     });
   });
