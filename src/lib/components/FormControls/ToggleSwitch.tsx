1844ca84e51ca0b20f570c9d19a4ef00509b3829
Conner Davis <conner@tulup.io>
2022-05-15 05:20:18 -0700

    feature(component): Add keyboard controls to `ToggleSwitch`
    This is a breaking API change.
    
    `ToggleSwitch` now requires:
    
    - `onChange(checked: boolean)`
    
    You can now use Tab to navigate into a switch,
    and use Space to toggle its state.

--- src/lib/components/FormControls/ToggleSwitch.tsx
+++ src/lib/components/FormControls/ToggleSwitch.tsx
@@ -1,24 +1,82 @@
-import { ComponentProps, FC } from 'react';
+import { ComponentProps, FC, KeyboardEvent, MouseEvent, useMemo } from 'react';
 import classNames from 'classnames';
+import * as nanoid from 'nanoid';
 
-export type ToggleSwitchProps = ComponentProps<'input'> & {
+export type ToggleSwitchProps = Omit<ComponentProps<'button'>, 'onChange'> & {
+  checked: boolean;
   label: string;
+  onChange: (checked: boolean) => void;
 };
 
-export const ToggleSwitch: FC<ToggleSwitchProps> = ({ className, label, id, disabled, ...props }) => (
-  <label
-    htmlFor={id}
-    className={classNames(
-      'relative flex items-center',
-      {
-        'cursor-not-allowed opacity-50': disabled,
-        'cursor-pointer': !disabled,
-      },
-      className,
-    )}
-  >
-    <input type="checkbox" id={id} className="sr-only" disabled={disabled} {...props} />
-    <div className="toggle-bg h-6 w-11 rounded-full border border-gray-200 bg-gray-200 dark:border-gray-600 dark:bg-gray-700" />
-    <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">{label}</span>
-  </label>
-);
+export const ToggleSwitch: FC<ToggleSwitchProps> = ({
+  checked,
+  className,
+  disabled,
+  label,
+  name,
+  onChange,
+  ...props
+}) => {
+  const id = useMemo(() => nanoid.nanoid(), []);
+
+  const toggle = (): void => onChange(!checked);
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>): void => {
+    event.preventDefault();
+    toggle();
+  };
+
+  const handleKeyUp = (event: KeyboardEvent<HTMLButtonElement>): void => {
+    if (event.key === 'Space') {
+      event.preventDefault();
+      toggle();
+    }
+  };
+
+  const handleKeyPress = (event: KeyboardEvent<HTMLButtonElement>): void => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      {name && checked && <input checked={checked} hidden name={name} readOnly type="checkbox" className="sr-only" />}
+      <button
+        aria-checked={checked}
+        aria-labelledby={`${id}-flowbite-toggleswitch-label`}
+        disabled={disabled}
+        id={`${id}-flowbite-toggleswitch`}
+        onClick={handleClick}
+        onKeyDown={handleKeyUp}
+        onKeyPress={handleKeyPress}
+        role="switch"
+        tabIndex={0}
+        type="button"
+        className={classNames(
+          'group relative flex items-center rounded-lg focus:outline-none',
+          {
+            'cursor-not-allowed opacity-50': disabled,
+            'cursor-pointer': !disabled,
+          },
+          className,
+        )}
+        {...props}
+      >
+        <div
+          className={classNames(
+            'toggle-bg h-6 w-11 rounded-full border group-focus:ring-4 group-focus:ring-blue-500/25',
+            checked
+              ? 'border-blue-700 bg-blue-700 after:translate-x-full after:border-white'
+              : 'border-gray-200 bg-gray-200 dark:border-gray-600 dark:bg-gray-700',
+          )}
+        />
+        <span
+          data-testid="flowbite-toggleswitch-label"
+          id={`${id}-flowbite-toggleswitch-label`}
+          className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"
+        >
+          {label}
+        </span>
+      </button>
+    </>
+  );
+};
