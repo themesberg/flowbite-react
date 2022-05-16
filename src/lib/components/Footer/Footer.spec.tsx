6d7ee16bf79c79a932a2f931e8dc49c96c14dd84
mouracamila <camilapaleo@gmail.com>
2022-05-16 17:52:17 +0200

    feat: increasing test coverage

--- src/lib/components/Footer/Footer.spec.tsx
+++ src/lib/components/Footer/Footer.spec.tsx
@@ -1,11 +1,41 @@
 import { cleanup, render } from '@testing-library/react';
 import { Footer } from '.';
+import { BsFacebook } from 'react-icons/bs';
 
 describe('Footer Component should be able to render a', () => {
   afterEach(cleanup);
 
   it('footer', () => {
-    const { getByTestId } = render(<Footer />);
+    const { getByTestId } = render(<FooterTest />);
     expect(getByTestId('footer-element')).toBeTruthy();
   });
+
+  it('footer with logo', () => {
+    const { getByTestId } = render(<FooterTest />);
+    expect((getByTestId('footer-element').children[0].children[0] as HTMLImageElement).src).toEqual(
+      'https://flowbite.com/docs/images/logo.svg',
+    );
+  });
+
+  it('footer with social media icons ', () => {
+    const { getByTestId } = render(<FooterTest />);
+    expect(getByTestId('footer-element').children[3].children[0].tagName).toEqual('svg');
+  });
 });
+
+const FooterTest = (): JSX.Element => (
+  <Footer className="flex flex-col">
+    <Footer.Brand
+      href="https://flowbite.com"
+      src="https://flowbite.com/docs/images/logo.svg"
+      alt="Flowbite Logo"
+      name="Flowbite"
+    />
+    <Footer.LinkGroup className="mt-3 flex-wrap items-center text-sm sm:mt-0">
+      <Footer.Link href="#">About</Footer.Link>
+      <Footer.Link href="#">Services</Footer.Link>
+    </Footer.LinkGroup>
+    <Footer.Copyright href="#" by="Flowbite™" year={2022} />
+    <Footer.Icon href="#" className="text-gray-400 hover:text-gray-900" icon={BsFacebook} />
+  </Footer>
+);
