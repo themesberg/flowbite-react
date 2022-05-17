b77cd9bca794f68c38240e1a802c6757751228bd
Conner Davis <conner@tulup.io>
2022-05-16 17:16:45 -0700

    fix(component): Check for `window` in `Modal`s, resolves #123

--- src/lib/components/Modal/index.tsx
+++ src/lib/components/Modal/index.tsx
@@ -6,6 +6,7 @@ import { ModalHeader } from './ModalHeader';
 import { ModalBody } from './ModalBody';
 import { ModalFooter } from './ModalFooter';
 import { ModalContext } from './ModalContext';
+import windowExists from '../../helpers/window-exists';
 
 type Size = 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' | '6xl' | '7xl';
 type Placement = `${'top' | 'bottom'}-${'left' | 'center' | 'right'}` | `center${'' | '-left' | '-right'}`;
@@ -46,17 +47,19 @@ const placementClasses: Record<Placement, string> = {
 
 const ModalComponent: FC<ModalProps> = ({
   children,
-  root = document.body,
+  root = windowExists() ? document.body : undefined,
   show,
   popup,
   size = '2xl',
   placement = 'center',
   onClose,
 }): JSX.Element | null => {
-  const [container] = useState<HTMLDivElement>(document.createElement('div'));
+  const [container] = useState<HTMLDivElement | undefined>(windowExists() ? document.createElement('div') : undefined);
 
   useEffect(() => {
-    if (!show) return;
+    if (!container || !root || !show) {
+      return;
+    }
 
     root.appendChild(container);
 
@@ -65,27 +68,29 @@ const ModalComponent: FC<ModalProps> = ({
     };
   }, [container, root, show]);
 
-  return createPortal(
-    <ModalContext.Provider value={{ popup, onClose }}>
-      <div
-        aria-hidden={!show}
-        className={classNames(
-          'fixed top-0 right-0 left-0 z-50 h-modal overflow-y-auto overflow-x-hidden md:inset-0 md:h-full',
-          placementClasses[placement],
-          {
-            'flex bg-gray-900 bg-opacity-50 dark:bg-opacity-80': show,
-            hidden: !show,
-          },
-        )}
-        data-testid="modal"
-      >
-        <div className={classNames('relative h-full w-full p-4 md:h-auto', sizeClasses[size])}>
-          <div className="relative rounded-lg bg-white shadow dark:bg-gray-700">{children}</div>
-        </div>
-      </div>
-    </ModalContext.Provider>,
-    container,
-  );
+  return container
+    ? createPortal(
+        <ModalContext.Provider value={{ popup, onClose }}>
+          <div
+            aria-hidden={!show}
+            className={classNames(
+              'fixed top-0 right-0 left-0 z-50 h-modal overflow-y-auto overflow-x-hidden md:inset-0 md:h-full',
+              placementClasses[placement],
+              {
+                'flex bg-gray-900 bg-opacity-50 dark:bg-opacity-80': show,
+                hidden: !show,
+              },
+            )}
+            data-testid="modal"
+          >
+            <div className={classNames('relative h-full w-full p-4 md:h-auto', sizeClasses[size])}>
+              <div className="relative rounded-lg bg-white shadow dark:bg-gray-700">{children}</div>
+            </div>
+          </div>
+        </ModalContext.Provider>,
+        container,
+      )
+    : null;
 };
 
 ModalComponent.displayName = 'Modal';
