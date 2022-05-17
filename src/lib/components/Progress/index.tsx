ba141659867e618b4c4176923a781c57933a52c7
Conner Davis <conner@tulup.io>
2022-05-17 02:38:43 -0700

    feature(a11y): Add accessible name & role to `Progress`

--- src/lib/components/Progress/index.tsx
+++ src/lib/components/Progress/index.tsx
@@ -1,18 +1,21 @@
 import classNames from 'classnames';
-import { FC, PropsWithChildren } from 'react';
+import * as nanoid from 'nanoid';
+import { ComponentProps, FC, PropsWithChildren } from 'react';
 
 type Color = 'dark' | 'blue' | 'red' | 'green' | 'yellow' | 'indigo' | 'purple';
 type Size = 'sm' | 'md' | 'lg' | 'xl';
 
-export type ProgressProps = PropsWithChildren<{
-  className?: string;
-  progress: number;
-  color?: Color;
-  size?: Size;
-  label?: string;
-  labelPosition?: 'inside' | 'outside';
-  labelProgress?: boolean;
-}>;
+export type ProgressProps = PropsWithChildren<
+  ComponentProps<'div'> & {
+    className?: string;
+    color?: Color;
+    label?: string;
+    labelPosition?: 'inside' | 'outside';
+    labelProgress?: boolean;
+    progress: number;
+    size?: Size;
+  }
+>;
 
 const colorClasses: Record<Color, string> = {
   dark: 'bg-gray-600 dark:bg-gray-300',
@@ -31,36 +34,44 @@ const sizeClasses: Record<Size, string> = {
 };
 export const Progress: FC<ProgressProps> = ({
   className,
-  progress,
   color = 'blue',
-  size = 'md',
-  label,
+  label = '',
   labelPosition = 'inside',
   labelProgress = false,
+  progress,
+  size = 'md',
+  ...props
 }) => {
+  const id = nanoid.nanoid();
+
   return (
-    <div>
-      {label && labelPosition === 'outside' && (
-        <div className="mb-1 flex justify-between">
-          <span className="font-medium dark:text-white">{label}</span>
-          {labelProgress && <span className="font-medium dark:text-white">{progress}%</span>}
-        </div>
-      )}
-      <div
-        className={classNames('w-full overflow-hidden rounded-full bg-gray-200 dark:bg-gray-700', sizeClasses[size])}
-      >
+    <>
+      <span id={`${id}-flowbite-progress`} className="sr-only">
+        {label}
+      </span>
+      <div aria-labelledby={`${id}-flowbite-progress`} aria-valuenow={progress} role="progressbar" {...props}>
+        {label && labelPosition === 'outside' && (
+          <div className="mb-1 flex justify-between font-medium dark:text-white">
+            <span>{label}</span>
+            {labelProgress && <span>{progress}%</span>}
+          </div>
+        )}
         <div
-          className={classNames(
-            'flex items-center justify-center rounded-full text-center font-medium leading-none text-blue-100',
-            colorClasses[color],
-            sizeClasses[size],
-            className,
-          )}
-          style={{ width: `${progress}%` }}
+          className={classNames('w-full overflow-hidden rounded-full bg-gray-200 dark:bg-gray-700', sizeClasses[size])}
         >
-          {label && labelPosition === 'inside' && label}
+          <div
+            className={classNames(
+              'flex items-center justify-center rounded-full text-center font-medium leading-none text-blue-100',
+              colorClasses[color],
+              sizeClasses[size],
+              className,
+            )}
+            style={{ width: `${progress}%` }}
+          >
+            {label && labelPosition === 'inside' && label}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
