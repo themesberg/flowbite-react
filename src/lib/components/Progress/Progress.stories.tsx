1e46aaca12d379207fd3e53b081800175c9748ba
tulup-conner <conner@tulup.io>
2022-05-17 04:38:06 -0700

    test(component): Add unit tests for `Progress`, resolves #42 (#148)
    * fix(component): Add missing dark mode colors to `Progress`
    
    * feature(a11y): Add accessible name & role to `Progress`
    
    * test(component): Add unit tests for `Progress`, resolves #42
    
    * feature(story): Add missing stories for `Progress`

--- src/lib/components/Progress/Progress.stories.tsx
+++ src/lib/components/Progress/Progress.stories.tsx
@@ -5,12 +5,66 @@ import { Progress, ProgressProps } from '.';
 export default {
   title: 'Components/Progress',
   component: Progress,
+  decorators: [
+    (Story): JSX.Element => (
+      <div className="flex w-1/2 flex-col">
+        <Story />
+      </div>
+    ),
+  ],
 } as Meta;
 
 const Template: Story<ProgressProps> = (args) => <Progress {...args} />;
 
-export const DefaultProgress = Template.bind({});
-DefaultProgress.storyName = 'Default';
-DefaultProgress.args = {
+export const Default = Template.bind({});
+Default.args = {
+  progress: 45,
+};
+
+export const Sizes = (): JSX.Element => (
+  <>
+    <div className="text-base font-medium dark:text-white">Small</div>
+    <Progress progress={45} size="sm" color="dark" />
+    <div className="mt-3 text-base font-medium dark:text-white">Default</div>
+    <Progress progress={45} size="md" color="dark" />
+    <div className="mt-3 text-lg font-medium dark:text-white">Large</div>
+    <Progress progress={45} size="lg" color="dark" />
+    <div className="mt-3 text-lg font-medium dark:text-white">Extra Large</div>
+    <Progress progress={45} size="xl" color="dark" />
+  </>
+);
+
+export const Colors = (): JSX.Element => (
+  <>
+    <div className="text-base font-medium">Dark</div>
+    <Progress progress={45} color="dark" />
+    <div className="mt-3 text-base font-medium text-blue-700">Blue</div>
+    <Progress progress={45} color="blue" />
+    <div className="mt-3 text-base font-medium text-red-700">Red</div>
+    <Progress progress={45} color="red" />
+    <div className="mt-3 text-base font-medium text-green-700">Green</div>
+    <Progress progress={45} color="green" />
+    <div className="mt-3 text-base font-medium text-yellow-700">Yellow</div>
+    <Progress progress={45} color="yellow" />
+    <div className="mt-3 text-base font-medium text-indigo-700">Indigo</div>
+    <Progress progress={45} color="indigo" />
+    <div className="mt-3 text-base font-medium text-purple-700">Purple</div>
+    <Progress progress={45} color="purple" />
+  </>
+);
+
+export const WithLabelInside = Template.bind({});
+WithLabelInside.storyName = 'With label inside';
+WithLabelInside.args = {
+  label: 'Flowbite',
+  progress: 45,
+};
+
+export const WithLabelOutside = Template.bind({});
+WithLabelOutside.storyName = 'With label outside';
+WithLabelOutside.args = {
+  label: 'Flowbite',
+  labelPosition: 'outside',
+  labelProgress: true,
   progress: 45,
 };
