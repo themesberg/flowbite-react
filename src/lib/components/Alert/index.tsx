ad3bfcd1b3d32218403dd300b01f4a4d683ba4a9
Conner Davis <conner@tulup.io>
2022-05-21 02:16:40 -0700

    fix(a11y): Remove redundant a11y label from `Alert` close button

--- src/lib/components/Alert/index.tsx
+++ src/lib/components/Alert/index.tsx
@@ -39,14 +39,13 @@ export const Alert: FC<AlertProps> = ({
         <div>{children}</div>
         {typeof onDismiss === 'function' && (
           <button
-            aria-label="Close"
             className={classNames(theme.closeButton.base, theme.closeButton.color[color])}
             data-testid="alert-dismiss"
             onClick={onDismiss}
             type="button"
           >
             <span className="sr-only">Close</span>
-            <HiX className="h-5 w-5" />
+            <HiX aria-hidden="true" className="h-5 w-5" />
           </button>
         )}
       </div>
