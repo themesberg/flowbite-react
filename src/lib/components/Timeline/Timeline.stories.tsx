5f17389cd21e3f8fdeaaea27c5f694cf7fd77878
Joey Chrysanthopoulos <joeyachrys@outlook.com>
2022-05-24 12:07:00 -0500

    fix: replace parent with fragment

--- src/lib/components/Timeline/Timeline.stories.tsx
+++ src/lib/components/Timeline/Timeline.stories.tsx
@@ -1,7 +1,5 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
-import { HiArrowNarrowRight } from 'react-icons/hi';
 import { Timeline, TimelineProps } from '.';
-import { Button } from '../Button';
 
 export default {
   title: 'Components/Timeline',
@@ -14,11 +12,9 @@ export const DefaultTimeline = Template.bind({});
 DefaultTimeline.storyName = 'Default Timeline';
 DefaultTimeline.args = {
   children: (
-    <Timeline>
+    <>
       <Timeline.Item>
-        <Timeline.Point>
-          <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700" />
-        </Timeline.Point>
+        <Timeline.Point />
         <Timeline.Content>
           <Timeline.Time>February 2022</Timeline.Time>
           <Timeline.Title>Application UI code in Tailwind CSS</Timeline.Title>
@@ -26,16 +22,10 @@ DefaultTimeline.args = {
             Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order
             E-commerce & Marketing pages.
           </Timeline.Body>
-          <Button color="alternative">
-            Learn More
-            <HiArrowNarrowRight className="ml-2 h-3 w-3" />
-          </Button>
         </Timeline.Content>
       </Timeline.Item>
       <Timeline.Item>
-        <Timeline.Point>
-          <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700" />
-        </Timeline.Point>
+        <Timeline.Point />
         <Timeline.Content>
           <Timeline.Time>March 2022</Timeline.Time>
           <Timeline.Title>Marketing UI design in Figma</Timeline.Title>
@@ -46,9 +36,7 @@ DefaultTimeline.args = {
         </Timeline.Content>
       </Timeline.Item>
       <Timeline.Item>
-        <Timeline.Point>
-          <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700" />
-        </Timeline.Point>
+        <Timeline.Point />
         <Timeline.Content>
           <Timeline.Time>April 2022</Timeline.Time>
           <Timeline.Title>E-Commerce UI code in Tailwind CSS</Timeline.Title>
@@ -57,6 +45,6 @@ DefaultTimeline.args = {
           </Timeline.Body>
         </Timeline.Content>
       </Timeline.Item>
-    </Timeline>
+    </>
   ),
 };
