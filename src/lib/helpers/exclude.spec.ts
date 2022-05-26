c9117878b0d2c42ba509582d2733a22609076a24
Conner Davis <conner@tulup.io>
2022-05-25 17:28:19 -0700

    feature(helper): Add helper to exclude key from an object
    Additionally, provide a shortcut helper for the
    most common existing case, which is to remove
    `className` from a React component.

---
+++ src/lib/helpers/exclude.spec.ts
@@ -0,0 +1,34 @@
+import exclude from './exclude';
+
+describe('Helpers / Exclude (delete key from object)', () => {
+  describe('Given object that contains targeted key', () => {
+    it('should return input object without that key', () => {
+      const input = {
+        a: 1,
+        b: 2,
+        c: 3,
+      };
+      const output = exclude({ key: 'a', source: input });
+
+      expect(output).toEqual({
+        b: 2,
+        c: 3,
+      });
+    });
+  });
+
+  describe('Given object that does not contain target key', () => {
+    it('should return input object', () => {
+      const input = {
+        b: 2,
+        c: 3,
+      };
+      const output = exclude({ key: 'a', source: input });
+
+      expect(output).toEqual({
+        b: 2,
+        c: 3,
+      });
+    });
+  });
+});
