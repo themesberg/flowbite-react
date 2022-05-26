459edc8c9d7927aa5f89b7d3287d932f64c7ce7c
Conner Davis <conner@tulup.io>
2022-05-25 23:56:50 -0700

    feature(component): Allow props on `Breadcrumb`, excluding `className`

--- src/lib/components/Breadcrumb/BreadcrumbItem.tsx
+++ src/lib/components/Breadcrumb/BreadcrumbItem.tsx
@@ -1,25 +1,22 @@
 import { FC, ComponentProps, PropsWithChildren } from 'react';
 import { HiOutlineChevronRight } from 'react-icons/hi';
+import { excludeClassName } from '../../helpers/exclude';
 import { useTheme } from '../Flowbite/ThemeContext';
 
-export interface BreadcrumbItemProps extends ComponentProps<'li'> {
+export interface BreadcrumbItemProps extends PropsWithChildren<ComponentProps<'li'>> {
   href?: string;
   icon?: FC<ComponentProps<'svg'>>;
 }
 
-const BreadcrumbItem: FC<PropsWithChildren<BreadcrumbItemProps>> = ({
-  children,
-  href,
-  icon: Icon,
-  ...rest
-}): JSX.Element => {
+const BreadcrumbItem: FC<BreadcrumbItemProps> = ({ children, href, icon: Icon, ...props }): JSX.Element => {
   const isLink = typeof href !== 'undefined';
+  const theirProps = excludeClassName(props);
   const theme = useTheme().theme.breadcrumb.item;
 
   const Component = isLink ? 'a' : 'span';
 
   return (
-    <li className={theme.base} {...rest}>
+    <li className={theme.base} {...theirProps}>
       <HiOutlineChevronRight aria-hidden className={theme.chevron} data-testid="flowbite-breadcrumb-separator" />
       <Component className={theme.href[isLink ? 'on' : 'off']} data-testid="flowbite-breadcrumb-item" href={href}>
         {Icon && <Icon aria-hidden className={theme.icon} />}
--- src/lib/components/Breadcrumb/index.tsx
+++ src/lib/components/Breadcrumb/index.tsx
@@ -1,12 +1,14 @@
 import { ComponentProps, FC } from 'react';
+import { excludeClassName } from '../../helpers/exclude';
 import { useTheme } from '../Flowbite/ThemeContext';
 import BreadcrumbItem from './BreadcrumbItem';
 
-const BreadcrumbComponent: FC<ComponentProps<'nav'>> = ({ children, ...rest }): JSX.Element => {
+const BreadcrumbComponent: FC<ComponentProps<'nav'>> = ({ children, ...props }): JSX.Element => {
+  const theirProps = excludeClassName(props);
   const theme = useTheme().theme.breadcrumb;
 
   return (
-    <nav aria-label="Breadcrumb" {...rest}>
+    <nav aria-label="Breadcrumb" {...theirProps}>
       <ol className={theme.list}>{children}</ol>
     </nav>
   );
