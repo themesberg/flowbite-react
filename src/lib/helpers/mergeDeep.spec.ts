25ddd51848a2c555aae799f505937796d638d4cc
Conner Davis <conner@tulup.io>
2022-05-27 03:01:44 -0700

    test(helper): Update `mergeDeep` unit tests for real use cases
    Now that we are actually using `mergeDeep` for
    `<Flowbite theme={..}>`, I wanted to update this
    spec to more accurately reflect the use cases we
    need to solve.

--- src/lib/helpers/mergeDeep.spec.ts
+++ src/lib/helpers/mergeDeep.spec.ts
@@ -1,131 +1,82 @@
 import { mergeDeep } from './mergeDeep';
 
 describe('Helper / mergeDeep (Deeply merge two objects)', () => {
-  describe('given two unknown values', () => {
-    describe('both containing an array with same key', () => {
-      it('should only keep elements in latter object', () => {
-        const combineMe = {
-          a: {
-            fruit: ['apples', 'bananas'],
-          },
-          b: 2,
-          c: 3,
-        };
-        const withThis = {
-          a: {
-            fruit: ['lemons'],
-          },
-          d: 4,
-          e: 5,
-        };
-
-        expect(mergeDeep(combineMe, withThis)).toEqual({
-          a: {
-            fruit: ['lemons'],
-          },
-          b: 2,
-          c: 3,
-          d: 4,
-          e: 5,
-        });
-      });
-    });
-
-    describe('containing different objects', () => {
-      it('should combine them', () => {
-        const combineMe = {
-          a: {
-            fruit: 'apple',
-          },
-          b: 2,
-        };
-        const withThis = {
-          b: 2,
-          c: {
-            vegetable: 'carrot',
-          },
-          d: 4,
-          e: 5,
-        };
-
-        expect(mergeDeep(combineMe, withThis)).toEqual({
-          a: { fruit: 'apple' },
-          b: 2,
-          c: { vegetable: 'carrot' },
-          d: 4,
-          e: 5,
-        });
-      });
-    });
-
-    describe('with identical items', () => {
-      it("shouldn't duplicate them", () => {
-        const combineMe = {
-          a: 1,
-          b: 2,
-          c: 3,
-        };
-        const withThis = {
-          b: 2,
-          d: 4,
-          e: 5,
-        };
-
-        expect(mergeDeep(combineMe, withThis)).toEqual({
-          a: 1,
-          b: 2,
-          c: 3,
-          d: 4,
-          e: 5,
-        });
-      });
-    });
-
-    it('should combine all of their unique properties', () => {
+  describe('given two objects', () => {
+    it('should combine unique shallow properties', () => {
       const combineMe = {
-        a: 1,
-        b: 2,
-        c: 3,
+        a: 'a',
+        b: 'b',
+        c: 'c',
       };
       const withThis = {
-        d: 4,
-        e: 5,
+        d: 'd',
+        e: 'e',
       };
 
       expect(mergeDeep(combineMe, withThis)).toEqual({
-        a: 1,
-        b: 2,
-        c: 3,
-        d: 4,
-        e: 5,
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd',
+        e: 'e',
       });
     });
 
-    it('should combine all of their unique nested properties', () => {
-      const combineMe = {
-        a: {
-          fruit: 'apple',
-        },
-        b: 2,
-        c: 3,
-      };
-      const withThis = {
-        a: {
-          vegetable: 'carrot',
-        },
-        d: 4,
-        e: 5,
-      };
+    describe('with identical key', () => {
+      it("should use overriding object's value", () => {
+        const combineMe = {
+          base: 'base',
+          content: {
+            base: 'content',
+          },
+          flush: {
+            off: 'no-flush',
+            on: 'flush',
+          },
+        };
+        const withThis = {
+          base: 'new-base',
+          content: {
+            base: 'new-content',
+          },
+          flush: {
+            off: 'new-no-flush',
+            on: 'new-flush',
+          },
+        };
 
-      expect(mergeDeep(combineMe, withThis)).toEqual({
-        a: {
-          fruit: 'apple',
-          vegetable: 'carrot',
-        },
-        b: 2,
-        c: 3,
-        d: 4,
-        e: 5,
+        expect(mergeDeep(combineMe, withThis)).toEqual({
+          base: 'new-base',
+          content: {
+            base: 'new-content',
+          },
+          flush: {
+            off: 'new-no-flush',
+            on: 'new-flush',
+          },
+        });
+      });
+
+      describe('that is an object', () => {
+        it('should combine keys from both objects', () => {
+          const combineMe = {
+            content: {
+              base: 'base',
+            },
+          };
+          const withThis = {
+            content: {
+              primary: 'primary',
+            },
+          };
+
+          expect(mergeDeep(combineMe, withThis)).toEqual({
+            content: {
+              base: 'base',
+              primary: 'primary',
+            },
+          });
+        });
       });
     });
   });
