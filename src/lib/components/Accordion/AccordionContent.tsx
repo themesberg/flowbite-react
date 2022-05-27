6a0fee2480208ef13eb60f88e43e0e9cf09ba711
Conner Davis <conner@tulup.io>
2022-05-26 18:18:37 -0700

    refactor(component): Simplify `Accordion.Content` rendering
    Use the HTML `hidden` attribute to toggle the
    element, rather than returning `null` to React.

--- src/lib/components/Accordion/AccordionContent.tsx
+++ src/lib/components/Accordion/AccordionContent.tsx
@@ -4,15 +4,15 @@ import { useTheme } from '../Flowbite/ThemeContext';
 
 import { useAccordionContext } from './AccordionPanelContext';
 
-export const AccordionContent: FC<ComponentProps<'div'>> = ({ children, ...props }): JSX.Element | null => {
+export const AccordionContent: FC<ComponentProps<'div'>> = ({ children, ...props }): JSX.Element => {
   const theirProps = excludeClassName(props);
 
   const { isOpen } = useAccordionContext();
   const theme = useTheme().theme.accordion.content;
 
-  return isOpen ? (
-    <div className={theme.base} {...theirProps}>
+  return (
+    <div className={theme.base} data-testid="flowbite-accordion-content" hidden={!isOpen} {...theirProps}>
       {children}
     </div>
-  ) : null;
+  );
 };
