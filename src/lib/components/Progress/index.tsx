23a3f1483c874b05ab15dde69608ab1e21a893b4
mouracamila <camilapaleo@gmail.com>
2022-05-31 17:44:17 +0200

    feat(component):Rewrite Progress to use FlowbiteTheme, close #141

--- src/lib/components/Progress/index.tsx
+++ src/lib/components/Progress/index.tsx
@@ -1,39 +1,25 @@
 import classNames from 'classnames';
-import type { ComponentProps, FC, PropsWithChildren } from 'react';
 import { useId } from 'react';
+import { ComponentProps, FC, PropsWithChildren } from 'react';
+import { FlowbiteColors, FlowbiteSizes } from '../Flowbite/FlowbiteTheme';
 
-type Color = 'dark' | 'blue' | 'red' | 'green' | 'yellow' | 'indigo' | 'purple';
-type Size = 'sm' | 'md' | 'lg' | 'xl';
+import { useTheme } from '../Flowbite/ThemeContext';
+export interface ProgressProps extends PropsWithChildren<Omit<ComponentProps<'div'>, 'className'>> {
+  size?: keyof ProgressSizes;
+  label?: string;
+  labelPosition?: 'inside' | 'outside';
+  labelProgress?: boolean;
+  progress: number;
+}
+export interface ProgressColor
+  extends Pick<FlowbiteColors, 'dark' | 'blue' | 'red' | 'green' | 'yellow' | 'indigo' | 'purple'> {
+  [key: string]: string;
+}
+export interface ProgressSizes extends Pick<FlowbiteSizes, 'sm' | 'md' | 'lg' | 'xl'> {
+  [key: string]: string;
+}
 
-export type ProgressProps = PropsWithChildren<
-  ComponentProps<'div'> & {
-    className?: string;
-    color?: Color;
-    label?: string;
-    labelPosition?: 'inside' | 'outside';
-    labelProgress?: boolean;
-    progress: number;
-    size?: Size;
-  }
->;
-
-const colorClasses: Record<Color, string> = {
-  dark: 'bg-gray-600 dark:bg-gray-300',
-  blue: 'bg-blue-600 dark:bg-blue-600',
-  red: 'bg-red-600 dark:bg-red-500',
-  green: 'bg-green-600 dark:bg-green-500',
-  yellow: 'bg-yellow-400 dark:bg-yellow-400',
-  indigo: 'bg-indigo-600 dark:bg-indigo-500',
-  purple: 'bg-purple-600 dark:bg-purple-500',
-};
-const sizeClasses: Record<Size, string> = {
-  sm: 'h-1.5',
-  md: 'h-2.5',
-  lg: 'h-4',
-  xl: 'h-6',
-};
 export const Progress: FC<ProgressProps> = ({
-  className,
   color = 'blue',
   label = '',
   labelPosition = 'inside',
@@ -41,8 +27,9 @@ export const Progress: FC<ProgressProps> = ({
   progress,
   size = 'md',
   ...props
-}) => {
+}): JSX.Element => {
   const id = useId();
+  const theme = useTheme().theme.progress;
 
   return (
     <>
@@ -51,21 +38,14 @@ export const Progress: FC<ProgressProps> = ({
       </span>
       <div aria-labelledby={`${id}-flowbite-progress`} aria-valuenow={progress} role="progressbar" {...props}>
         {label && labelPosition === 'outside' && (
-          <div className="mb-1 flex justify-between font-medium dark:text-white">
+          <div className={theme.label}>
             <span>{label}</span>
             {labelProgress && <span>{progress}%</span>}
           </div>
         )}
-        <div
-          className={classNames('w-full overflow-hidden rounded-full bg-gray-200 dark:bg-gray-700', sizeClasses[size])}
-        >
+        <div className={classNames(theme.base, theme.size[size])}>
           <div
-            className={classNames(
-              'flex items-center justify-center rounded-full text-center font-medium leading-none text-blue-100',
-              colorClasses[color],
-              sizeClasses[size],
-              className,
-            )}
+            className={classNames(theme.bar, theme.color[color], theme.size[size])}
             style={{ width: `${progress}%` }}
           >
             {label && labelPosition === 'inside' && label}
--- src/lib/theme/default.ts
+++ src/lib/theme/default.ts
@@ -485,6 +485,26 @@ export default {
         off: 'self-center whitespace-nowrap text-xl font-semibold dark:text-white',
       },
       img: 'mr-3 h-6 sm:h-7',
+    }
+  },
+  progress: {
+    base: 'w-full overflow-hidden rounded-full bg-gray-200 dark:bg-gray-700',
+    label: 'mb-1 flex justify-between font-medium dark:text-white',
+    bar: 'flex items-center justify-center rounded-full text-center font-medium leading-none text-blue-100',
+    color: {
+      dark: 'bg-gray-600 dark:bg-gray-300',
+      blue: 'bg-blue-600',
+      red: 'bg-red-600 dark:bg-red-500',
+      green: 'bg-green-600 dark:bg-green-500',
+      yellow: 'bg-yellow-400',
+      indigo: 'bg-indigo-600 dark:bg-indigo-500',
+      purple: 'bg-purple-600 dark:bg-purple-500',
+    },
+    size: {
+      sm: 'h-1.5',
+      md: 'h-2.5',
+      lg: 'h-4',
+      xl: 'h-6',
     },
   },
   spinner: {
