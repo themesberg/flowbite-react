bc8141a23c8c08fc2a5073386b60fab45d0586d9
Conner Davis <conner@tulup.io>
2022-05-31 00:43:36 -0700

    feature(component): Rewrite `Carousel`s to use themes, resolves #168, #132

--- src/lib/components/Carousel/index.tsx
+++ src/lib/components/Carousel/index.tsx
@@ -16,39 +16,42 @@ import classNames from 'classnames';
 import { HiOutlineChevronLeft, HiOutlineChevronRight } from 'react-icons/hi';
 import ScrollContainer from 'react-indiana-drag-scroll';
 import windowExists from '../../helpers/window-exists';
+import { useTheme } from '../Flowbite/ThemeContext';
+import { excludeClassName } from '../../helpers/exclude';
 
-export type CarouselProps = PropsWithChildren<{
-  slide?: boolean;
-  slideInterval?: number;
+export interface CarouselProps extends PropsWithChildren<ComponentProps<'div'>> {
   indicators?: boolean;
   leftControl?: ReactNode;
   rightControl?: ReactNode;
-}>;
+  slide?: boolean;
+  slideInterval?: number;
+}
 
 export const Carousel: FC<CarouselProps> = ({
   children,
-  slide = true,
-  slideInterval,
   indicators = true,
   leftControl,
   rightControl,
-}) => {
+  slide = true,
+  slideInterval,
+  ...props
+}): JSX.Element => {
+  const isDeviceMobile = windowExists() && navigator.userAgent.indexOf('IEMobile') !== -1;
+  const theirProps = excludeClassName(props);
+
+  const carouselContainer = useRef<HTMLDivElement>(null);
   const [activeItem, setActiveItem] = useState(0);
   const [isDragging, setIsDragging] = useState(false);
-  const carouselContainer = useRef<HTMLDivElement>(null);
-  const isDeviceMobile = windowExists() && navigator.userAgent.indexOf('IEMobile') !== -1;
+  const theme = useTheme().theme.carousel;
 
   const items = useMemo(
     () =>
       Children.map(children as ReactElement<ComponentProps<'img'>>[], (child: ReactElement<ComponentProps<'img'>>) =>
         cloneElement(child, {
-          className: classNames(
-            child.props.className,
-            'absolute top-1/2 left-1/2 block w-full -translate-x-1/2 -translate-y-1/2',
-          ),
+          className: theme.item.base,
         }),
       ),
-    [children],
+    [children, theme.item.base],
   );
 
   const navigateTo = useCallback(
@@ -79,47 +82,49 @@ export const Carousel: FC<CarouselProps> = ({
   const handleDragging = (dragging: boolean) => () => setIsDragging(dragging);
 
   return (
-    <div className="relative w-full" data-testid="carousel">
+    <div className={theme.base} data-testid="carousel" {...theirProps}>
       <ScrollContainer
         className={classNames(
-          'flex h-56 snap-mandatory overflow-y-hidden overflow-x-scroll scroll-smooth rounded-lg sm:h-64 xl:h-80 2xl:h-96',
-          { 'snap-x': isDeviceMobile || !isDragging },
+          theme.scrollContainer.base,
+          (isDeviceMobile || !isDragging) && theme.scrollContainer.snap,
         )}
         draggingClassName="cursor-grab"
-        onStartScroll={handleDragging(true)}
-        onEndScroll={handleDragging(false)}
         innerRef={carouselContainer}
+        onEndScroll={handleDragging(false)}
+        onStartScroll={handleDragging(true)}
         vertical={false}
       >
-        {items?.map((item, index) => (
-          <div
-            key={index}
-            className="w-full flex-shrink-0 transform cursor-grab snap-center"
-            data-active={activeItem === index}
-            data-testid="carousel-item"
-          >
-            {item}
-          </div>
-        ))}
-      </ScrollContainer>
-
-      {indicators && (
-        <div className="absolute bottom-5 left-1/2 flex -translate-x-1/2 space-x-3">
-          {items.map((_, index) => (
-            <button
+        {items?.map(
+          (item, index): JSX.Element => (
+            <div
               key={index}
-              className={classNames('h-3 w-3 rounded-full', {
-                'bg-white dark:bg-gray-800': index === activeItem,
-                'bg-white/50 hover:bg-white dark:bg-gray-800/50 dark:hover:bg-gray-800': index !== activeItem,
-              })}
-              onClick={navigateTo(index)}
-              data-testid="carousel-indicator"
-            />
-          ))}
+              className={theme.item.wrapper}
+              data-active={activeItem === index}
+              data-testid="carousel-item"
+            >
+              {item}
+            </div>
+          ),
+        )}
+      </ScrollContainer>
+      {indicators && (
+        <div className={theme.indicators.wrapper}>
+          {items.map(
+            (_, index): JSX.Element => (
+              <button
+                key={index}
+                className={classNames(
+                  theme.indicators.base,
+                  theme.indicators.active[index === activeItem ? 'on' : 'off'],
+                )}
+                onClick={navigateTo(index)}
+                data-testid="carousel-indicator"
+              />
+            ),
+          )}
         </div>
       )}
-
-      <div className="absolute top-0 left-0 flex h-full items-center justify-center px-4 focus:outline-none">
+      <div className={theme.leftControl}>
         <button
           className="group"
           data-testid="carousel-left-control"
@@ -129,7 +134,7 @@ export const Carousel: FC<CarouselProps> = ({
           {leftControl ? leftControl : <DefaultLeftControl />}
         </button>
       </div>
-      <div className="absolute top-0 right-0 flex h-full items-center justify-center px-4 focus:outline-none">
+      <div className={theme.rightControl}>
         <button
           className="group"
           data-testid="carousel-right-control"
