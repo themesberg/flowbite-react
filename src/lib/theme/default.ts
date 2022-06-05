80b7e2a364a557f54b94b512fe41c2e4839bd9bc
Ricardo Lüders <ricardo@luders.com.br>
2022-06-05 19:48:16 +0200

    feat(component): theme support to pagination (#195)
    * feat(component): theme support to pagination
    
    * fix(review): adjust based on feedback
    
    * fix(a11y): missing aria-hidden

--- src/lib/theme/default.ts
+++ src/lib/theme/default.ts
@@ -275,6 +275,32 @@ export default {
       },
     },
   },
+  pagination: {
+    base: '',
+    layout: {
+      table: {
+        base: 'text-sm text-gray-700 dark:text-gray-400',
+        span: 'font-semibold text-gray-900 dark:text-white',
+      },
+    },
+    pages: {
+      base: 'xs:mt-0 mt-2 inline-flex items-center -space-x-px',
+      showIcon: 'inline-flex',
+      previous: {
+        base: 'ml-0 rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white',
+        icon: 'h-5 w-5',
+      },
+      next: {
+        base: 'rounded-r-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white',
+        icon: 'h-5 w-5',
+      },
+      selector: {
+        base: 'w-12 border border-gray-300 bg-white py-2 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white',
+        active:
+          'bg-blue-50 text-blue-600 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white',
+      },
+    },
+  },
   spinner: {
     base: 'inline animate-spin text-gray-200',
     color: {
