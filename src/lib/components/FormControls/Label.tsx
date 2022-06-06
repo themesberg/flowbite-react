f201279af8b7886c8604efa9c761f0ae6a6f9f92
Conner Davis <conner@tulup.io>
2022-06-06 15:17:13 -0700

    feat(component): Allow `Label` as self-closing tag, resolves #173
    Instead of relying on`children`, allow the use of
    prop `value`.

--- src/lib/components/FormControls/Label.tsx
+++ src/lib/components/FormControls/Label.tsx
@@ -1,11 +1,12 @@
-import type { ComponentProps, FC } from 'react';
 import classNames from 'classnames';
+import type { ComponentProps, FC, PropsWithChildren } from 'react';
 
 type Color = 'gray' | 'green' | 'red';
 
-export type LabelProps = ComponentProps<'label'> & {
+export interface LabelProps extends PropsWithChildren<ComponentProps<'label'>> {
   color?: Color;
-};
+  value?: string;
+}
 
 const colorClasses: Record<Color, string> = {
   gray: 'text-gray-900 dark:text-gray-300',
@@ -13,8 +14,10 @@ const colorClasses: Record<Color, string> = {
   red: 'text-red-700 dark:text-red-500',
 };
 
-export const Label: FC<LabelProps> = ({ children, color = 'gray', className, ...props }) => (
-  <label className={classNames('text-sm font-medium', colorClasses[color], className)} {...props}>
-    {children}
-  </label>
-);
+export const Label: FC<LabelProps> = ({ children, color = 'gray', className, value, ...props }): JSX.Element => {
+  return (
+    <label className={classNames('text-sm font-medium', colorClasses[color], className)} {...props}>
+      {value ?? children ?? ''}
+    </label>
+  );
+};
