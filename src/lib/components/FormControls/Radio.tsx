9e69efd2ecf5f8b6b8313c3527f2e21c93e6fe6b
Ricardo Lüders <rluders@redhat.com>
2022-06-15 09:41:02 +0200

    fix(radio): missing excludeClassname

--- src/lib/components/FormControls/Radio.tsx
+++ src/lib/components/FormControls/Radio.tsx
@@ -1,9 +1,11 @@
 import type { ComponentProps, FC } from 'react';
+import { excludeClassName } from '../../helpers/exclude';
 import { useTheme } from '../Flowbite/ThemeContext';
 
 export type RadioProps = Omit<ComponentProps<'input'>, 'type' | 'className'>;
 
 export const Radio: FC<RadioProps> = (props) => {
   const theme = useTheme().theme.formControls.radio;
-  return <input className={theme.base} type="radio" {...props} />;
+  const theirProps = excludeClassName(props);
+  return <input className={theme.base} type="radio" {...theirProps} />;
 };
