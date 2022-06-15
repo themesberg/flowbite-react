2848d9b045eed0d7855c7cfbfb96b85a2c36d099
Ricardo Lüders <rluders@redhat.com>
2022-06-15 09:48:58 +0200

    fix(checkbox): missing excludeClassname

--- src/lib/components/FormControls/Checkbox.tsx
+++ src/lib/components/FormControls/Checkbox.tsx
@@ -1,9 +1,11 @@
 import type { ComponentProps, FC } from 'react';
+import { excludeClassName } from '../../helpers/exclude';
 import { useTheme } from '../Flowbite/ThemeContext';
 
 export type CheckboxProps = Omit<ComponentProps<'input'>, 'type' | 'className'>;
 
 export const Checkbox: FC<CheckboxProps> = (props) => {
   const theme = useTheme().theme.formControls.checkbox;
-  return <input className={theme.base} type="checkbox" {...props} />;
+  const theirProps = excludeClassName(props);
+  return <input className={theme.base} type="checkbox" {...theirProps} />;
 };
