cb4e8c8a3306a8c6ebf2bb059f0c836c86aff917
Ricardo Lüders <ricardo@luders.com.br>
2022-06-30 15:20:30 +0200

    refactor(dropdown): adds theme support to dropdown (#253)

--- src/lib/theme/default.ts
+++ src/lib/theme/default.ts
@@ -830,4 +830,30 @@ export default {
       placement: '-4px',
     },
   },
+  dropdown: {
+    floating: {
+      target: 'w-fit',
+      base: 'absolute inline-block rounded-lg py-2 px-3 text-sm font-medium shadow-sm',
+      animation: 'transition-opacity',
+      hidden: 'invisible opacity-0',
+      style: {
+        dark: 'bg-gray-900 text-white dark:bg-gray-700',
+        light: 'border border-gray-200 bg-white text-gray-900',
+        auto: 'border border-gray-200 bg-white text-gray-900 dark:border-none dark:bg-gray-700 dark:text-white',
+      },
+      content: 'relative z-20',
+      arrow: {
+        base: 'absolute z-10 h-2 w-2 rotate-45',
+        style: {
+          dark: 'bg-gray-900 dark:bg-gray-700',
+          light: 'bg-white',
+          auto: 'bg-white dark:bg-gray-700',
+        },
+        placement: '-4px',
+      },
+    },
+    arrowIcon: 'ml-2 h-4 w-4',
+    inlineWrapper: 'flex items-center',
+    content: 'py-1',
+  },
 };
