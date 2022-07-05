31aebcf0d0fa19929c68a6c6d74198ffd87ecbc2
Gonzalo Juarez <gonzalojuarezte@gmail.com>
2022-07-05 09:55:18 +0200

    Added ref forwarding for Radio component

--- src/lib/components/FormControls/Radio.tsx
+++ src/lib/components/FormControls/Radio.tsx
@@ -1,11 +1,12 @@
-import type { ComponentProps, FC } from 'react';
+import type { ComponentProps } from 'react';
+import { forwardRef } from 'react';
 import { excludeClassName } from '../../helpers/exclude';
 import { useTheme } from '../Flowbite/ThemeContext';
 
-export type RadioProps = Omit<ComponentProps<'input'>, 'type' | 'className'>;
+export type RadioProps = Omit<ComponentProps<'input'>, 'type' | 'className' | 'ref'>;
 
-export const Radio: FC<RadioProps> = (props) => {
+export const Radio = forwardRef<HTMLInputElement, RadioProps> ((props, ref) => {
   const theme = useTheme().theme.formControls.radio;
   const theirProps = excludeClassName(props);
-  return <input className={theme.base} type="radio" {...theirProps} />;
-};
+  return <input ref={ref} className={theme.base} type="radio" {...theirProps} />;
+});
