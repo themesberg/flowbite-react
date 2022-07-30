41faa35fa2573beb2bef25fff7829f9ea198fe57
Conner Davis <conner@tulup.io>
2022-07-30 16:02:47 -0700

    fix(theme): add `z-index` to `Dropdown` floating component
    Generally, the `Dropdown` theme doesn't seem to reflect what is currently on the `flowbite` docs
    (https://flowbite.com/docs/components/dropdowns/), so I made changes to reflect that.
    
    resolve #308

--- src/lib/theme/default.ts
+++ src/lib/theme/default.ts
@@ -271,7 +271,7 @@ export default {
   dropdown: {
     floating: {
       target: 'w-fit',
-      base: 'z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700',
+      base: 'z-10 w-fit rounded divide-y divide-gray-100 shadow',
       animation: 'transition-opacity',
       hidden: 'invisible opacity-0',
       style: {
@@ -279,7 +279,7 @@ export default {
         light: 'border border-gray-200 bg-white text-gray-900',
         auto: 'border border-gray-200 bg-white text-gray-900 dark:border-none dark:bg-gray-700 dark:text-white',
       },
-      content: 'relative z-20',
+      content: 'py-1 text-sm text-gray-700 dark:text-gray-200',
       arrow: {
         base: 'absolute z-10 h-2 w-2 rotate-45',
         style: {
