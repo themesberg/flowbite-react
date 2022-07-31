c20298aec5f0c70f36af7d7e9146aa655f63bcec
Conner Davis <conner@tulup.io>
2022-07-31 12:53:13 -0700

    docs(storybook): add color radio options to `Button` storybook
    We can use the default theme to automatically pick up on which colors the `Button` supports by
    default.
    
    resolve #272

--- src/lib/components/Button/Button.stories.tsx
+++ src/lib/components/Button/Button.stories.tsx
@@ -1,10 +1,17 @@
 import type { Meta, Story } from '@storybook/react/types-6-0';
 import type { ButtonProps } from '.';
 import { Button } from '.';
+import theme from '../../theme/default';
 
 export default {
   title: 'Components/Button',
   component: Button,
+  argTypes: {
+    color: {
+      options: Object.keys(theme.button.color),
+      control: { type: 'radio' },
+    },
+  },
 } as Meta;
 
 const Template: Story<ButtonProps> = (args) => <Button {...args} />;
