218571ce5c50576ed680a9950f49723c15a92d6c
Drex <90635055+Drex72@users.noreply.github.com>
2022-10-25 15:43:11 +0200

    Updated Sidebar Collapse on Small Screens

--- src/lib/components/Sidebar/Sidebar.tsx
+++ src/lib/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,7 @@
+
 import classNames from 'classnames';
-import { ComponentProps, FC, PropsWithChildren } from 'react';
+import { ComponentProps, FC, PropsWithChildren, useEffect, useState } from 'react';
+import { excludeClassName } from '../../helpers/exclude';
 import { useTheme } from '../Flowbite/ThemeContext';
 import SidebarCollapse from './SidebarCollapse';
 import { SidebarContext } from './SidebarContext';
@@ -9,17 +11,40 @@ import SidebarItemGroup from './SidebarItemGroup';
 import SidebarItems from './SidebarItems';
 import SidebarLogo from './SidebarLogo';
 
-export interface SidebarProps extends PropsWithChildren<ComponentProps<'div'>> {
+export interface SidebarProps extends PropsWithChildren<Omit<ComponentProps<'div'>, 'className'>> {
   collapseBehavior?: 'collapse' | 'hide';
-  collapsed?: boolean;
+  collapsed?: boolean ;
 }
 
+let isCollapsed: boolean = false;
 const SidebarComponent: FC<SidebarProps> = ({
   children,
   collapseBehavior = 'collapse',
-  collapsed: isCollapsed = false,
+  collapsed,
   ...props
 }): JSX.Element => {
+  const theirProps = excludeClassName(props);
+  const [screenSize, setScreenSize] = useState<number>(0);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize! <= 800) {
+      isCollapsed = true;
+    } else {
+      isCollapsed = false;
+    }
+  }, [screenSize]);
+
+  useEffect(() => {
+    isCollapsed = collapsed;
+  }, [collapsed]);
+
   const theme = useTheme().theme.sidebar;
 
   return (
@@ -28,7 +53,7 @@ const SidebarComponent: FC<SidebarProps> = ({
         aria-label="Sidebar"
         className={classNames(theme.base, theme.collapsed[isCollapsed ? 'on' : 'off'])}
         hidden={isCollapsed && collapseBehavior === 'hide'}
-        {...props}
+        {...theirProps}
       >
         <div className={theme.inner}>{children}</div>
       </aside>
