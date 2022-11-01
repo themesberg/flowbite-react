0e4ae62b2b93561c4857bea63df45fe9170f546e
Yannick <114925077+myasteiner@users.noreply.github.com>
2022-11-01 10:08:21 +0100

    feat(breadcrumbitem): add forwardRef to breadcrumb item (#421)
    Added forwardRef to breadcrumb item to support Next link

--- src/lib/components/Breadcrumb/BreadcrumbItem.tsx
+++ src/lib/components/Breadcrumb/BreadcrumbItem.tsx
@@ -1,29 +1,36 @@
 import classNames from 'classnames';
-import type { ComponentProps, FC, PropsWithChildren } from 'react';
+import { ComponentProps, FC, forwardRef, PropsWithChildren } from 'react';
 import { HiOutlineChevronRight } from 'react-icons/hi';
 import { useTheme } from '../Flowbite/ThemeContext';
 
-export interface BreadcrumbItemProps extends PropsWithChildren<ComponentProps<'li'>> {
+export interface BreadcrumbItemProps extends PropsWithChildren<Omit<ComponentProps<'li'>, 'ref'>> {
   href?: string;
   icon?: FC<ComponentProps<'svg'>>;
 }
 
-const BreadcrumbItem: FC<BreadcrumbItemProps> = ({ children, className, href, icon: Icon, ...props }): JSX.Element => {
-  const isLink = typeof href !== 'undefined';
-  const theme = useTheme().theme.breadcrumb.item;
+const BreadcrumbItem = forwardRef<HTMLAnchorElement | HTMLSpanElement, BreadcrumbItemProps>(
+  ({ children, className, href, icon: Icon, ...props }, ref): JSX.Element => {
+    const isLink = typeof href !== 'undefined';
+    const theme = useTheme().theme.breadcrumb.item;
 
-  const Component = isLink ? 'a' : 'span';
+    const Component = isLink ? 'a' : 'span';
 
-  return (
-    <li className={classNames(theme.base, className)} {...props}>
-      <HiOutlineChevronRight aria-hidden className={theme.chevron} data-testid="flowbite-breadcrumb-separator" />
-      <Component className={theme.href[isLink ? 'on' : 'off']} data-testid="flowbite-breadcrumb-item" href={href}>
-        {Icon && <Icon aria-hidden className={theme.icon} />}
-        {children}
-      </Component>
-    </li>
-  );
-};
+    return (
+      <li className={classNames(theme.base, className)} {...props}>
+        <HiOutlineChevronRight aria-hidden className={theme.chevron} data-testid="flowbite-breadcrumb-separator" />
+        <Component
+          ref={ref as never}
+          className={theme.href[isLink ? 'on' : 'off']}
+          data-testid="flowbite-breadcrumb-item"
+          href={href}
+        >
+          {Icon && <Icon aria-hidden className={theme.icon} />}
+          {children}
+        </Component>
+      </li>
+    );
+  },
+);
 
 BreadcrumbItem.displayName = 'Breadcrumb.Item';
 export default BreadcrumbItem;
