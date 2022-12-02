d3f75250d835ca981e5889560c9ec1defda6440e
tulup-conner <conner@tulup.io>
2022-12-02 06:38:46 -0800

    feat(/components/sidebar): add Sidebar.Item label to theme  (#449)
    feat(/components/sidebar): add `Sidebar.Item` label to theme
    
    resolve #441

--- src/lib/theme/default.ts
+++ src/lib/theme/default.ts
@@ -759,6 +759,7 @@ const theme: FlowbiteTheme = {
         base: 'h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white',
         active: 'text-gray-700 dark:text-gray-100',
       },
+      label: '',
     },
     items: '',
     itemGroup:
