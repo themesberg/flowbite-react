3bb2a4504cf10464b84d1c6cfe5e25acf0b74f40
alex <alexzelinsky124@gmail.com>
2022-12-04 14:23:06 -0800

    removed mb5 class from AvatarGroup component

--- src/lib/theme/default.ts
+++ src/lib/theme/default.ts
@@ -113,7 +113,7 @@ const theme: FlowbiteTheme = {
     base: 'relative flex items-center justify-center w-10 h-10 text-xs font-medium text-white bg-gray-700 rounded-full ring-2 ring-gray-300 hover:bg-gray-600 dark:ring-gray-500',
   },
   avatarGroup: {
-    base: 'flex mb-5 -space-x-4',
+    base: 'flex -space-x-4',
   },
   badge: {
     base: 'flex h-fit items-center gap-1 font-semibold',
