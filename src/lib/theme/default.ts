78436142abecc57fcf49e0cd67e40aefa6b21a44
Ricardo Lüders <rluders@redhat.com>
2022-12-23 11:54:45 +0100

    feat(theme): badge theme component

--- src/lib/theme/default.ts
+++ src/lib/theme/default.ts
@@ -120,23 +120,29 @@ const theme: FlowbiteTheme = {
     base: 'flex mb-5 -space-x-4',
   },
   badge: {
-    base: 'flex h-fit items-center gap-1 font-semibold',
-    color: {
-      info: 'bg-blue-100 text-blue-800 dark:bg-blue-200 dark:text-blue-800 group-hover:bg-blue-200 dark:group-hover:bg-blue-300',
-      gray: 'bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300 group-hover:bg-gray-200 dark:group-hover:bg-gray-600',
-      failure:
-        'bg-red-100 text-red-800 dark:bg-red-200 dark:text-red-900 group-hover:bg-red-200 dark:group-hover:bg-red-300',
-      success:
-        'bg-green-100 text-green-800 dark:bg-green-200 dark:text-green-900 group-hover:bg-green-200 dark:group-hover:bg-green-300',
-      warning:
-        'bg-yellow-100 text-yellow-800 dark:bg-yellow-200 dark:text-yellow-900 group-hover:bg-yellow-200 dark:group-hover:bg-yellow-300',
-      indigo:
-        'bg-indigo-100 text-indigo-800 dark:bg-indigo-200 dark:text-indigo-900 group-hover:bg-indigo-200 dark:group-hover:bg-indigo-300',
-      purple:
-        'bg-purple-100 text-purple-800 dark:bg-purple-200 dark:text-purple-900 group-hover:bg-purple-200 dark:group-hover:bg-purple-300',
-      pink: 'bg-pink-100 text-pink-800 dark:bg-pink-200 dark:text-pink-900 group-hover:bg-pink-200 dark:group-hover:bg-pink-300',
+    root: {
+      base: 'flex h-fit items-center gap-1 font-semibold',
+      color: {
+        info: 'bg-blue-100 text-blue-800 dark:bg-blue-200 dark:text-blue-800 group-hover:bg-blue-200 dark:group-hover:bg-blue-300',
+        gray: 'bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300 group-hover:bg-gray-200 dark:group-hover:bg-gray-600',
+        failure:
+          'bg-red-100 text-red-800 dark:bg-red-200 dark:text-red-900 group-hover:bg-red-200 dark:group-hover:bg-red-300',
+        success:
+          'bg-green-100 text-green-800 dark:bg-green-200 dark:text-green-900 group-hover:bg-green-200 dark:group-hover:bg-green-300',
+        warning:
+          'bg-yellow-100 text-yellow-800 dark:bg-yellow-200 dark:text-yellow-900 group-hover:bg-yellow-200 dark:group-hover:bg-yellow-300',
+        indigo:
+          'bg-indigo-100 text-indigo-800 dark:bg-indigo-200 dark:text-indigo-900 group-hover:bg-indigo-200 dark:group-hover:bg-indigo-300',
+        purple:
+          'bg-purple-100 text-purple-800 dark:bg-purple-200 dark:text-purple-900 group-hover:bg-purple-200 dark:group-hover:bg-purple-300',
+        pink: 'bg-pink-100 text-pink-800 dark:bg-pink-200 dark:text-pink-900 group-hover:bg-pink-200 dark:group-hover:bg-pink-300',
+      },
+      href: 'group',
+      size: {
+        xs: 'p-1 text-xs',
+        sm: 'p-1.5 text-sm',
+      },
     },
-    href: 'group',
     icon: {
       off: 'rounded px-2 py-0.5',
       on: 'rounded-full p-1.5',
@@ -144,10 +150,6 @@ const theme: FlowbiteTheme = {
         xs: 'w-3 h-3',
         sm: 'w-3.5 h-3.5',
       },
-    },
-    size: {
-      xs: 'p-1 text-xs',
-      sm: 'p-1.5 text-sm',
     },
   },
   breadcrumb: {
