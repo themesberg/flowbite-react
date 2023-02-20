d3770c10dc35eaec9d575b4c3f94ca72f2cde51c
Conner Davis <conner@tulup.io>
2023-02-19 23:36:43 -0800

    fix(/lib/components/footer/footertitle): allow `<Footer.Title as="..">` to not use `<h2>`
    You can cast the component to a component of your own, or a generic HTML element, e.g.,
    `<Footer.Title as="h3">`.
    
    resolve #594

--- src/lib/components/Footer/FooterTitle.tsx
+++ src/lib/components/Footer/FooterTitle.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import type { ComponentProps, FC, PropsWithChildren } from 'react';
+import type { ComponentProps, ElementType, FC, PropsWithChildren } from 'react';
 import type { DeepPartial } from '..';
 import { mergeDeep } from '../../helpers/mergeDeep';
 import { useTheme } from '../Flowbite/ThemeContext';
@@ -9,16 +9,23 @@ export interface FlowbiteFooterTitleTheme {
 }
 
 export interface FooterTitleProps extends PropsWithChildren<ComponentProps<'h2'>> {
+  as?: ElementType;
   theme?: DeepPartial<FlowbiteFooterTitleTheme>;
   title: string;
 }
 
-export const FooterTitle: FC<FooterTitleProps> = ({ className, theme: customTheme = {}, title }) => {
+export const FooterTitle: FC<FooterTitleProps> = ({
+  as: Component = 'h2',
+  className,
+  theme: customTheme = {},
+  title,
+  ...props
+}) => {
   const theme = mergeDeep(useTheme().theme.footer.title, customTheme);
 
   return (
-    <h2 data-testid="flowbite-footer-title" className={classNames(theme.base, className)}>
+    <Component data-testid="flowbite-footer-title" className={classNames(theme.base, className)} {...props}>
       {title}
-    </h2>
+    </Component>
   );
 };
