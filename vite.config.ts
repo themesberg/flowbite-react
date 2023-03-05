7b498e4acb769e731e3f75bcca43c99137777201
Conner Davis <conner@tulup.io>
2023-03-04 18:02:22 -0800

    chore(tsconfig.json): add root `path`: `~` -> `.`
    Add a relative path via `tsconfig.json`'s `paths` field. Now, instead of chaining `../../`s in
    `import`s, we can just refer to the base directory as `~`, e.g., `~/src/components/Alert`.
    Additionally, because we are exporting the contents of the library , the vast majority of imports
    can simply be achieved via `import { X } from ` 'src';`

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     sourcemap: true,
   },
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     coverage: {
       reporter: ['html', 'json', 'text'],
