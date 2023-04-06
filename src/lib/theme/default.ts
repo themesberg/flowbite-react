a4f39f5608a46f6c0d4661fa383e70583ab7d592
meesvandongen <35409045+meesvandongen@users.noreply.github.com>
2023-04-06 15:36:59 +0200

    fix: group list styling (#668)
    * fix: group list styling
    
    * fix: borders between list group items
    
    ---------
    
    Co-authored-by: Mees van Dongen <mvandongen@ilionx.com>
    Co-authored-by: Ricardo Lüders <ricardo@luders.com.br>

--- src/lib/theme/default.ts
+++ src/lib/theme/default.ts
@@ -491,12 +491,12 @@ const theme: FlowbiteTheme = {
   },
   listGroup: {
     root: {
-      base: 'list-none rounded-lg border border-gray-200 bg-white text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white',
+      base: 'list-none rounded-lg border border-gray-200 bg-white text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white text-left',
     },
     item: {
-      base: 'flex w-full cursor-pointer border-b border-gray-200 py-2 px-4 first:rounded-t-lg last:rounded-b-lg last:border-b-0 dark:border-gray-600',
+      base: '[&>*]:first:rounded-t-lg [&>*]:last:rounded-b-lg [&>*]:last:border-b-0',
       link: {
-        base: '',
+        base: 'flex w-full border-b border-gray-200 py-2 px-4 dark:border-gray-600',
         active: {
           off: 'hover:bg-gray-100 hover:text-blue-700 focus:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:text-white dark:focus:ring-gray-500',
           on: 'bg-blue-700 text-white dark:bg-gray-800',
