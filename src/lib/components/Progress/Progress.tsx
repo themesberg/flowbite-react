dfcc2108c16e9a3a7392a7955226802a227d84e2
SrivatsaRUpadhya <srivatsarupadhya@gmail.com>
2023-04-13 19:04:46 +0530

    fix(components/progressbar): fix progressbar showing label when progress == 0

--- src/lib/components/Progress/Progress.tsx
+++ src/lib/components/Progress/Progress.tsx
@@ -53,8 +53,8 @@ export const Progress: FC<ProgressProps> = ({
   return (
     <>
       <div id={id} aria-label={textLabel} aria-valuenow={progress} role="progressbar" {...props}>
-        {(textLabel && labelText && textLabelPosition === 'outside') ||
-        (progress && labelProgress && progressLabelPosition === 'outside') ? (
+        {((textLabel && labelText && textLabelPosition === 'outside') ||
+          (progress > 0 && labelProgress && progressLabelPosition === 'outside')) && (
           <div className={theme.label} data-testid="flowbite-progress-outer-label-container">
             {textLabel && labelText && textLabelPosition === 'outside' && (
               <span data-testid="flowbite-progress-outer-text-label">{textLabel}</span>
@@ -63,7 +63,7 @@ export const Progress: FC<ProgressProps> = ({
               <span data-testid="flowbite-progress-outer-progress-label">{progress}%</span>
             )}
           </div>
-        ) : null}
+        )}
 
         <div className={classNames(theme.base, theme.size[size], className)}>
           <div
@@ -71,14 +71,10 @@ export const Progress: FC<ProgressProps> = ({
             className={classNames(theme.bar, theme.color[color], theme.size[size])}
           >
             {textLabel && labelText && textLabelPosition === 'inside' && (
-              <span className="whitespace-nowrap" data-testid="flowbite-progress-inner-text-label">
-                {textLabel}
-              </span>
+              <span data-testid="flowbite-progress-inner-text-label">{textLabel}</span>
             )}
-            {labelProgress && progressLabelPosition === 'inside' && (
-              <span className="whitespace-nowrap" data-testid="flowbite-progress-inner-progress-label">
-                {progress}%
-              </span>
+            {progress > 0 && labelProgress && progressLabelPosition === 'inside' && (
+              <span data-testid="flowbite-progress-inner-progress-label">{progress}%</span>
             )}
           </div>
         </div>
