0fd9f22efb1d7bfc648c4c6e2e1b8556b4a355c0
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2023-04-28 12:37:23 +0300

    feat(tailwind.config.js): add `primary` color values

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   plugins: [require('flowbite/plugin')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: { 50: '#E7F2F6', 100: '#CFE5ED', 200: '#B7D8E3', 300: '#9FCBDA', 400: '#87BFD1', 500: '#3E98B5', 600: '#268BAC', 700: '#0E7EA3', 800: '#0A5872', 900: '#084C62' }
+      }
+    },
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
     },
