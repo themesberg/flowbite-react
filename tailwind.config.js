3afe3e61ed54858a9344022a1b441439c151a5a9
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2023-05-03 11:45:35 +0300

    docs(config): setup full font family settings and max width class

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,54 @@ module.exports = {
         primary: { 50: '#E7F2F6', 100: '#CFE5ED', 200: '#B7D8E3', 300: '#9FCBDA', 400: '#87BFD1', 500: '#3E98B5', 600: '#268BAC', 700: '#0E7EA3', 800: '#0A5872', 900: '#084C62' }
       }
     },
-    fontFamily: {
-      sans: ['Inter', 'sans-serif'],
+    maxWidth: {
+      '8xl': '90rem',
     },
+    fontFamily: {
+      sans: [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji',
+      ],
+      body: [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji',
+      ],
+      mono: [
+        'ui-monospace',
+        'SFMono-Regular',
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        'Liberation Mono',
+        'Courier New',
+        'monospace',
+      ],
+    }
   },
 };
