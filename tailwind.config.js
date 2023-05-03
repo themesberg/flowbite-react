7057a0a24eaee91cdca120683d5d4753432f16b6
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2023-05-03 17:21:45 +0300

    docs(config): move maxWidth values to `extend``

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,10 @@ module.exports = {
     extend: {
       colors: {
         primary: { 50: '#E7F2F6', 100: '#CFE5ED', 200: '#B7D8E3', 300: '#9FCBDA', 400: '#87BFD1', 500: '#3E98B5', 600: '#268BAC', 700: '#0E7EA3', 800: '#0A5872', 900: '#084C62' }
-      }
-    },
-    maxWidth: {
-      '8xl': '90rem',
+      },
+      maxWidth: {
+        '8xl': '90rem',
+      },
     },
     fontFamily: {
       sans: [
