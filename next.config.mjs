54ea24494d08ffd3a14d008589a2fc986b1f74b0
Conner Davis <conner@tulup.io>
2023-05-03 23:59:57 -0700

    revert(next.config): undo use styled components

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,22 @@
-/** @type {import('next').NextConfig} */
-
 import withMDX from '@next/mdx';
 
 const mdxConfig = withMDX({
   extension: /\.mdx?$/,
   options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [],
     rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
+    remarkPlugins: [],
   },
-})
+});
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   experimental: {
     appDir: true,
     swcFileReading: true,
   },
+  pageExtensions: ['mdx', 'tsx'],
   reactStrictMode: true,
   swcMinify: true,
-  styledComponents: true
 };
 
 export default withMDX(mdxConfig)(nextConfig);
