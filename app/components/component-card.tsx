3ab547f330aa791fd357dc03d7b913b303a4fd45
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2023-05-05 12:16:46 +0300

    docs(homepage): add contributors section with live GH data

--- app/components/component-card.tsx
+++ app/components/component-card.tsx
@@ -24,14 +24,14 @@ export const ComponentCard: FC<ComponentCardProps> = function ({ link, name, ima
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="2"
+            strokeWidth="2"
             stroke="currentColor"
             aria-hidden="true"
             className="h-5 w-5"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
             ></path>
           </svg>
