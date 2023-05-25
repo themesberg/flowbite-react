e1e540b4afdbc2a07dc595df43ea47c564a49771
Conner Davis <conner@tulup.io>
2023-05-25 10:36:09 -0700

    docs(app/components/code-preview): change default code highlighter style to `.tsx`

--- app/components/code-preview.tsx
+++ app/components/code-preview.tsx
@@ -30,7 +30,7 @@ export const CodePreview: FC<CodePreviewProps> = function ({ children, className
     <div className="mb-12 flex w-full flex-col gap-2">
       <h2 className="text-2xl font-bold">{title}</h2>
       <div className={classNames('py-4', className)}>{children}</div>
-      <pre className="language-jsx">
+      <pre className="language-tsx">
         <code>{code}</code>
       </pre>
     </div>
