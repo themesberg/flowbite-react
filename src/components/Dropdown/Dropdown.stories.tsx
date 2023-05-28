7c2341274df1b734e3e782a3e88326f21671966e
Mohammadali Ghezelbash <mo.ghezelbash@mci.ir>
2023-05-28 14:30:42 +0330

    feat(dropdown): add disabled to storybook
    #539

--- src/components/Dropdown/Dropdown.stories.tsx
+++ src/components/Dropdown/Dropdown.stories.tsx
@@ -84,3 +84,16 @@ ItemClickHandler.args = {
     </>
   ),
 };
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+  children: (
+    <>
+      <Dropdown.Item>Dashboard</Dropdown.Item>
+      <Dropdown.Item>Settings</Dropdown.Item>
+      <Dropdown.Item>Earnings</Dropdown.Item>
+      <Dropdown.Item>Sign out</Dropdown.Item>
+    </>
+  ),
+};
