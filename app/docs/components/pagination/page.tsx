162e7806f810cb8d52bab921a5713a8e7de5fbe2
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2023-05-30 11:30:47 +0300

    docs(pagination): fix prettier warning

--- app/docs/components/pagination/page.tsx
+++ app/docs/components/pagination/page.tsx
@@ -2,7 +2,8 @@ import type { Metadata, NextPage } from 'next';
 import FormsPageContent from '.';
 
 export const metadata: Metadata = {
-  description: 'Get started with the pagination component to indicate the number of pages with number, link, and control buttons and allow the user to navigate through these pages',
+  description:
+    'Get started with the pagination component to indicate the number of pages with number, link, and control buttons and allow the user to navigate through these pages',
   title: 'React Pagination - Flowbite',
 };
 
