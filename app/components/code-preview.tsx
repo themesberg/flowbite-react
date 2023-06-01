45d84d069803c82de0aab7052c176beb990f29cb
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2023-06-01 11:32:30 +0300

    docs(code preview): add copy code to clipboard functionality

--- app/components/code-preview.tsx
+++ app/components/code-preview.tsx
@@ -2,9 +2,11 @@
 
 import classNames from 'classnames';
 import type { ComponentProps, FC, PropsWithChildren } from 'react';
-import { Children } from 'react';
+import { Children, useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import type { Options } from 'react-element-to-jsx-string';
 import reactElementToJSXString from 'react-element-to-jsx-string';
+import { BsCheckLg, BsFillClipboardFill } from 'react-icons/bs';
 
 const reactElementToJSXStringOptions: Options = {
   filterProps: ['as', 'key'],
@@ -20,11 +22,19 @@ interface CodePreviewProps extends PropsWithChildren, ComponentProps<'div'> {
 
 export const CodePreview: FC<CodePreviewProps> = function ({ children, className }) {
   const childrenList = Children.toArray(children);
+  const [copied, setCopied] = useState(false);
   let code = childrenList.map((child) => reactElementToJSXString(child, reactElementToJSXStringOptions)).join('\n');
   code = deleteJSXSpaces(code);
   code = deleteSVGs(code);
   code = replaceWebpackImportsOnComponents(code);
   code = replaceWebpackImportsOnFunctions(code);
+
+  const onCodeCopy = (_, success) => {
+    if (success) {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 3000);
+    }
+  };
 
   return (
     <div className="code-example mt-8">
@@ -93,26 +103,21 @@ export const CodePreview: FC<CodePreviewProps> = function ({ children, className
               </li>
             </ul>
             <div className="flex justify-end">
-              <button
-                type="button"
-                className="copy-to-clipboard-button flex items-center border-l border-gray-200 bg-gray-100 px-3 py-2 text-xs font-medium text-gray-600 hover:text-primary-700 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:text-white"
-              >
-                <svg
-                  className="mr-2 h-4 w-4"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
+              <CopyToClipboard text={code} onCopy={onCodeCopy}>
+                <button
+                  type="button"
+                  onClick={(e) => e.preventDefault()}
+                  className="copy-to-clipboard-button flex items-center border-l border-gray-200 bg-gray-100 px-3 py-2 text-xs font-medium text-gray-600 hover:text-primary-700 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:text-white"
                 >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
-                  ></path>
-                </svg>
-                Copy
-              </button>
+                  {copied ? (
+                    <BsCheckLg className="mr-2 h-4 w-4 text-green-500 dark:text-green-400" />
+                  ) : (
+                    <BsFillClipboardFill className="mr-2 h-3 w-3" />
+                  )}
+
+                  {copied ? 'Code copied!' : 'Copy code'}
+                </button>
+              </CopyToClipboard>
             </div>
           </div>
           <pre className="language-tsx !my-0">
