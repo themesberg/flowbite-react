27c053a882597608748ff4b870f6150d0542ed10
Conner Davis <conner@tulup.io>
2023-06-07 15:27:39 -0700

    docs(app/components/code-preview): allow toggle dark mode on each code preview
    This will only toggle the preview box itself - not the code. That reflects the behavior found on
    flowbite.com.

--- app/components/code-preview.tsx
+++ app/components/code-preview.tsx
@@ -6,6 +6,7 @@ import { Children, useState } from 'react';
 import type { Options } from 'react-element-to-jsx-string';
 import reactElementToJSXString from 'react-element-to-jsx-string';
 import { BsCheckLg, BsFillClipboardFill } from 'react-icons/bs';
+import { HiMoon, HiSun } from 'react-icons/hi';
 
 const reactElementToJSXStringOptions: Options = {
   filterProps: ['as', 'key'],
@@ -19,7 +20,12 @@ interface CodePreviewProps extends PropsWithChildren, ComponentProps<'div'> {
   title: string;
 }
 
+interface CodePreviewState {
+  isDarkMode: boolean;
+}
+
 export const CodePreview: FC<CodePreviewProps> = function ({ children, className }) {
+  const [isDarkMode, setDarkMode] = useState(false);
   const [isJustCopied, setJustCopied] = useState(false);
 
   const copyToClipboard = (toCopy: string) => {
@@ -40,50 +46,13 @@ export const CodePreview: FC<CodePreviewProps> = function ({ children, className
     <div className="code-example mt-8">
       <div className="w-full rounded-t-xl border border-gray-200 bg-gray-50 p-4 dark:border-gray-600 dark:bg-gray-700">
         <div className="grid grid-cols-2">
-          <a
-            href={`https://github.com/themesberg/flowbite-react/`}
-            className="inline-flex w-fit items-center justify-center gap-2 rounded-lg border border-gray-200 bg-white px-3 py-2 text-center text-xs font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-600 hover:text-primary-700 focus:text-primary-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:!bg-gray-900 dark:bg-gray-800 dark:bg-transparent dark:text-gray-400 dark:hover:border-gray-700 dark:hover:text-white dark:focus:text-white dark:focus:ring-gray-700"
-            target="_blank"
-            rel="noreferrer nofollow noopener"
-          >
-            <svg
-              className="h-4 w-4"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              aria-hidden="true"
-              focusable="false"
-              data-icon="github"
-              role="img"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            Edit on GitHub
-          </a>{' '}
+          <EditOnGithubButton />
           <div className="ml-auto">
-            <button
-              type="button"
-              className="toggle-dark-state-example mr-2 flex items-center rounded-lg border border-gray-200 bg-white p-2 text-xs font-medium text-gray-700 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-2 focus:ring-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-500"
-            >
-              <svg
-                aria-hidden="true"
-                data-toggle-icon="moon"
-                className="h-4 w-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-              </svg>{' '}
-              <span className="sr-only">Toggle dark/light mode</span>
-            </button>
+            <ToggleDarkModeButton isDarkMode={isDarkMode} onClick={() => setDarkMode(!isDarkMode)} />
           </div>
         </div>
       </div>
-      <div className="code-preview-wrapper">
+      <div className={classNames('code-preview-wrapper', isDarkMode && 'dark')}>
         <div className="code-preview flex border-x border-gray-200 bg-white bg-gradient-to-r p-0 dark:border-gray-600 dark:bg-gray-900">
           <div className="code-responsive-wrapper w-full">
             <div className="mx-auto w-full bg-white bg-gradient-to-r p-2 dark:bg-gray-900 sm:p-6">
@@ -117,7 +86,7 @@ export const CodePreview: FC<CodePreviewProps> = function ({ children, className
               </button>
             </div>
           </div>
-          <pre className="language-tsx !my-0">
+          <pre className="language-tsx">
             <code>{code}</code>
           </pre>
           <button
@@ -129,6 +98,46 @@ export const CodePreview: FC<CodePreviewProps> = function ({ children, className
         </div>
       </div>
     </div>
+  );
+};
+
+const EditOnGithubButton: FC = () => {
+  return (
+    <a
+      href="https://github.com/themesberg/flowbite-react/"
+      className="inline-flex w-fit items-center justify-center gap-2 rounded-lg border border-gray-200 bg-white px-3 py-2 text-center text-xs font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-600 hover:text-primary-700 focus:text-primary-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:!bg-gray-900 dark:bg-gray-800 dark:bg-transparent dark:text-gray-400 dark:hover:border-gray-700 dark:hover:text-white dark:focus:text-white dark:focus:ring-gray-700"
+      target="_blank"
+      rel="noreferrer nofollow noopener"
+    >
+      <svg
+        className="h-4 w-4"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+        focusable="false"
+        data-icon="github"
+        role="img"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+      Edit on GitHub
+    </a>
+  );
+};
+
+const ToggleDarkModeButton: FC<ComponentProps<'button'> & CodePreviewState> = ({ isDarkMode, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="flex items-center rounded-lg border border-gray-200 bg-white p-2 text-xs font-medium text-gray-700 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-2 focus:ring-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-500"
+    >
+      <span className="sr-only">Toggle dark/light mode</span>
+      {isDarkMode ? <HiSun className="h-4 w-4" /> : <HiMoon className="h-4 w-4" />}
+    </button>
   );
 };
 
