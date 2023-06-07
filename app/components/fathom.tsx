6578304826ecf6b7d72ad4eeeb9ae272f0658775
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2023-06-07 10:13:22 +0300

    chore(spacing): add spacing after blocks

--- app/components/fathom.tsx
+++ app/components/fathom.tsx
@@ -6,6 +6,7 @@ import { useEffect } from 'react';
 export default function Fathom() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
+
   useEffect(() => {
     load('UXMSXUQI', {
       includedDomains: ['flowbite-react.com'],
