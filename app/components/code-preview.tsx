5ff6c042c00d72278bdc01c211370c9c9f06ab3a
Conner Davis <conner@tulup.io>
2023-06-08 13:26:57 -0700

    docs(app/docs/components/*): update Edit on Github Button links
    Provide a link to the Github.com page to view code of the primary component in each code preview.

--- app/components/code-preview.tsx
+++ app/components/code-preview.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import classNames from 'classnames';
+import { usePathname } from 'next/navigation';
 import type { ComponentProps, FC, PropsWithChildren } from 'react';
 import { Children, useState } from 'react';
 import type { Options } from 'react-element-to-jsx-string';
 import reactElementToJSXString from 'react-element-to-jsx-string';
 import { BsCheckLg, BsFillClipboardFill } from 'react-icons/bs';
 import { HiMoon, HiSun } from 'react-icons/hi';
+import { Tooltip } from '~/src';
 
 const reactElementToJSXStringOptions: Options = {
   filterProps: ['key', 'ref'],
@@ -15,7 +17,8 @@ const reactElementToJSXStringOptions: Options = {
 };
 
 interface CodePreviewProps extends PropsWithChildren, ComponentProps<'div'> {
-  code?: string;
+  functionBody?: string[];
+  githubPage?: string;
   importExternal?: string;
   importFlowbiteReact?: string;
   title: string;
@@ -30,7 +33,9 @@ interface CodePreviewState {
 export const CodePreview: FC<CodePreviewProps> = function ({
   children,
   className,
-  importExternal = '',
+  functionBody,
+  githubPage,
+  importExternal,
   importFlowbiteReact,
   title,
 }) {
@@ -54,8 +59,9 @@ export const CodePreview: FC<CodePreviewProps> = function ({
   code = `'use client';
 
 import { ${importFlowbiteReact ?? firstComponentDisplayName(code)} } from 'flowbite-react';
-${importExternal === '' ? '' : `${importExternal}\n`}
+${importExternal ? `${importExternal}\n` : ''}
 export default function ${titleCaseToUpperCamelCase(title)}() {
+${functionBody ? functionBody.map((line) => `  ${line}\n`).join('') : ''}
   return (
     ${isFragment ? '<>\n      ' : ''}${code.replaceAll(/\n/g, isFragment ? '\n      ' : '\n    ')}${
     isFragment ? '\n    </>' : ''
@@ -67,7 +73,7 @@ export default function ${titleCaseToUpperCamelCase(title)}() {
     <div className="code-example mt-8">
       <div className="w-full rounded-t-xl border border-gray-200 bg-gray-50 p-4 dark:border-gray-600 dark:bg-gray-700">
         <div className="grid grid-cols-2">
-          <EditOnGithubButton />
+          <EditOnGithubButton githubPage={githubPage} title={title} />
           <div className="ml-auto">
             <ToggleDarkModeButton isDarkMode={isDarkMode} onClick={() => setDarkMode(!isDarkMode)} />
           </div>
@@ -106,28 +112,34 @@ export default function ${titleCaseToUpperCamelCase(title)}() {
   );
 };
 
-const EditOnGithubButton: FC = () => {
+const EditOnGithubButton: FC<CodePreviewProps> = ({ githubPage }) => {
+  const pathname = usePathname();
+
+  const githubSrcHref = 'https://github.com/themesberg/flowbite-react/blob/main/src/components/';
+  const page = pathname.split('/').pop() ?? '';
+  const component = slugToUpperCamelCase(page);
+  const href = `${githubSrcHref}${githubPage ?? `${component}/${component}`}.tsx`;
+
   return (
     <a
-      href="https://github.com/themesberg/flowbite-react/"
-      className="inline-flex w-fit items-center justify-center gap-2 rounded-lg border border-gray-200 bg-white px-3 py-2 text-center text-xs font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:text-primary-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:bg-transparent dark:text-gray-400 dark:hover:border-gray-700 dark:hover:text-white dark:focus:text-white dark:focus:ring-gray-700"
-      target="_blank"
+      href={href}
       rel="noreferrer nofollow noopener"
+      className="inline-flex w-fit items-center justify-center gap-2 rounded-lg border border-gray-200 bg-white px-3 py-2 text-center text-xs font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:text-primary-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:bg-transparent dark:text-gray-400 dark:hover:border-gray-700 dark:hover:text-white dark:focus:text-white dark:focus:ring-gray-700"
     >
       <svg
-        className="h-4 w-4"
+        aria-hidden
         fill="currentColor"
         viewBox="0 0 24 24"
-        aria-hidden="true"
         focusable="false"
         data-icon="github"
         role="img"
+        className="h-4 w-4"
       >
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-          clip-rule="evenodd"
-        ></path>
+          clipRule="evenodd"
+        />
       </svg>
       Edit on GitHub
     </a>
@@ -136,13 +148,15 @@ const EditOnGithubButton: FC = () => {
 
 const ToggleDarkModeButton: FC<ComponentProps<'button'> & CodePreviewState> = ({ isDarkMode, onClick }) => {
   return (
-    <button
-      onClick={onClick}
-      className="flex items-center rounded-lg border border-gray-200 bg-white p-2 text-xs font-medium text-gray-700 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-2 focus:ring-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-500"
-    >
-      <span className="sr-only">Toggle dark/light mode</span>
-      {isDarkMode ? <HiSun className="h-4 w-4" /> : <HiMoon className="h-4 w-4" />}
-    </button>
+    <Tooltip content={isDarkMode ? 'Toggle light mode' : 'Toggle dark mode'}>
+      <button
+        onClick={onClick}
+        className="flex items-center rounded-lg border border-gray-200 bg-white p-2 text-xs font-medium text-gray-700 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-2 focus:ring-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-500"
+      >
+        <span className="sr-only">Toggle dark/light mode</span>
+        {isDarkMode ? <HiSun className="h-4 w-4" /> : <HiMoon className="h-4 w-4" />}
+      </button>
+    </Tooltip>
   );
 };
 
@@ -190,7 +204,7 @@ const deleteSVGs = (str: string) => {
 };
 
 const replaceWebpackImportsOnFunctions = (str: string) => {
-  return str.replaceAll(/function (.*?) \((.*)\)(.*)}/g, '$1}');
+  return str.replaceAll(/function (.*?)\s?\(props\)\s?{(.*)}/g, '$1}');
 };
 
 const titleCaseToUpperCamelCase = (str: string) => {
@@ -198,4 +212,11 @@ const titleCaseToUpperCamelCase = (str: string) => {
     .replaceAll(/(?:^\w|[A-Z]|\b\w)/g, (word) => word.toUpperCase())
     .replaceAll(/\s+/g, '')
     .replaceAll(/-/g, '');
+};
+
+const slugToUpperCamelCase = (str: string) => {
+  return str
+    .replaceAll(/-/g, ' ')
+    .replaceAll(/\b[a-z]/g, (letter) => letter.toUpperCase())
+    .replaceAll(/\s/g, '');
 };
