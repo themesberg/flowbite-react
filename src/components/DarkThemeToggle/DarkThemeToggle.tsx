1d615c0bfbef4fdc48d9f19c18c069a15c74e228
Ricardo Lüders <rluders@redhat.com>
2023-06-25 20:36:56 +0200

    feat(components/darkthemetoggle): supports custom icons

--- src/components/DarkThemeToggle/DarkThemeToggle.tsx
+++ src/components/DarkThemeToggle/DarkThemeToggle.tsx
@@ -17,10 +17,18 @@ export interface FlowbiteDarkThemeToggleRootTheme {
 }
 
 export interface DarkThemeToggleProps extends ComponentProps<'button'> {
+  iconDark?: string;
+  iconLight?: string;
   theme?: DeepPartial<FlowbiteDarkThemeToggleTheme>;
 }
 
-export const DarkThemeToggle: FC<DarkThemeToggleProps> = ({ className, theme: customTheme = {}, ...props }) => {
+export const DarkThemeToggle: FC<DarkThemeToggleProps> = ({
+  className,
+  theme: customTheme = {},
+  iconDark: IconDark = HiSun,
+  iconLight: IconLight = HiMoon,
+  ...props
+}) => {
   const theme = mergeDeep(useTheme().theme.darkThemeToggle, customTheme);
 
   const { mode, toggleMode } = useContext(ThemeContext);
@@ -35,9 +43,9 @@ export const DarkThemeToggle: FC<DarkThemeToggleProps> = ({ className, theme: cu
       {...props}
     >
       {mode === 'dark' ? (
-        <HiSun aria-label="Currently dark mode" className={theme.root.icon} />
+        <IconLight aria-label="Currently dark mode" className={theme.root.icon} />
       ) : (
-        <HiMoon aria-label="Currently light mode" className={theme.root.icon} />
+        <IconDark aria-label="Currently light mode" className={theme.root.icon} />
       )}
     </button>
   );
