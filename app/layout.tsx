b1375c55c710c800d95d0e03b5fa618859013e23
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2023-07-18 15:48:47 +0300

    docs(meta-data): improve meta data and favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,19 +11,44 @@ const interFont = InterFont({
 });
 
 export const metadata: Metadata = {
-  description: 'Official React components built for Flowbite and Tailwind CSS',
-  icons: '/favicon.svg',
+  description:
+    'Flowbite React is an open-source UI component library built with React components, Tailwind CSS utility classes and based on the Flowbite design system and components.',
+  icons: {
+    icon: [
+      { url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+      { url: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+    ],
+    shortcut: '/favicon.ico',
+    apple: [{ url: '/apple-touch-icon.png', sizes: '180x180' }],
+  },
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#ffffff' },
+    { media: '(prefers-color-scheme: dark)', color: '#1f2937' },
+  ],
+  manifest: '/site.webmanifest',
   other: {
     charSet: 'utf-8',
     lang: 'en',
   },
   title: 'Flowbite React - UI Component Library',
-  viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
   openGraph: {
     description:
       'Flowbite React is an open-source UI component library built with React components, Tailwind CSS utility classes and based on the Flowbite design system and components.',
-    images: 'https://flowbite-react.vercel.app/favicon.svg',
+    images: 'https://flowbite.s3.amazonaws.com/github/flowbite-react.png',
     title: 'Flowbite React - UI Component Library',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Flowbite React - UI component library',
+    description:
+      'Flowbite React is an open-source UI component library built with React components, Tailwind CSS utility classes and based on the Flowbite design system and components.',
+    creator: '@zoltanszogyenyi',
+    images: ['https://flowbite.s3.amazonaws.com/github/flowbite-react.png'],
   },
 };
 
