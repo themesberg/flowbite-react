509e553e52612f00b54ed4e4f2ccc322358d6d3e
Conner Davis <conner@tulup.io>
2023-09-08 15:44:09 -0700

    feat(src/components/banner): add component, Banner
    This component is currently a work-in-progress; you can expect that there may be significant changes
    in the future, in particular, to its theme. There currently isn't a theme, but there probably will
    be in the future.

---
+++ src/components/Banner/BannerCollapseButton.tsx
@@ -0,0 +1,22 @@
+import type { FC, MouseEventHandler } from 'react';
+import type { ButtonProps } from '../Button';
+import { Button } from '../Button';
+
+export type BannerCollapseButtonProps = ButtonProps;
+
+export const BannerCollapseButton: FC<BannerCollapseButtonProps> = ({ children, ...props }) => {
+  const onClick: MouseEventHandler<HTMLButtonElement> = (event) => {
+    const collapseButton = event.target as HTMLButtonElement;
+    const parentBanner = collapseButton.closest('[role="banner"]');
+
+    parentBanner?.remove();
+  };
+
+  return (
+    <Button onClick={onClick} {...props}>
+      {children}
+    </Button>
+  );
+};
+
+BannerCollapseButton.displayName = 'Banner.CollapseButton';
