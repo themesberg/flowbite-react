6d78e4f8313ec22ad11b1d1c8ceaf0dd20df701a
okellomarvinkevin <marvin.okello@sophany.it>
2023-09-08 13:01:25 +0200

    feat: Created floating label component, with the possibility of styles filled, outlined and standard as well as for each case, success and failure

--- src/components/FloatingLabel/FloatingLabel.stories.tsx
+++ src/components/FloatingLabel/FloatingLabel.stories.tsx
@@ -10,9 +10,45 @@ export default {
 
 const Template: Story<FloatingLabelProps> = (args) => <FloatingLabel {...args} />;
 
-export const Default = Template.bind({})
-Default.storyName = 'Default label';
-Default.args = {
-    /*to be replaced with the actual args which will be a placeholder, boolean and value*/
-    color: "success"
-};
+export const FilledSuccess = Template.bind({})
+FilledSuccess.storyName = 'Filled success'
+FilledSuccess.args = {
+    color: "success",
+    buttonStyle: "filled",
+    label: "Label"
+}
+export const OutlinedSuccess = Template.bind({})
+OutlinedSuccess.storyName = 'Outlined success'
+OutlinedSuccess.args = {
+    color: "success",
+    buttonStyle: "outlined",
+    label: "Label"
+}
+export const StandardSuccess = Template.bind({})
+StandardSuccess.storyName = 'Standard success'
+StandardSuccess.args = {
+    color: "success",
+    buttonStyle: "standard",
+    label: "Label"
+}
+export const FilledError = Template.bind({})
+FilledError.storyName = 'Filled Error '
+FilledError.args = {
+    color: "failure",
+    buttonStyle: "filled",
+    label: "Label"
+}
+export const OutlinedError = Template.bind({})
+OutlinedError.storyName = 'Outlined Error'
+OutlinedError.args = {
+    color: "failure",
+    buttonStyle: "outlined",
+    label: "Label"
+}
+export const StandardError = Template.bind({})
+StandardError.storyName = 'Standard Error'
+StandardError.args = {
+    color: "failure",
+    buttonStyle: "standard",
+    label: "Label"
+}
