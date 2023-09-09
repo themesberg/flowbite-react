dd0ca15352e18b19a7c932e61c47221ac5af733e
Conner Davis <conner@tulup.io>
2023-09-08 17:04:55 -0700

    feat(src/components/banner): add `role="banner"` to `<Banner>`

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -5,7 +5,7 @@ export type BannerComponentProps = ComponentProps<'div'>;
 
 const BannerComponent: FC<BannerComponentProps> = ({ children, ...props }) => {
   return (
-    <div data-testid="flowbite-banner" tabIndex={-1} {...props}>
+    <div data-testid="flowbite-banner" role="banner" tabIndex={-1} {...props}>
       {children}
     </div>
   );
