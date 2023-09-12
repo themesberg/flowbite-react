c3b2ff7073bcc6c063e5954555aaf91f6d8619da
okellomarvinkevin <marvin.okello@sophany.it>
2023-09-12 12:39:17 +0200

    feat: wrote tests for the the Floating label component

--- src/components/FloatingLabel/FloatingLabel.spec.tsx
+++ src/components/FloatingLabel/FloatingLabel.spec.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { render } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { FloatingLabel } from '~/src/components/FloatingLabel/FloatingLabel';
@@ -28,3 +29,38 @@ describe('Components / Floating Label', () => {
     });
   });
 });
+=======
+import { render } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import {FloatingLabel} from "~/src/components/FloatingLabel/FloatingLabel";
+
+describe("Components / Floating Label", () => {
+  describe("Floating Label properties", () => {
+    it("should have `role=\"textbox\"` by default", () => {
+      const textInput = render(<FloatingLabel  buttonStyle={"filled"} label={"Label"}/>).getByRole("textbox");
+      expect(textInput).toBeInTheDocument();
+    });
+
+    it("should have a label", () => {
+      const input = render(<FloatingLabel buttonStyle={"filled"} label={"Label"}/>).getByLabelText('Label');
+      expect(input).toBeInTheDocument();
+    })
+  });
+  it("should update the input value correctly", () => {
+    const input: HTMLInputElement | null = render(<FloatingLabel buttonStyle={"filled"} label={"Label"}
+                                  defaultValue={"my name is"} />).getByTestId("floating-label") as HTMLInputElement;
+    expect(input.value).toBe("my name is");
+  })
+
+  it("should be disabled", () => {
+    const input = render(<FloatingLabel buttonStyle={"filled"} label={"Label"} disabled={true}/>).getByTestId("floating-label");
+    expect(input).toBeDisabled()
+  })
+
+
+});
+
+
+
+
+>>>>>>> 49d5804 (feat: wrote tests for the the Floating label component)
--- src/components/FloatingLabel/FloatingLabel.tsx
+++ src/components/FloatingLabel/FloatingLabel.tsx
@@ -1,4 +1,5 @@
 import type { ComponentProps } from 'react';
+<<<<<<< HEAD
 import { forwardRef, useId } from 'react';
 import { twMerge } from 'tailwind-merge';
 import { mergeDeep } from '~/src/helpers/merge-deep';
@@ -73,6 +74,73 @@ export const FloatingLabel = forwardRef<HTMLInputElement, FloatingLabelProps>(
       className,
       theme: customTheme = {},
       ...props
+=======
+import {forwardRef} from 'react';
+import type {FlowbiteSizes } from '../../';
+
+export interface FlowbiteFloatingLabelSizes extends Pick<FlowbiteSizes, 'sm' | 'md' > {
+    [key: string]: string;
+}
+
+export interface FloatingLabelProps extends Omit<ComponentProps<'input'>, 'ref' | 'color'> {
+    error?: boolean;
+    helperText?: string;
+    sizing?: keyof FlowbiteFloatingLabelSizes;
+    buttonStyle: string;
+    label: string;
+    disabled?: boolean;
+}
+
+export const FloatingLabel = forwardRef<HTMLInputElement, FloatingLabelProps>(
+    (
+        {
+            error = null,
+            helperText,
+            sizing= "md",
+            buttonStyle,
+            label,
+            disabled= false,
+            ...props
+        },
+        ref,
+    ) => {
+        const inputColor = (error === false) ? "green-600" : (error === null ? "gray-400" : "red-600");
+
+        const filledStyles = `block rounded-t-lg px-2.5 pb-2.5 pt-${sizing === "sm"?"4":"5"} w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-${inputColor} dark:border-${inputColor} appearance-none dark:text-white dark:focus:border-${inputColor} focus:outline-none focus:ring-0 focus:border-${inputColor} peer`
+        const outlinedStyles = `block px-2.5 pb-2.5 pt-${sizing === "sm"?"3":"4"} w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-${inputColor} appearance-none dark:text-white dark:border-${inputColor} dark:focus:border-${inputColor} focus:outline-none focus:ring-0 focus:border-${inputColor} peer`
+        const standardStyles = `block py-${sizing === "sm"?"2":"2.5"} px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-${inputColor} appearance-none dark:text-white dark:border-${inputColor} dark:focus:border-${inputColor} focus:outline-none focus:ring-0 focus:border-${inputColor} peer`
+
+        let buttonTheme;
+
+        if (buttonStyle === 'filled') {
+            buttonTheme = filledStyles;
+        } else if (buttonStyle === 'outlined') {
+            buttonTheme = outlinedStyles;
+        } else {
+            buttonTheme = standardStyles;
+        }
+        const randomId = Math.random().toString(36).substring(6);
+        return (
+            <div>
+                <div className="relative">
+                    <input type="text"
+                           id={props.id? props.id : "floatingLabel"+ randomId }
+                           aria-describedby="floatingLabelInputHelp"
+                           className={`${buttonTheme}`}
+                           placeholder=" "
+                           data-testid="floating-label"
+                           disabled={disabled}
+                           {...props}
+                           ref={ref}/>
+                    <label htmlFor={props.id? props.id : "floatingLabel" + randomId}
+                           className={`absolute text-sm text-${inputColor} dark:text-${inputColor} duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1`}>
+                        {label}
+                    </label>
+                </div>
+                <p id="floatingLabelInputHelp" className={`mt-2 text-xs text-${inputColor} dark:text-${inputColor}`}> {helperText}</p>
+            </div>
+        );
+>>>>>>> 49d5804 (feat: wrote tests for the the Floating label component)
     },
     ref,
   ) => {
