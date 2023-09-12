605e03c55d03e3e2582f2515baf6bbe439ec520c
okellomarvinkevin <marvin.okello@sophany.it>
2023-09-12 17:53:19 +0200

    Added Floating Label under Forms in the docs

--- app/docs/components/floating-label/index.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import type { FC } from 'react';
-import FloatingLabelDocs from './floating-label.mdx';
-
-const FloatingLabelContent: FC = () => <FloatingLabelDocs />;
-export default FloatingLabelContent;
