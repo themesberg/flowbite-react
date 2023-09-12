8a190b373839e90a7e7bb2588b9c2f0f071e49dc
okellomarvinkevin <marvin.okello@sophany.it>
2023-09-12 14:20:14 +0200

    formatting code

--- src/components/FloatingLabel/FloatingLabel.tsx
+++ src/components/FloatingLabel/FloatingLabel.tsx
@@ -78,39 +78,39 @@ export const FloatingLabel = forwardRef<HTMLInputElement, FloatingLabelProps>(
 import {forwardRef} from 'react';
 import type {FlowbiteSizes } from '../../';
 
-export interface FlowbiteFloatingLabelSizes extends Pick<FlowbiteSizes, 'sm' | 'md' > {
-    [key: string]: string;
+export interface FlowbiteFloatingLabelSizes extends Pick<FlowbiteSizes, "sm" | "md"> {
+  [key: string]: string;
 }
 
-export interface FloatingLabelProps extends Omit<ComponentProps<'input'>, 'ref' | 'color'> {
-    error?: boolean;
-    helperText?: string;
-    sizing?: keyof FlowbiteFloatingLabelSizes;
-    buttonStyle: string;
-    label: string;
-    disabled?: boolean;
+export interface FloatingLabelProps extends Omit<ComponentProps<"input">, "ref" | "color"> {
+  error?: boolean;
+  helperText?: string;
+  sizing?: keyof FlowbiteFloatingLabelSizes;
+  buttonStyle: string;
+  label: string;
+  disabled?: boolean;
 }
 
 export const FloatingLabel = forwardRef<HTMLInputElement, FloatingLabelProps>(
-    (
-        {
-            error = null,
-            helperText,
-            sizing= "md",
-            buttonStyle,
-            label,
-            disabled= false,
-            ...props
-        },
-        ref,
-    ) => {
-        const inputColor = (error === false) ? "green-600" : (error === null ? "gray-400" : "red-600");
+  (
+    {
+      error = null,
+      helperText,
+      sizing = "md",
+      buttonStyle,
+      label,
+      disabled = false,
+      ...props
+    },
+    ref
+  ) => {
+    const inputColor = (error === false) ? "green-600" : (error === null ? "gray-400" : "red-600");
 
-        const filledStyles = `block rounded-t-lg px-2.5 pb-2.5 pt-${sizing === "sm"?"4":"5"} w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-${inputColor} dark:border-${inputColor} appearance-none dark:text-white dark:focus:border-${inputColor} focus:outline-none focus:ring-0 focus:border-${inputColor} peer`
-        const outlinedStyles = `block px-2.5 pb-2.5 pt-${sizing === "sm"?"3":"4"} w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-${inputColor} appearance-none dark:text-white dark:border-${inputColor} dark:focus:border-${inputColor} focus:outline-none focus:ring-0 focus:border-${inputColor} peer`
-        const standardStyles = `block py-${sizing === "sm"?"2":"2.5"} px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-${inputColor} appearance-none dark:text-white dark:border-${inputColor} dark:focus:border-${inputColor} focus:outline-none focus:ring-0 focus:border-${inputColor} peer`
+    const filledStyles = `block rounded-t-lg px-2.5 pb-2.5 pt-${sizing === "sm" ? "4" : "5"} w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-${inputColor} dark:border-${inputColor} appearance-none dark:text-white dark:focus:border-${inputColor} focus:outline-none focus:ring-0 focus:border-${inputColor} peer`;
+    const outlinedStyles = `block px-2.5 pb-2.5 pt-${sizing === "sm" ? "3" : "4"} w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-${inputColor} appearance-none dark:text-white dark:border-${inputColor} dark:focus:border-${inputColor} focus:outline-none focus:ring-0 focus:border-${inputColor} peer`;
+    const standardStyles = `block py-${sizing === "sm" ? "2" : "2.5"} px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-${inputColor} appearance-none dark:text-white dark:border-${inputColor} dark:focus:border-${inputColor} focus:outline-none focus:ring-0 focus:border-${inputColor} peer`;
 
-        let buttonTheme;
+    let buttonTheme;
 
         if (buttonStyle === 'filled') {
             buttonTheme = filledStyles;
@@ -513,4 +513,4 @@ export const FloatingLabel = forwardRef<HTMLInputElement, FloatingLabelProps>(
   },
 );
 
-FloatingLabel.displayName = 'FloatingLabel';
+FloatingLabel.displayName = "FloatingLabel";
