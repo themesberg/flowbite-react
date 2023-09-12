789428b3b6f7546d88fde689442f0ace384253b8
okellomarvinkevin <marvin.okello@sophany.it>
2023-09-12 15:58:52 +0200

    formatted the code with prettier

--- app/docs/components/floating-label/index.tsx
+++ app/docs/components/floating-label/index.tsx
@@ -1,7 +1,7 @@
-'use client'
+'use client';
 
 import type { FC } from 'react';
 import FloatingLabelDocs from './floating-label.mdx';
 
-const FloatingLabelContent: FC = () => <FloatingLabelDocs />
-export default  FloatingLabelContent;
+const FloatingLabelContent: FC = () => <FloatingLabelDocs />;
+export default FloatingLabelContent;
