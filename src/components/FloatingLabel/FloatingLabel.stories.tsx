e339f5e5a390e9deced6b3084af9a33dcbd0f316
okellomarvinkevin <marvin.okello@sophany.it>
2023-09-15 11:47:07 +0200

    fix: updated code to fix the validation styles

--- src/components/FloatingLabel/FloatingLabel.stories.tsx
+++ src/components/FloatingLabel/FloatingLabel.stories.tsx
@@ -46,14 +46,12 @@ DisabledStandard.args = {
 };
 
 export const FilledSuccess = Template.bind({});
-FilledSuccess.storyName = 'Filled success';
 FilledSuccess.args = {
   color: 'success',
   variant: 'filled',
   label: 'Label',
 };
 export const OutlinedSuccess = Template.bind({});
-OutlinedSuccess.storyName = 'Outlined success';
 OutlinedSuccess.args = {
   color: 'success',
   variant: 'outlined',
