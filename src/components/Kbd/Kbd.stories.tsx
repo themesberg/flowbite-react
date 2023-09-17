2d597bb72c5ae72db1d4ff821db29f0b10b6be92
Mateo Wartelle <mateowartelle@gmail.com>
2023-09-17 15:39:21 -0500

    feat: kbd basic tests, storybook withIcon + onlyIcon

--- src/components/Kbd/Kbd.stories.tsx
+++ src/components/Kbd/Kbd.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryFn } from '@storybook/react';
-import { MdKeyboardArrowDown } from 'react-icons/md';
+import { MdKeyboardArrowDown, MdKeyboardCommandKey } from 'react-icons/md';
 import type { KbdProps } from './Kbd';
 import { Kbd } from './Kbd';
 
@@ -14,7 +14,15 @@ Default.args = {
   children: <>Shift</>,
 };
 
-export const WithIcon = Template.bind({});
-WithIcon.args = {
+export const OnlyIcon = Template.bind({});
+OnlyIcon.storyName = 'Only icon';
+OnlyIcon.args = {
   icon: MdKeyboardArrowDown,
+};
+
+export const WithIcon = Template.bind({});
+WithIcon.storyName = 'With icon';
+WithIcon.args = {
+  icon: MdKeyboardCommandKey,
+  children: <>command</>,
 };
