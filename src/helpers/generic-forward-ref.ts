b62160116b26180023f15ed07804e9a48600180b
Ignacio Dominguez <ignacio.dominguez@zego.com>
2023-09-18 18:58:36 +0200

    fix(button): fix error Button cannot be used as a JSX component (#974)
    Fix error after updating to v0.6.0 Button cannot be used as a JSX component. Its return type
    'ReactNode' is not a valid JSX element.
    
    fix #962

--- src/helpers/generic-forward-ref.ts
+++ src/helpers/generic-forward-ref.ts
@@ -5,8 +5,8 @@ import { forwardRef } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type FixedForwardRef = <T, P = {}>(
-  render: (props: P, ref: React.Ref<T>) => React.ReactNode,
-) => (props: P & React.RefAttributes<T>) => React.ReactNode;
+  render: (props: P, ref: React.Ref<T>) => JSX.Element,
+) => (props: P & React.RefAttributes<T>) => JSX.Element;
 
 const genericForwardRef = forwardRef as FixedForwardRef;
 
