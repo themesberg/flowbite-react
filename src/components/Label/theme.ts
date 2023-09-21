aca8178e82e9675def998f782e37152b9caf3064
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2023-09-21 11:27:43 +0300

    fix(labels): default dark color of the input labels should be white

--- src/components/Label/theme.ts
+++ src/components/Label/theme.ts
@@ -5,7 +5,7 @@ export const labelTheme: FlowbiteLabelTheme = {
     base: 'text-sm font-medium',
     disabled: 'opacity-50',
     colors: {
-      default: 'text-gray-900 dark:text-gray-300',
+      default: 'text-gray-900 dark:text-white',
       info: 'text-cyan-500 dark:text-cyan-600',
       failure: 'text-red-700 dark:text-red-500',
       warning: 'text-yellow-500 dark:text-yellow-600',
