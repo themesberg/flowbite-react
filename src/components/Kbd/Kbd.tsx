cd61cabf46e4fb8dc4b3fa205a22084fd3151189
Mateo Wartelle <Mateowartelle@gmail.com>
2023-09-21 11:47:17 -0500

    feat : KBD component (#969)
    * feat: kBD (Keyboard) component
    
    * feat: add kbd story (default, withIcon)
    
    * feat: remove extra sentence + flex wrap letter keys
    
    * docs: fix docs
    
    * feat: kbd basic tests, storybook withIcon + onlyIcon
    
    * docs: add missing importExternal for arrow keys docs
    
    * docs: review changes for documentation

---
+++ src/components/Kbd/Kbd.tsx
@@ -0,0 +1,32 @@
+import type { ComponentProps, FC, PropsWithChildren } from 'react';
+import { twMerge } from 'tailwind-merge';
+import type { DeepPartial } from '../../';
+import { useTheme } from '../../';
+import { mergeDeep } from '../../helpers/merge-deep';
+
+export interface FlowbiteKbdTheme {
+  root: FlowbiteKbdRootTheme;
+}
+
+export interface FlowbiteKbdRootTheme {
+  base: string;
+  icon: string;
+}
+
+export interface KbdProps extends PropsWithChildren<ComponentProps<'span'>> {
+  icon?: FC<ComponentProps<'svg'>>;
+  theme?: DeepPartial<FlowbiteKbdTheme>;
+}
+
+export const Kbd: FC<KbdProps> = ({ children, className, icon: Icon, theme: customTheme = {}, ...props }) => {
+  const theme = mergeDeep(useTheme().theme.kbd, customTheme);
+
+  return (
+    <span className={twMerge(theme.root.base, className)} data-testid="flowbite-kbd" {...props}>
+      {Icon && <Icon className={theme.root.icon} data-testid="flowbite-kbd-icon" />}
+      {children}
+    </span>
+  );
+};
+
+Kbd.displayName = 'Kbd';
