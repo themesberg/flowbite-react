9899335580cca8bac7ac71def6637bbfe9be6139
Sebastian Sutu <sebastian.sutu@sensidev.com>
2023-09-25 11:01:30 +0300

    feat: sync all tabs with the latest theme mode value

--- src/helpers/use-theme-mode.ts
+++ src/helpers/use-theme-mode.ts
@@ -11,6 +11,16 @@ export type Mode = 'light' | 'dark';
 export const useThemeMode = () => {
   const [mode, setMode] = useState<Mode>(getInitialMode());
 
+  /**
+   * Sync all tabs with the latest theme mode value
+   */
+  useWatchLSValue({
+    key: LS_THEME_MODE,
+    onChange(newValue) {
+      if (newValue) handleSetMode(newValue as Mode);
+    },
+  });
+
   useEffect(() => {
     setModeInLS(mode);
     setModeOnBody(mode);
@@ -59,4 +69,18 @@ const getInitialMode: () => Mode = () => {
   const LSMode = localStorage.getItem(LS_THEME_MODE) as Mode | undefined;
 
   return LSMode ?? prefersColorScheme();
+};
+
+/**
+ * Triggers `onChange` when another browser tab instance mutates the LS value.
+ */
+const useWatchLSValue = ({ key: watchKey, onChange }: { key: string; onChange(newValue: string | null): void }) => {
+  function handleStorageChange({ key, newValue }: StorageEvent) {
+    if (key === watchKey) onChange(newValue);
+  }
+
+  useEffect(() => {
+    window.addEventListener('storage', handleStorageChange);
+    return () => window.removeEventListener('storage', handleStorageChange);
+  }, []);
 };
