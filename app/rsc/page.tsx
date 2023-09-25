c6ef8084c3065ceffe6a71d49140fb4085f51cbc
Sebastian Sutu <sebastian.sutu@sensidev.com>
2023-09-25 14:44:57 +0300

    update demo RSC page

--- app/rsc/page.tsx
+++ app/rsc/page.tsx
@@ -1,15 +1,14 @@
-import { Flowbite } from '~/src';
-import { Alert } from '~/src/components/Alert';
-import { FileInput } from '~/src/components/FileInput';
+import { Alert, FileInput, Flowbite } from '~/src';
 
 export default function ServerComponent() {
   console.log('RSC powar');
 
   return (
-    <Flowbite theme={{ theme: { alert: { rounded: '4xl' } } }}>
+    <Flowbite theme={{ theme: { alert: { rounded: 'rounded-xl' } } }}>
       <div className="flex flex-col gap-4 p-4">
         <h1 className="textlg">Welcome to RSC, brah</h1>
-        <Alert>alert</Alert>
+        <Alert>alert 1</Alert>
+        <Alert theme={{ rounded: 'rounded-sm' }}>alert 2 (custom theme)</Alert>
         <FileInput />
       </div>
     </Flowbite>
