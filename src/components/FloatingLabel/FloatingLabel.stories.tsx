68a4ed99b0240cb4ea60b9aaba2b5851f06b6536
okellomarvinkevin <marvin.okello@sophany.it>
2023-09-29 18:49:02 +0200

    fixes after rebase

--- src/components/FloatingLabel/FloatingLabel.stories.tsx
+++ src/components/FloatingLabel/FloatingLabel.stories.tsx
@@ -9,67 +9,103 @@ export default {
 
 const Template: Story<FloatingLabelProps> = (args) => <FloatingLabel {...args} />;
 
+export const DefaultFilled = Template.bind({});
+DefaultFilled.args = {
+  variant: 'filled',
+  label: 'Label',
+};
+export const DefaultOutlined = Template.bind({});
+DefaultOutlined.args = {
+  variant: 'outlined',
+  label: 'Label',
+};
+export const DefaultStandard = Template.bind({});
+DefaultStandard.args = {
+  variant: 'standard',
+  label: 'Label',
+};
+
+export const DisabledFilled = Template.bind({});
+DisabledFilled.args = {
+  variant: 'filled',
+  label: 'Label',
+  disabled: true,
+};
+export const DisabledOutlined = Template.bind({});
+DisabledOutlined.args = {
+  variant: 'outlined',
+  label: 'Label',
+  disabled: true,
+};
+
+export const DisabledStandard = Template.bind({});
+DisabledStandard.args = {
+  variant: 'standard',
+  label: 'Label',
+  disabled: true,
+};
+
 export const FilledSuccess = Template.bind({});
 FilledSuccess.args = {
-  error: false,
-  buttonStyle: 'filled',
+  color: 'success',
+  variant: 'filled',
   label: 'Label',
 };
 export const OutlinedSuccess = Template.bind({});
 OutlinedSuccess.args = {
-  error: false,
-  buttonStyle: 'outlined',
+  color: 'success',
+  variant: 'outlined',
   label: 'Label',
 };
 export const StandardSuccess = Template.bind({});
 StandardSuccess.args = {
-  error: false,
-  buttonStyle: 'standard',
+  color: 'success',
+  variant: 'standard',
   label: 'Label',
 };
 export const FilledError = Template.bind({});
 FilledError.args = {
-  error: true,
-  buttonStyle: 'filled',
+  color: 'error',
+  variant: 'filled',
   label: 'Label',
 };
 export const OutlinedError = Template.bind({});
 OutlinedError.args = {
-  error: true,
-  buttonStyle: 'outlined',
+  color: 'error',
+  variant: 'outlined',
   label: 'Label',
 };
 export const StandardError = Template.bind({});
 
 StandardError.args = {
-  error: true,
-  buttonStyle: 'standard',
+  color: 'error',
+  variant: 'standard',
   label: 'Label',
 };
 
 export const SmallFilled = Template.bind({});
 SmallFilled.args = {
-  buttonStyle: 'filled',
+  variant: 'filled',
   label: 'Small Filled',
   sizing: 'sm',
 };
 export const SmallOutlined = Template.bind({});
 SmallOutlined.args = {
-  buttonStyle: 'outlined',
+  variant: 'outlined',
   label: 'Small Outlined',
   sizing: 'sm',
 };
 
 export const SmallStandard = Template.bind({});
 SmallStandard.args = {
-  buttonStyle: 'standard',
+  variant: 'standard',
   label: 'Small Standard',
   sizing: 'sm',
 };
 
 export const HelperText = Template.bind({});
 HelperText.args = {
-  buttonStyle: 'filled',
+  variant: 'filled',
   label: 'Floating Helper',
   helperText: 'Remember, contributions to this topic should follow our Community Guidelines.',
 };
