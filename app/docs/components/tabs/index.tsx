ef4d6a4ead9253b594586a1fce858f540219a778
Ricardo Lüders <rluders@redhat.com>
2023-09-30 19:22:26 +0200

    docs: adds tab custom theme example

--- app/docs/components/tabs/index.tsx
+++ app/docs/components/tabs/index.tsx
@@ -3,17 +3,33 @@
 import type { FC } from 'react';
 import { useRef, useState } from 'react';
 import { DocsContentLayout } from '~/app/components/docs-content-layout';
-import type { TabsRef } from '~/src';
+import type { CustomFlowbiteTheme, TabsRef } from '~/src';
 import TabsDocs from './tabs.mdx';
 
 const TabsPage: FC = () => {
   const [activeTab, setActiveTab] = useState<number>(0);
   const tabsRef = useRef<TabsRef>(null);
 
+  const customTheme: CustomFlowbiteTheme['tab'] = {
+    tablist: {
+      tabitem: {
+        styles: {
+          default: {
+            active: {
+              on: 'bg-blue-100 text-blue-600 dark:bg-blue-800 dark:text-blue-500',
+              off: 'text-gray-500 hover:bg-blue-50 hover:text-blue-600 dark:text-blue-400 dark:hover:bg-blue-800  dark:hover:text-blue-300',
+            },
+          },
+        },
+      },
+    },
+  };
+
   const state = {
     activeTab,
     setActiveTab,
     tabsRef,
+    customTheme,
   };
 
   return (
