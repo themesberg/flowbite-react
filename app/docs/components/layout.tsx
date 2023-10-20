2bcd3f9ead04a4ad3990198b3c203c73bac5df87
Sebastian Sutu <sebastian.sutu@sensidev.com>
2023-10-20 23:07:51 +0300

    remove unused layout

--- app/docs/components/layout.tsx
+++
@@ -1,7 +0,0 @@
-import type { FC, PropsWithChildren } from 'react';
-
-const ComponentDocsLayout: FC<PropsWithChildren> = ({ children }) => {
-  return <div className="mx-auto flex w-full max-w-4xl flex-col gap-8 dark:text-white">{children}</div>;
-};
-
-export default ComponentDocsLayout;
