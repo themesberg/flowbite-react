ed23b6c37b2881f5cd856537766ab79020cf74cc
Sebastian Sutu <sebastian.sutu@sensidev.com>
2023-10-20 23:34:56 +0300

    add back `remark-toc`

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,6 +1,7 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
+import remarkToc from 'remark-toc';
 
 export const Doc = defineDocumentType(() => ({
   name: 'Doc',
@@ -29,5 +30,6 @@ export default makeSource({
   documentTypes: [Doc],
   mdx: {
     rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
+    remarkPlugins: [remarkToc],
   },
 });
