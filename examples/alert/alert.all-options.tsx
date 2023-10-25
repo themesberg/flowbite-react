721f6c9b3d5a0ac8a4b59551b35b40e14ceef7da
Sebastian Sutu <sebastian.sutu@sensidev.com>
2023-10-25 09:53:45 +0300

    fix alert.all-options content

--- examples/alert/alert.all-options.tsx
+++ examples/alert/alert.all-options.tsx
@@ -10,7 +10,13 @@ import { Alert } from 'flowbite-react';
 
 function Component() {
   return (
-    <Alert color="failure" icon={HiInformationCircle}>
+    <Alert
+      additionalContent={<ExampleAdditionalContent />}
+      color="success"
+      icon={HiInformationCircle}
+      onDismiss={() => alert('Alert dismissed!')}
+      rounded
+    >
       <span>
         <span className="font-medium">Info alert!</span> Change a few things up and try submitting again.
       </span>
