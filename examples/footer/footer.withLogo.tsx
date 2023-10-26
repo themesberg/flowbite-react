ace90ab9131dd9688f82625c6ff8a66186482601
Sebastian Sutu <sebastian.sutu@sensidev.com>
2023-10-26 17:06:53 +0300

    fix: docs - footer

---
+++ examples/footer/footer.withLogo.tsx
@@ -0,0 +1,105 @@
+import { type CodeData } from '~/app/components/code-demo';
+import { Footer, FooterBrand, FooterCopyright, FooterDivider, FooterLink, FooterLinkGroup } from '~/src';
+
+const code = `
+'use client';
+
+import { Footer } from 'flowbite-react';
+
+function Component() {
+  return (
+    <Footer container>
+      <div className="w-full text-center">
+        <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
+          <Footer.Brand
+            href="https://flowbite.com"
+            src="https://flowbite.com/docs/images/logo.svg"
+            alt="Flowbite Logo"
+            name="Flowbite"
+          />
+          <Footer.LinkGroup>
+            <Footer.Link href="#">About</Footer.Link>
+            <Footer.Link href="#">Privacy Policy</Footer.Link>
+            <Footer.Link href="#">Licensing</Footer.Link>
+            <Footer.Link href="#">Contact</Footer.Link>
+          </Footer.LinkGroup>
+        </div>
+        <Footer.Divider />
+        <Footer.Copyright href="#" by="Flowbite™" year={2022} />
+      </div>
+    </Footer>
+  );
+}
+`;
+
+const codeRSC = `
+import { Footer, FooterBrand, FooterCopyright, FooterDivider, FooterLink, FooterLinkGroup } from 'flowbite-react';
+
+function Component() {
+  return (
+    <Footer container>
+      <div className="w-full text-center">
+        <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
+          <FooterBrand
+            href="https://flowbite.com"
+            src="https://flowbite.com/docs/images/logo.svg"
+            alt="Flowbite Logo"
+            name="Flowbite"
+          />
+          <FooterLinkGroup>
+            <FooterLink href="#">About</FooterLink>
+            <FooterLink href="#">Privacy Policy</FooterLink>
+            <FooterLink href="#">Licensing</FooterLink>
+            <FooterLink href="#">Contact</FooterLink>
+          </FooterLinkGroup>
+        </div>
+        <FooterDivider />
+        <FooterCopyright href="#" by="Flowbite™" year={2022} />
+      </div>
+    </Footer>
+  );
+}
+`;
+
+function Component() {
+  return (
+    <Footer container>
+      <div className="w-full text-center">
+        <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
+          <FooterBrand
+            href="https://flowbite.com"
+            src="https://flowbite.com/docs/images/logo.svg"
+            alt="Flowbite Logo"
+            name="Flowbite"
+          />
+          <FooterLinkGroup>
+            <FooterLink href="#">About</FooterLink>
+            <FooterLink href="#">Privacy Policy</FooterLink>
+            <FooterLink href="#">Licensing</FooterLink>
+            <FooterLink href="#">Contact</FooterLink>
+          </FooterLinkGroup>
+        </div>
+        <FooterDivider />
+        <FooterCopyright href="#" by="Flowbite™" year={2022} />
+      </div>
+    </Footer>
+  );
+}
+
+export const withLogo: CodeData = {
+  type: 'single',
+  code: [
+    {
+      fileName: 'client',
+      language: 'tsx',
+      code,
+    },
+    {
+      fileName: 'server',
+      language: 'tsx',
+      code: codeRSC,
+    },
+  ],
+  githubSlug: 'components/footer.md#footer-with-logo',
+  component: <Component />,
+};
--- examples/index.ts
+++ examples/index.ts
@@ -3,4 +3,5 @@ export * as alert from './alert';
 export * as avatar from './avatar';
 export * as badge from './badge';
 export * as breadcrumb from './breadcrumb';
+export * as footer from './footer';
 export * as navbar from './navbar';
