6fdc28cd33a859bb0281a63c92338480cdeca708
Sebastian Sutu <sebastian.sutu@sensidev.com>
2023-10-28 14:12:39 +0300

    fix: docs - tabs

---
+++ examples/tabs/tabs.withUnderline.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import { HiAdjustments, HiClipboardList, HiUserCircle } from 'react-icons/hi';
+import { MdDashboard } from 'react-icons/md';
+import { type CodeData } from '~/app/components/code-demo';
+import { Tabs } from '~/src';
+
+const code = `
+import { Tabs } from 'flowbite-react';
+import { HiAdjustments, HiClipboardList, HiUserCircle } from 'react-icons/hi';
+import { MdDashboard } from 'react-icons/md';
+
+function Component() {
+  return (
+    <Tabs aria-label="Tabs with underline" style="underline">
+      <Tabs.Item active title="Profile" icon={HiUserCircle}>
+        This is <span className="font-medium text-gray-800 dark:text-white">Profile tab's associated content</span>.
+        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+        control the content visibility and styling.
+      </Tabs.Item>
+      <Tabs.Item title="Dashboard" icon={MdDashboard}>
+        This is <span className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</span>.
+        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+        control the content visibility and styling.
+      </Tabs.Item>
+      <Tabs.Item title="Settings" icon={HiAdjustments}>
+        This is <span className="font-medium text-gray-800 dark:text-white">Settings tab's associated content</span>.
+        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+        control the content visibility and styling.
+      </Tabs.Item>
+      <Tabs.Item title="Contacts" icon={HiClipboardList}>
+        This is <span className="font-medium text-gray-800 dark:text-white">Contacts tab's associated content</span>.
+        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+        control the content visibility and styling.
+      </Tabs.Item>
+      <Tabs.Item disabled title="Disabled">
+        Disabled content
+      </Tabs.Item>
+    </Tabs>
+  );
+}
+`;
+
+function Component() {
+  return (
+    <Tabs aria-label="Tabs with underline" style="underline">
+      <Tabs.Item active title="Profile" icon={HiUserCircle}>
+        This is <span className="font-medium text-gray-800 dark:text-white">Profile tab's associated content</span>.
+        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+        control the content visibility and styling.
+      </Tabs.Item>
+      <Tabs.Item title="Dashboard" icon={MdDashboard}>
+        This is <span className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</span>.
+        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+        control the content visibility and styling.
+      </Tabs.Item>
+      <Tabs.Item title="Settings" icon={HiAdjustments}>
+        This is <span className="font-medium text-gray-800 dark:text-white">Settings tab's associated content</span>.
+        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+        control the content visibility and styling.
+      </Tabs.Item>
+      <Tabs.Item title="Contacts" icon={HiClipboardList}>
+        This is <span className="font-medium text-gray-800 dark:text-white">Contacts tab's associated content</span>.
+        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+        control the content visibility and styling.
+      </Tabs.Item>
+      <Tabs.Item disabled title="Disabled">
+        Disabled content
+      </Tabs.Item>
+    </Tabs>
+  );
+}
+
+export const withUnderline: CodeData = {
+  type: 'single',
+  code: {
+    fileName: 'client',
+    language: 'tsx',
+    code,
+  },
+  githubSlug: 'tabs/tabs.withUnderline.tsx',
+  component: <Component />,
+};
