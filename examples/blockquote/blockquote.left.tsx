401ca9f78f8d112cd96c5778ecd47fff04e66301
Sebastian Sutu <sebastian.sutu@sensidev.com>
2023-10-30 16:06:39 +0200

    fix: docs - blockquote

---
+++ examples/blockquote/blockquote.left.tsx
@@ -0,0 +1,57 @@
+import { type CodeData } from '~/app/components/code-demo';
+import { Blockquote } from '~/src';
+
+const code = `
+'use client';
+
+import { Blockquote } from 'flowbite-react';
+
+function Component() {
+  return (
+    <Blockquote>
+      "Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to
+      complex dashboard. Perfect choice for your next SaaS application."
+    </Blockquote>
+  );
+}
+`;
+
+const codeRSC = `
+import { Blockquote } from 'flowbite-react';
+
+function Component() {
+  return (
+    <Blockquote>
+      "Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to
+      complex dashboard. Perfect choice for your next SaaS application."
+    </Blockquote>
+  );
+}
+`;
+
+function Component() {
+  return (
+    <Blockquote>
+      "Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to
+      complex dashboard. Perfect choice for your next SaaS application."
+    </Blockquote>
+  );
+}
+
+export const left: CodeData = {
+  type: 'single',
+  code: [
+    {
+      fileName: 'client',
+      language: 'tsx',
+      code,
+    },
+    {
+      fileName: 'server',
+      language: 'tsx',
+      code: codeRSC,
+    },
+  ],
+  githubSlug: 'blockquote/blockquote.left.tsx',
+  component: <Component />,
+};
