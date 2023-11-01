ea596ddbe2bcf2a9f768d1c4e1e4be9b55978116
Sebastian Sutu <sebastian.sutu@sensidev.com>
2023-11-01 10:59:58 +0200

    homepage: remove redundant div

--- app/page.tsx
+++ app/page.tsx
@@ -18,19 +18,17 @@ export default function HomePageContent() {
     <div className="relative bg-white text-gray-600 antialiased dark:bg-gray-900 dark:text-gray-400">
       <Banner />
       <HomeNavbar />
-      <div className="lg:flex">
-        <main className="min-w-0 flex-auto divide-y dark:divide-gray-700">
-          <HeroSection />
-          <FeaturedSection />
-          <ComponentsSection />
-          <ReactSection />
-          <DarkModeSection />
-          <TailwindSection />
-          <SocialProofSection />
-          <FigmaSection />
-          <ContributorsSection />
-        </main>
-      </div>
+      <main className="min-w-0 flex-auto divide-y dark:divide-gray-700">
+        <HeroSection />
+        <FeaturedSection />
+        <ComponentsSection />
+        <ReactSection />
+        <DarkModeSection />
+        <TailwindSection />
+        <SocialProofSection />
+        <FigmaSection />
+        <ContributorsSection />
+      </main>
       <MainFooter />
     </div>
   );
