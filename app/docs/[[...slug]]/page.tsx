e5636a6667eab48a5ff1e709721287dc8a56ec77
Sutu Sebastian <41998826+SutuSebastian@users.noreply.github.com>
2023-11-07 17:14:15 +0200

    Docs: add open graph meta tags (#1111)
    docs - add open graph meta tags
    
    Co-authored-by: Sebastian Sutu <sebastian.sutu@stainless-code.com>

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -29,6 +29,18 @@ export function generateMetadata({ params }: Props): Metadata {
   return {
     title: doc.title,
     description: doc.description,
+    openGraph: {
+      type: 'article',
+      title: doc.title,
+      description: doc.description,
+      images: 'https://flowbite.s3.amazonaws.com/github/flowbite-react.png',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: doc.title,
+      description: doc.description,
+      images: ['https://flowbite.s3.amazonaws.com/github/flowbite-react.png'],
+    },
   };
 }
 
