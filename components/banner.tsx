c0487db31e6ab6fd71f94969016795e194301a61
Sutu Sebastian <41998826+SutuSebastian@users.noreply.github.com>
2023-11-15 17:20:59 +0200

    docs: Server components page + banner update (#1134)
    docs: create `server-components` page; update sidebar; update banner with new RSC support
    
    Co-authored-by: Sebastian Sutu <sebastian.sutu@stainless-code.com>

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import type { FC } from 'react';
 
 export const Banner: FC = () => {
@@ -9,16 +10,14 @@ export const Banner: FC = () => {
       <div className="items-center md:flex">
         <p className="text-sm font-medium text-gray-900 dark:text-white md:my-0">
           <span className="mr-2 hidden rounded bg-cyan-100 px-2.5 py-0.5 text-xs font-semibold text-cyan-800 dark:bg-cyan-200 dark:text-cyan-800 md:inline">
-            Community
+            New
           </span>
-          🗳️ Help us by voting and providing feedback for supporting Server Components in Next.js!
-          <a
+          Flowbite React now supports Server Components and has full Next.js App Router support!
+          <Link
             className="ml-2 inline-flex items-center text-sm font-medium text-cyan-600 hover:underline dark:text-cyan-500 md:ml-2"
-            href="https://github.com/themesberg/flowbite-react/discussions/940"
-            target="_blank"
-            rel="nofollow noreferred noopener"
+            href="/docs/getting-started/server-components"
           >
-            Check discussion on GitHub
+            Check it out
             <svg
               className="ml-1.5 h-3 w-3 text-cyan-600 dark:text-cyan-500"
               aria-hidden="true"
@@ -34,7 +33,7 @@ export const Banner: FC = () => {
                 d="M1 5h12m0 0L9 1m4 4L9 9"
               />
             </svg>
-          </a>
+          </Link>
         </p>
       </div>
     </div>
