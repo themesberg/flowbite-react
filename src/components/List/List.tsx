8550e7ec370b7acd2cf7da9a18c2ef9fe4d5e0d1
fatemeh paghar <66066475+paghar@users.noreply.github.com>
2023-11-20 11:09:34 +0100

    Fix/list props warning (#1138)
    fix(list.tsx): define props as components argument

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -32,16 +32,24 @@ export interface ListProps extends PropsWithChildren<ComponentProps<'ul'> & Comp
   nested?: boolean;
 }
 
-const ListComponent: FC<ListProps> = ({ children, className, theme: customTheme = {}, ...props }) => {
+const ListComponent: FC<ListProps> = ({
+  children,
+  className,
+  unstyled,
+  nested,
+  ordered,
+  theme: customTheme = {},
+  ...props
+}) => {
   const theme = mergeDeep(getTheme().list, customTheme);
-  const Component = props.ordered ? 'ol' : 'ul';
+  const Component = ordered ? 'ol' : 'ul';
 
   return (
     <Component
       className={twMerge(
-        theme.root.ordered[props.ordered ? 'on' : 'off'],
-        props.unstyled && theme.root.unstyled,
-        props.nested && theme.root.nested,
+        theme.root.ordered[ordered ? 'on' : 'off'],
+        unstyled && theme.root.unstyled,
+        nested && theme.root.nested,
         theme.root.base,
         className,
       )}
