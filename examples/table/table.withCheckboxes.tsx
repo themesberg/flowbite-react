0606246144c809eb97cb294cbdbce1a088a6531a
Sebastian Sutu <sebastian.sutu@stainless-code.com>
2023-11-23 11:59:21 +0200

    docs(table): fix page overflow

--- examples/table/table.withCheckboxes.tsx
+++ examples/table/table.withCheckboxes.tsx
@@ -8,68 +8,70 @@ import { Checkbox, Table } from 'flowbite-react';
 
 function Component() {
   return (
-    <Table hoverable>
-      <Table.Head>
-        <Table.HeadCell className="p-4">
-          <Checkbox />
-        </Table.HeadCell>
-        <Table.HeadCell>Product name</Table.HeadCell>
-        <Table.HeadCell>Color</Table.HeadCell>
-        <Table.HeadCell>Category</Table.HeadCell>
-        <Table.HeadCell>Price</Table.HeadCell>
-        <Table.HeadCell>
-          <span className="sr-only">Edit</span>
-        </Table.HeadCell>
-      </Table.Head>
-      <Table.Body className="divide-y">
-        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-          <Table.Cell className="p-4">
+    <div className="overflow-x-auto">
+      <Table hoverable>
+        <Table.Head>
+          <Table.HeadCell className="p-4">
             <Checkbox />
-          </Table.Cell>
-          <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-            {'Apple MacBook Pro 17"'}
-          </Table.Cell>
-          <Table.Cell>Sliver</Table.Cell>
-          <Table.Cell>Laptop</Table.Cell>
-          <Table.Cell>$2999</Table.Cell>
-          <Table.Cell>
-            <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-              Edit
-            </a>
-          </Table.Cell>
-        </Table.Row>
-        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-          <Table.Cell className="p-4">
-            <Checkbox />
-          </Table.Cell>
-          <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-            Microsoft Surface Pro
-          </Table.Cell>
-          <Table.Cell>White</Table.Cell>
-          <Table.Cell>Laptop PC</Table.Cell>
-          <Table.Cell>$1999</Table.Cell>
-          <Table.Cell>
-            <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-              Edit
-            </a>
-          </Table.Cell>
-        </Table.Row>
-        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-          <Table.Cell className="p-4">
-            <Checkbox />
-          </Table.Cell>
-          <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Magic Mouse 2</Table.Cell>
-          <Table.Cell>Black</Table.Cell>
-          <Table.Cell>Accessories</Table.Cell>
-          <Table.Cell>$99</Table.Cell>
-          <Table.Cell>
-            <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-              Edit
-            </a>
-          </Table.Cell>
-        </Table.Row>
-      </Table.Body>
-    </Table>
+          </Table.HeadCell>
+          <Table.HeadCell>Product name</Table.HeadCell>
+          <Table.HeadCell>Color</Table.HeadCell>
+          <Table.HeadCell>Category</Table.HeadCell>
+          <Table.HeadCell>Price</Table.HeadCell>
+          <Table.HeadCell>
+            <span className="sr-only">Edit</span>
+          </Table.HeadCell>
+        </Table.Head>
+        <Table.Body className="divide-y">
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="p-4">
+              <Checkbox />
+            </Table.Cell>
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {'Apple MacBook Pro 17"'}
+            </Table.Cell>
+            <Table.Cell>Sliver</Table.Cell>
+            <Table.Cell>Laptop</Table.Cell>
+            <Table.Cell>$2999</Table.Cell>
+            <Table.Cell>
+              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                Edit
+              </a>
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="p-4">
+              <Checkbox />
+            </Table.Cell>
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              Microsoft Surface Pro
+            </Table.Cell>
+            <Table.Cell>White</Table.Cell>
+            <Table.Cell>Laptop PC</Table.Cell>
+            <Table.Cell>$1999</Table.Cell>
+            <Table.Cell>
+              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                Edit
+              </a>
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="p-4">
+              <Checkbox />
+            </Table.Cell>
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Magic Mouse 2</Table.Cell>
+            <Table.Cell>Black</Table.Cell>
+            <Table.Cell>Accessories</Table.Cell>
+            <Table.Cell>$99</Table.Cell>
+            <Table.Cell>
+              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                Edit
+              </a>
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    </div>
   );
 }
 `;
@@ -79,136 +81,140 @@ import { Checkbox, Table, TableBody, TableCell, TableHead, TableHeadCell, TableR
 
 function Component() {
   return (
-    <Table hoverable>
-      <TableHead>
-        <TableHeadCell className="p-4">
-          <Checkbox />
-        </TableHeadCell>
-        <TableHeadCell>Product name</TableHeadCell>
-        <TableHeadCell>Color</TableHeadCell>
-        <TableHeadCell>Category</TableHeadCell>
-        <TableHeadCell>Price</TableHeadCell>
-        <TableHeadCell>
-          <span className="sr-only">Edit</span>
-        </TableHeadCell>
-      </TableHead>
-      <TableBody className="divide-y">
-        <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-          <TableCell className="p-4">
+    <div className="overflow-x-auto">
+      <Table hoverable>
+        <TableHead>
+          <TableHeadCell className="p-4">
             <Checkbox />
-          </TableCell>
-          <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-            {'Apple MacBook Pro 17"'}
-          </TableCell>
-          <TableCell>Sliver</TableCell>
-          <TableCell>Laptop</TableCell>
-          <TableCell>$2999</TableCell>
-          <TableCell>
-            <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-              Edit
-            </a>
-          </TableCell>
-        </TableRow>
-        <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-          <TableCell className="p-4">
-            <Checkbox />
-          </TableCell>
-          <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-            Microsoft Surface Pro
-          </TableCell>
-          <TableCell>White</TableCell>
-          <TableCell>Laptop PC</TableCell>
-          <TableCell>$1999</TableCell>
-          <TableCell>
-            <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-              Edit
-            </a>
-          </TableCell>
-        </TableRow>
-        <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-          <TableCell className="p-4">
-            <Checkbox />
-          </TableCell>
-          <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Magic Mouse 2</TableCell>
-          <TableCell>Black</TableCell>
-          <TableCell>Accessories</TableCell>
-          <TableCell>$99</TableCell>
-          <TableCell>
-            <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-              Edit
-            </a>
-          </TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+          </TableHeadCell>
+          <TableHeadCell>Product name</TableHeadCell>
+          <TableHeadCell>Color</TableHeadCell>
+          <TableHeadCell>Category</TableHeadCell>
+          <TableHeadCell>Price</TableHeadCell>
+          <TableHeadCell>
+            <span className="sr-only">Edit</span>
+          </TableHeadCell>
+        </TableHead>
+        <TableBody className="divide-y">
+          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <TableCell className="p-4">
+              <Checkbox />
+            </TableCell>
+            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {'Apple MacBook Pro 17"'}
+            </TableCell>
+            <TableCell>Sliver</TableCell>
+            <TableCell>Laptop</TableCell>
+            <TableCell>$2999</TableCell>
+            <TableCell>
+              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                Edit
+              </a>
+            </TableCell>
+          </TableRow>
+          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <TableCell className="p-4">
+              <Checkbox />
+            </TableCell>
+            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              Microsoft Surface Pro
+            </TableCell>
+            <TableCell>White</TableCell>
+            <TableCell>Laptop PC</TableCell>
+            <TableCell>$1999</TableCell>
+            <TableCell>
+              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                Edit
+              </a>
+            </TableCell>
+          </TableRow>
+          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <TableCell className="p-4">
+              <Checkbox />
+            </TableCell>
+            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Magic Mouse 2</TableCell>
+            <TableCell>Black</TableCell>
+            <TableCell>Accessories</TableCell>
+            <TableCell>$99</TableCell>
+            <TableCell>
+              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                Edit
+              </a>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
   );
 }
 `;
 
 function Component() {
   return (
-    <Table hoverable>
-      <TableHead>
-        <TableHeadCell className="p-4">
-          <Checkbox />
-        </TableHeadCell>
-        <TableHeadCell>Product name</TableHeadCell>
-        <TableHeadCell>Color</TableHeadCell>
-        <TableHeadCell>Category</TableHeadCell>
-        <TableHeadCell>Price</TableHeadCell>
-        <TableHeadCell>
-          <span className="sr-only">Edit</span>
-        </TableHeadCell>
-      </TableHead>
-      <TableBody className="divide-y">
-        <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-          <TableCell className="p-4">
+    <div className="overflow-x-auto">
+      <Table hoverable>
+        <TableHead>
+          <TableHeadCell className="p-4">
             <Checkbox />
-          </TableCell>
-          <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-            {'Apple MacBook Pro 17"'}
-          </TableCell>
-          <TableCell>Sliver</TableCell>
-          <TableCell>Laptop</TableCell>
-          <TableCell>$2999</TableCell>
-          <TableCell>
-            <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-              Edit
-            </a>
-          </TableCell>
-        </TableRow>
-        <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-          <TableCell className="p-4">
-            <Checkbox />
-          </TableCell>
-          <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-            Microsoft Surface Pro
-          </TableCell>
-          <TableCell>White</TableCell>
-          <TableCell>Laptop PC</TableCell>
-          <TableCell>$1999</TableCell>
-          <TableCell>
-            <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-              Edit
-            </a>
-          </TableCell>
-        </TableRow>
-        <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-          <TableCell className="p-4">
-            <Checkbox />
-          </TableCell>
-          <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Magic Mouse 2</TableCell>
-          <TableCell>Black</TableCell>
-          <TableCell>Accessories</TableCell>
-          <TableCell>$99</TableCell>
-          <TableCell>
-            <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-              Edit
-            </a>
-          </TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+          </TableHeadCell>
+          <TableHeadCell>Product name</TableHeadCell>
+          <TableHeadCell>Color</TableHeadCell>
+          <TableHeadCell>Category</TableHeadCell>
+          <TableHeadCell>Price</TableHeadCell>
+          <TableHeadCell>
+            <span className="sr-only">Edit</span>
+          </TableHeadCell>
+        </TableHead>
+        <TableBody className="divide-y">
+          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <TableCell className="p-4">
+              <Checkbox />
+            </TableCell>
+            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {'Apple MacBook Pro 17"'}
+            </TableCell>
+            <TableCell>Sliver</TableCell>
+            <TableCell>Laptop</TableCell>
+            <TableCell>$2999</TableCell>
+            <TableCell>
+              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                Edit
+              </a>
+            </TableCell>
+          </TableRow>
+          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <TableCell className="p-4">
+              <Checkbox />
+            </TableCell>
+            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              Microsoft Surface Pro
+            </TableCell>
+            <TableCell>White</TableCell>
+            <TableCell>Laptop PC</TableCell>
+            <TableCell>$1999</TableCell>
+            <TableCell>
+              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                Edit
+              </a>
+            </TableCell>
+          </TableRow>
+          <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <TableCell className="p-4">
+              <Checkbox />
+            </TableCell>
+            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Magic Mouse 2</TableCell>
+            <TableCell>Black</TableCell>
+            <TableCell>Accessories</TableCell>
+            <TableCell>$99</TableCell>
+            <TableCell>
+              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                Edit
+              </a>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
   );
 }
 
