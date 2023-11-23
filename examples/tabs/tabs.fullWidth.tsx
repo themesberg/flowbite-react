338f74106dd47f57b0c2a919fe33a7f7fb04d93e
Sutu Sebastian <41998826+SutuSebastian@users.noreply.github.com>
2023-11-23 13:42:51 +0200

    docs(tabs): fix page overflow (#1146)
    Co-authored-by: Sebastian Sutu <sebastian.sutu@stainless-code.com>

--- examples/tabs/tabs.fullWidth.tsx
+++ examples/tabs/tabs.fullWidth.tsx
@@ -12,62 +12,66 @@ import { MdDashboard } from 'react-icons/md';
 
 function Component() {
   return (
-    <Tabs aria-label="Full width tabs" style="fullWidth">
-      <Tabs.Item active title="Profile" icon={HiUserCircle}>
-        This is <span className="font-medium text-gray-800 dark:text-white">Profile tab's associated content</span>.
-        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-        control the content visibility and styling.
-      </Tabs.Item>
-      <Tabs.Item title="Dashboard" icon={MdDashboard}>
-        This is <span className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</span>.
-        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-        control the content visibility and styling.
-      </Tabs.Item>
-      <Tabs.Item title="Settings" icon={HiAdjustments}>
-        This is <span className="font-medium text-gray-800 dark:text-white">Settings tab's associated content</span>.
-        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-        control the content visibility and styling.
-      </Tabs.Item>
-      <Tabs.Item title="Contacts" icon={HiClipboardList}>
-        This is <span className="font-medium text-gray-800 dark:text-white">Contacts tab's associated content</span>.
-        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-        control the content visibility and styling.
-      </Tabs.Item>
-      <Tabs.Item disabled title="Disabled">
-        Disabled content
-      </Tabs.Item>
-    </Tabs>
+    <div className="overflow-x-auto">
+      <Tabs aria-label="Full width tabs" style="fullWidth">
+        <Tabs.Item active title="Profile" icon={HiUserCircle}>
+          This is <span className="font-medium text-gray-800 dark:text-white">Profile tab's associated content</span>.
+          Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+          control the content visibility and styling.
+        </Tabs.Item>
+        <Tabs.Item title="Dashboard" icon={MdDashboard}>
+          This is <span className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</span>.
+          Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+          control the content visibility and styling.
+        </Tabs.Item>
+        <Tabs.Item title="Settings" icon={HiAdjustments}>
+          This is <span className="font-medium text-gray-800 dark:text-white">Settings tab's associated content</span>.
+          Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+          control the content visibility and styling.
+        </Tabs.Item>
+        <Tabs.Item title="Contacts" icon={HiClipboardList}>
+          This is <span className="font-medium text-gray-800 dark:text-white">Contacts tab's associated content</span>.
+          Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+          control the content visibility and styling.
+        </Tabs.Item>
+        <Tabs.Item disabled title="Disabled">
+          Disabled content
+        </Tabs.Item>
+      </Tabs>
+    </div>
   );
 }
 `;
 
 function Component() {
   return (
-    <Tabs aria-label="Full width tabs" style="fullWidth">
-      <Tabs.Item active title="Profile" icon={HiUserCircle}>
-        This is <span className="font-medium text-gray-800 dark:text-white">Profile tab's associated content</span>.
-        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-        control the content visibility and styling.
-      </Tabs.Item>
-      <Tabs.Item title="Dashboard" icon={MdDashboard}>
-        This is <span className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</span>.
-        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-        control the content visibility and styling.
-      </Tabs.Item>
-      <Tabs.Item title="Settings" icon={HiAdjustments}>
-        This is <span className="font-medium text-gray-800 dark:text-white">Settings tab's associated content</span>.
-        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-        control the content visibility and styling.
-      </Tabs.Item>
-      <Tabs.Item title="Contacts" icon={HiClipboardList}>
-        This is <span className="font-medium text-gray-800 dark:text-white">Contacts tab's associated content</span>.
-        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-        control the content visibility and styling.
-      </Tabs.Item>
-      <Tabs.Item disabled title="Disabled">
-        Disabled content
-      </Tabs.Item>
-    </Tabs>
+    <div className="overflow-x-auto">
+      <Tabs aria-label="Full width tabs" style="fullWidth">
+        <Tabs.Item active title="Profile" icon={HiUserCircle}>
+          This is <span className="font-medium text-gray-800 dark:text-white">Profile tab's associated content</span>.
+          Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+          control the content visibility and styling.
+        </Tabs.Item>
+        <Tabs.Item title="Dashboard" icon={MdDashboard}>
+          This is <span className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</span>.
+          Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+          control the content visibility and styling.
+        </Tabs.Item>
+        <Tabs.Item title="Settings" icon={HiAdjustments}>
+          This is <span className="font-medium text-gray-800 dark:text-white">Settings tab's associated content</span>.
+          Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+          control the content visibility and styling.
+        </Tabs.Item>
+        <Tabs.Item title="Contacts" icon={HiClipboardList}>
+          This is <span className="font-medium text-gray-800 dark:text-white">Contacts tab's associated content</span>.
+          Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+          control the content visibility and styling.
+        </Tabs.Item>
+        <Tabs.Item disabled title="Disabled">
+          Disabled content
+        </Tabs.Item>
+      </Tabs>
+    </div>
   );
 }
 
