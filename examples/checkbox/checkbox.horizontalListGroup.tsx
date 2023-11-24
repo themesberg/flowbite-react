2bd88466d6f86e7bbe5ee8888e6c5914f2ecf5a6
Mateo Wartelle <mateowartelle@gmail.com>
2023-11-24 08:10:10 -0600

    docs(checkbox): make the whole checkbox area clickable

--- examples/checkbox/checkbox.horizontalListGroup.tsx
+++ examples/checkbox/checkbox.horizontalListGroup.tsx
@@ -13,33 +13,33 @@ function Component() {
       <ul className="w-full items-center rounded-lg border border-gray-200 bg-white text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white sm:flex">
         <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 sm:border-r">
           <div className="flex items-center pl-3">
-            <Checkbox id="vue-js-hlist-checkbox" />
-            <Label htmlFor="vue-js-hlist-checkbox" className="w-100 ml-2 flex py-4">
-              {'Vue JS'}
+            <Label htmlFor="vue-js-hlist-checkbox" className="w-full ml-2 gap-1 flex items-center py-4">
+              <Checkbox id="vue-js-hlist-checkbox" />
+              Vue JS
             </Label>
           </div>
         </li>
         <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 sm:border-r">
           <div className="flex items-center pl-3">
-            <Checkbox id="react-js-hlist-checkbox" />
-            <Label htmlFor="react-js-hlist-checkbox" className="w-100 ml-2 flex py-4">
-              {'React'}
+            <Label htmlFor="react-js-hlist-checkbox" className="w-full ml-2 gap-1 flex items-center py-4">
+              <Checkbox id="react-js-hlist-checkbox" />
+              React
             </Label>
           </div>
         </li>
         <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 sm:border-r">
           <div className="flex items-center pl-3">
-            <Checkbox id="angular-js-hlist-checkbox" />
-            <Label htmlFor="angular-js-hlist-checkbox" className="w-100 ml-2 flex py-4">
-              {'Angular'}
+            <Label htmlFor="angular-js-hlist-checkbox" className="w-full ml-2 gap-1 flex items-center py-4">
+              <Checkbox id="angular-js-hlist-checkbox" />
+              Angular
             </Label>
           </div>
         </li>
         <li className="w-full dark:border-gray-600">
           <div className="flex items-center pl-3">
-            <Checkbox id="laravel-hlist-checkbox" />
-            <Label htmlFor="laravel-hlist-checkbox" className="w-100 ml-2 flex py-4">
-              {'Laravel'}
+            <Label htmlFor="laravel-hlist-checkbox" className="w-full	ml-2 gap-1 flex items-center py-4">
+              <Checkbox id="laravel-hlist-checkbox" />
+              Laravel
             </Label>
           </div>
         </li>
@@ -59,33 +59,33 @@ function Component() {
       <ul className="w-full items-center rounded-lg border border-gray-200 bg-white text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white sm:flex">
         <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 sm:border-r">
           <div className="flex items-center pl-3">
-            <Checkbox id="vue-js-hlist-checkbox" />
-            <Label htmlFor="vue-js-hlist-checkbox" className="w-100 ml-2 flex py-4">
-              {'Vue JS'}
+            <Label htmlFor="vue-js-hlist-checkbox" className="w-full ml-2 gap-1 flex items-center py-4">
+              <Checkbox id="vue-js-hlist-checkbox" />
+              Vue JS
             </Label>
           </div>
         </li>
         <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 sm:border-r">
           <div className="flex items-center pl-3">
-            <Checkbox id="react-js-hlist-checkbox" />
-            <Label htmlFor="react-js-hlist-checkbox" className="w-100 ml-2 flex py-4">
-              {'React'}
+            <Label htmlFor="react-js-hlist-checkbox" className="w-full ml-2 gap-1 flex items-center py-4">
+              <Checkbox id="react-js-hlist-checkbox" />
+              React
             </Label>
           </div>
         </li>
         <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 sm:border-r">
           <div className="flex items-center pl-3">
-            <Checkbox id="angular-js-hlist-checkbox" />
-            <Label htmlFor="angular-js-hlist-checkbox" className="w-100 ml-2 flex py-4">
-              {'Angular'}
+            <Label htmlFor="angular-js-hlist-checkbox" className="w-full ml-2 gap-1 flex items-center py-4">
+              <Checkbox id="angular-js-hlist-checkbox" />
+              Angular
             </Label>
           </div>
         </li>
         <li className="w-full dark:border-gray-600">
           <div className="flex items-center pl-3">
-            <Checkbox id="laravel-hlist-checkbox" />
-            <Label htmlFor="laravel-hlist-checkbox" className="w-100 ml-2 flex py-4">
-              {'Laravel'}
+            <Label htmlFor="laravel-hlist-checkbox" className="w-full	ml-2 gap-1 flex items-center py-4">
+              <Checkbox id="laravel-hlist-checkbox" />
+              Laravel
             </Label>
           </div>
         </li>
@@ -102,32 +102,32 @@ function Component() {
       <ul className="w-full items-center rounded-lg border border-gray-200 bg-white text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white sm:flex">
         <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 sm:border-r">
           <div className="flex items-center pl-3">
-            <Checkbox id="vue-js-hlist-checkbox" />
-            <Label htmlFor="vue-js-hlist-checkbox" className="w-100 ml-2 flex py-4">
+            <Label htmlFor="vue-js-hlist-checkbox" className="w-full ml-2 gap-1 flex items-center py-4">
+              <Checkbox id="vue-js-hlist-checkbox" />
               Vue JS
             </Label>
           </div>
         </li>
         <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 sm:border-r">
           <div className="flex items-center pl-3">
-            <Checkbox id="react-js-hlist-checkbox" />
-            <Label htmlFor="react-js-hlist-checkbox" className="w-100 ml-2 flex py-4">
+            <Label htmlFor="react-js-hlist-checkbox" className="w-full ml-2 gap-1 flex items-center py-4">
+              <Checkbox id="react-js-hlist-checkbox" />
               React
             </Label>
           </div>
         </li>
         <li className="w-full border-b border-gray-200 dark:border-gray-600 sm:border-b-0 sm:border-r">
           <div className="flex items-center pl-3">
-            <Checkbox id="angular-js-hlist-checkbox" />
-            <Label htmlFor="angular-js-hlist-checkbox" className="w-100 ml-2 flex py-4">
+            <Label htmlFor="angular-js-hlist-checkbox" className="w-full ml-2 gap-1 flex items-center py-4">
+              <Checkbox id="angular-js-hlist-checkbox" />
               Angular
             </Label>
           </div>
         </li>
         <li className="w-full dark:border-gray-600">
           <div className="flex items-center pl-3">
-            <Checkbox id="laravel-hlist-checkbox" />
-            <Label htmlFor="laravel-hlist-checkbox" className="w-100 ml-2 flex py-4">
+            <Label htmlFor="laravel-hlist-checkbox" className="w-full	ml-2 gap-1 flex items-center py-4">
+              <Checkbox id="laravel-hlist-checkbox" />
               Laravel
             </Label>
           </div>
--- examples/checkbox/checkbox.listGroup.tsx
+++ examples/checkbox/checkbox.listGroup.tsx
@@ -13,32 +13,32 @@ function Component() {
       <ul className="w-48 rounded-lg border border-gray-200 bg-white text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white">
         <li className="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
           <div className="flex items-center pl-3">
-            <Checkbox id="vue-js-list-checkbox" />
-            <Label htmlFor="vue-js-list-checkbox" className="w-100 ml-2 flex py-4">
+            <Label htmlFor="vue-js-list-checkbox" className="w-full gap-1 flex items-center py-4">
+              <Checkbox id="vue-js-list-checkbox" />
               Vue JS
             </Label>
           </div>
         </li>
         <li className="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
           <div className="flex items-center pl-3">
-            <Checkbox id="react-list-checkbox" />
-            <Label htmlFor="react-list-checkbox" className="w-100 ml-2 flex py-4">
+            <Label htmlFor="react-list-checkbox" className="w-full gap-1 flex items-center py-4">
+              <Checkbox id="react-list-checkbox" />
               React
             </Label>
           </div>
         </li>
         <li className="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
           <div className="flex items-center pl-3">
-            <Checkbox id="angular-list-checkbox" />
-            <Label htmlFor="angular-list-checkbox" className="w-100 ml-2 flex py-4">
+            <Label htmlFor="angular-list-checkbox" className="w-full gap-1 flex items-center py-4">
+              <Checkbox id="angular-list-checkbox" />
               Angular
             </Label>
           </div>
         </li>
         <li className="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
-          <div className="flex items-center pl-3">
-            <Checkbox id="laravel-list-checkbox" />
-            <Label htmlFor="laravel-list-checkbox" className="w-100 ml-2 flex py-4">
+          <div className="flex items-center pl-3 w-full">
+            <Label htmlFor="laravel-list-checkbox" className="w-full gap-1 flex items-center py-4">
+              <Checkbox id="laravel-list-checkbox" />
               Laravel
             </Label>
           </div>
@@ -59,32 +59,32 @@ function Component() {
       <ul className="w-48 rounded-lg border border-gray-200 bg-white text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white">
         <li className="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
           <div className="flex items-center pl-3">
-            <Checkbox id="vue-js-list-checkbox" />
-            <Label htmlFor="vue-js-list-checkbox" className="w-100 ml-2 flex py-4">
+            <Label htmlFor="vue-js-list-checkbox" className="w-full gap-1 flex items-center py-4">
+              <Checkbox id="vue-js-list-checkbox" />
               Vue JS
             </Label>
           </div>
         </li>
         <li className="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
           <div className="flex items-center pl-3">
-            <Checkbox id="react-list-checkbox" />
-            <Label htmlFor="react-list-checkbox" className="w-100 ml-2 flex py-4">
+            <Label htmlFor="react-list-checkbox" className="w-full gap-1 flex items-center py-4">
+              <Checkbox id="react-list-checkbox" />
               React
             </Label>
           </div>
         </li>
         <li className="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
           <div className="flex items-center pl-3">
-            <Checkbox id="angular-list-checkbox" />
-            <Label htmlFor="angular-list-checkbox" className="w-100 ml-2 flex py-4">
+            <Label htmlFor="angular-list-checkbox" className="w-full gap-1 flex items-center py-4">
+              <Checkbox id="angular-list-checkbox" />
               Angular
             </Label>
           </div>
         </li>
         <li className="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
-          <div className="flex items-center pl-3">
-            <Checkbox id="laravel-list-checkbox" />
-            <Label htmlFor="laravel-list-checkbox" className="w-100 ml-2 flex py-4">
+          <div className="flex items-center pl-3 w-full">
+            <Label htmlFor="laravel-list-checkbox" className="w-full gap-1 flex items-center py-4">
+              <Checkbox id="laravel-list-checkbox" />
               Laravel
             </Label>
           </div>
@@ -102,32 +102,32 @@ function Component() {
       <ul className="w-48 rounded-lg border border-gray-200 bg-white text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white">
         <li className="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
           <div className="flex items-center pl-3">
-            <Checkbox id="vue-js-list-checkbox" />
-            <Label htmlFor="vue-js-list-checkbox" className="w-100 ml-2 flex py-4">
+            <Label htmlFor="vue-js-list-checkbox" className="w-full gap-1 flex items-center py-4">
+              <Checkbox id="vue-js-list-checkbox" />
               Vue JS
             </Label>
           </div>
         </li>
         <li className="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
           <div className="flex items-center pl-3">
-            <Checkbox id="react-list-checkbox" />
-            <Label htmlFor="react-list-checkbox" className="w-100 ml-2 flex py-4">
+            <Label htmlFor="react-list-checkbox" className="w-full gap-1 flex items-center py-4">
+              <Checkbox id="react-list-checkbox" />
               React
             </Label>
           </div>
         </li>
         <li className="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
           <div className="flex items-center pl-3">
-            <Checkbox id="angular-list-checkbox" />
-            <Label htmlFor="angular-list-checkbox" className="w-100 ml-2 flex py-4">
+            <Label htmlFor="angular-list-checkbox" className="w-full gap-1 flex items-center py-4">
+              <Checkbox id="angular-list-checkbox" />
               Angular
             </Label>
           </div>
         </li>
         <li className="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
           <div className="flex items-center pl-3 w-full">
-            <Checkbox id="laravel-list-checkbox" />
-            <Label htmlFor="laravel-list-checkbox" className="w-100 ml-2 flex py-4">
+            <Label htmlFor="laravel-list-checkbox" className="w-full gap-1 flex items-center py-4">
+              <Checkbox id="laravel-list-checkbox" />
               Laravel
             </Label>
           </div>
