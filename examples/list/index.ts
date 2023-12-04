1597894ba1562d3cf285a092f2bbe4a3fcc0d00f
Sebastian Sutu <sebastian.sutu@stainless-code.com>
2023-12-04 09:25:33 +0200

    fix prettier tailwind plugin + format code

--- examples/list/index.ts
+++ examples/list/index.ts
@@ -1,4 +1,4 @@
-export { root } from './list.root';
-export { unstyled } from './list.unstyled';
 export { nested } from './list.nested';
 export { ordered } from './list.ordered';
+export { root } from './list.root';
+export { unstyled } from './list.unstyled';
