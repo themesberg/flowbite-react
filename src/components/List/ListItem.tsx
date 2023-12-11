3c01ad5c7259644039c5ff35cde0243b80932d65
fatemeh paghar <f.paghar@gmail.com>
2023-12-12 00:23:37 +0330

    refactor(listitem.tsx): uuse ComponentProps<'li'> to allow all vanilla li HTML element props
    Use ComponentProps<'li'> to allow all vanilla li HTML element props

--- src/components/List/ListItem.tsx
+++ src/components/List/ListItem.tsx
@@ -1,4 +1,4 @@
-import type { ComponentProps, FC, PropsWithChildren } from 'react';
+import type { ComponentProps, FC } from 'react';
 import { twMerge } from 'tailwind-merge';
 import { mergeDeep } from '../../helpers/merge-deep';
 import { getTheme } from '../../theme-store';
@@ -12,7 +12,7 @@ export interface FlowbiteListItemTheme {
   };
 }
 
-export interface ListItemProps extends PropsWithChildren {
+export interface ListItemProps extends ComponentProps<'li'> {
   theme?: DeepPartial<FlowbiteListItemTheme>;
   className?: string;
   icon?: FC<ComponentProps<'svg'>>;
