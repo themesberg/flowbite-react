1fbe6ca69346b090f9007e0ae904213977ad6d6e
fatemeh paghar <66066475+paghar@users.noreply.github.com>
2023-12-11 10:00:56 +0100

    feat(list component): develop new horizontal state of list component (#1178)
    * feat(list component): develop new horizontal state of list component
    
    * refactor(list.ts): change the order of classes in twmerge

--- examples/list/index.ts
+++ examples/list/index.ts
@@ -2,3 +2,4 @@ export { nested } from './list.nested';
 export { ordered } from './list.ordered';
 export { root } from './list.root';
 export { unstyled } from './list.unstyled';
+export { horizontal } from './list.horizontal';
