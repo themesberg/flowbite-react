4300ef029b410184a7661997a93e73cc6dbdd747
Sebastian Sutu <sebastian.sutu@stainless-code.com>
2023-12-13 18:09:36 +0200

    docs: create new Astro guide

--- data/docs-sidebar.ts
+++ data/docs-sidebar.ts
@@ -32,6 +32,7 @@ export const DOCS_SIDEBAR: DocsSidebarSection[] = [
       { title: 'Next.js', href: '/docs/guides/next-js', isNew: true },
       { title: 'Vite', href: '/docs/guides/vite', isNew: true },
       { title: 'Remix', href: '/docs/guides/remix', isNew: true },
+      { title: 'Astro', href: '/docs/guides/astro', isNew: true },
     ],
   },
   {
