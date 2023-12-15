c48f02680d4234c5b5372b095eaf1a8d486fe961
Diego Dias <ddiasfront@gmail.com>
2023-12-14 22:39:39 -0300

    chore(reducing complexity  for datevalue): assertive state/context management according view/date

--- packages/ui/src/components/Datepicker/Datepicker.tsx
+++ packages/ui/src/components/Datepicker/Datepicker.tsx
@@ -244,14 +244,11 @@ const DatepickerRender: ForwardRefRenderFunction<DatepickerRef, DatepickerProps>
   }, [inputRef, datepickerRef, setIsOpen]);
 
   useEffect(() => {
-    if (!dateValue) return;
-    if (dateValue.getTime() === selectedDate.getTime()) return;
-    const yearDifference = selectedDate.getFullYear() - dateValue.getFullYear();
-    const monthDifference = selectedDate.getMonth() - dateValue.getMonth();
-    const pageCounter = -(yearDifference * 12 + monthDifference);
-    setViewDate(getViewDatePage(view, viewDate, pageCounter));
-    dateValue && changeSelectedDate(dateValue, false);
-  }, [dateValue, view, viewDate, changeSelectedDate, selectedDate]);
+    if (dateValue) {
+      setSelectedDate(dateValue);
+      setViewDate(dateValue);
+    }
+  }, [dateValue, setSelectedDate]);
 
   return (
     <DatepickerContext.Provider
