044d3b869b42a7813c49e0400d7d3bc7c502d574
Diego Dias <ddiasfront@gmail.com>
2023-12-16 14:07:29 -0300

    feat: added necessary documentation

---
+++ examples/datepicker/datepicker.dateValue.tsx
@@ -0,0 +1,54 @@
+import { type CodeData } from '~/components/code-demo';
+import { Datepicker } from '~/src';
+
+const code = `
+'use client';
+
+import { Datepicker } from 'flowbite-react';
+
+function Component() {
+  return (
+    <Datepicker
+      dateValue={new Date(2022, 11, 25)} // 25th December 2022
+    />
+  );
+}
+`;
+
+const codeRSC = `
+import { Datepicker } from 'flowbite-react';
+
+function Component() {
+  return (
+    <Datepicker
+      weekStart={new Date(2022, 11, 25);} // 25th December 2022
+    />
+  );
+}
+`;
+
+function Component() {
+  return (
+    <Datepicker
+      dateValue={new Date(2022, 11, 25)} // 25th December 2022
+    />
+  );
+}
+
+export const dateValue: CodeData = {
+  type: 'single',
+  code: [
+    {
+      fileName: 'client',
+      language: 'tsx',
+      code,
+    },
+    {
+      fileName: 'server',
+      language: 'tsx',
+      code: codeRSC,
+    },
+  ],
+  githubSlug: 'datepicker/datepicker.dateValue.tsx',
+  component: <Component />,
+};
