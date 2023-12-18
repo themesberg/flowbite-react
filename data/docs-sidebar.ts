798d2e8b680405a8aa43ccab20e6ac3068c5690a
Sebastian Sutu <sebastian.sutu@stainless-code.com>
2023-12-18 12:54:39 +0200

    docs: create new Create React App guide

--- data/docs-sidebar.ts
+++ data/docs-sidebar.ts
@@ -36,6 +36,7 @@ export const DOCS_SIDEBAR: DocsSidebarSection[] = [
       { title: 'Gatsby', href: '/docs/guides/gatsby', isNew: true },
       { title: 'Laravel', href: '/docs/guides/laravel', isNew: true },
       { title: 'Parcel', href: '/docs/guides/parcel', isNew: true },
+      { title: 'Create React App', href: '/docs/guides/create-react-app', isNew: true },
     ],
   },
   {
