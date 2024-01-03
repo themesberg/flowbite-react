3b30fd693f3edaa25199397707883d6b3f5cf0d9
abdulbasithqb <abdul.basith@qburst.com>
2024-01-03 12:58:08 +0530

    enhancement:checkbox and radio components with 'disabled:cursor-not-allowed' style

--- src/components/Checkbox/theme.ts
+++ src/components/Checkbox/theme.ts
@@ -2,7 +2,7 @@ import type { FlowbiteCheckboxTheme } from './Checkbox';
 
 export const checkboxTheme: FlowbiteCheckboxTheme = {
   root: {
-    base: 'h-4 w-4 rounded focus:ring-2 border border-gray-300 dark:border-gray-600 dark:bg-gray-700 bg-gray-100',
+    base: 'h-4 w-4 rounded focus:ring-2 border border-gray-300 dark:border-gray-600 dark:bg-gray-700 bg-gray-100 disabled:cursor-not-allowed',
     color: {
       default: 'focus:ring-cyan-600 dark:ring-offset-gray-800 dark:focus:ring-cyan-600 text-cyan-600',
       dark: 'focus:ring-gray-800 dark:ring-offset-gray-800 dark:focus:ring-gray-800 text-gray-800',
