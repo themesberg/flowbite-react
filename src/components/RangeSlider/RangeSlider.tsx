9e9b01c538c64f8bc440b1ae1308e1692ed01cd1
Karina Grishaeva <revuwem@gmail.com>
2024-01-08 20:52:49 +0400

    fix(range-slider): remove base wrappers
    remove base wrappers according to vanilla implementation
    
    BREAKING CHANGE: className prop applies directly to input

--- src/components/RangeSlider/RangeSlider.tsx
+++ src/components/RangeSlider/RangeSlider.tsx
@@ -34,16 +34,13 @@ export const RangeSlider = forwardRef<HTMLInputElement, RangeSliderProps>(
 
     return (
       <>
-        <div data-testid="flowbite-range-slider" className={twMerge(theme.root.base, className)}>
-          <div className={theme.field.base}>
-            <input
-              ref={ref}
-              type="range"
-              className={twMerge(theme.field.input.base, theme.field.input.sizes[sizing])}
-              {...props}
-            />
-          </div>
-        </div>
+        <input
+          data-testid="flowbite-range-slider"
+          ref={ref}
+          type="range"
+          className={twMerge(theme.field.input.base, theme.field.input.sizes[sizing], className)}
+          {...props}
+        />
       </>
     );
   },
