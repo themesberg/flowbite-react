eec073ad17a47c59afd5f0b1bd4ebb79f8879e9c
SutuSebastian <sebastian.sutu@stainless-code.com>
2024-01-17 10:20:37 +0200

    fix(storybook): - replace `@vitejs/plugin-react-sw` -> `@vitejs/plugin-react` - fix build - add `.gitignore` - update stories path

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -11,7 +11,7 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, 'package.json')));
 }
 const config: StorybookConfig = {
-  stories: ['../../../**/*.stories.@(ts|tsx)'],
+  stories: ['../../../packages/ui/**/*.stories.@(ts|tsx)'],
   addons: [
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
