a6698d48474595f2ee05a3f817ecbdb322aa7807
Nigel Lima <nigelnaiguel.comp@gmail.com>
2024-01-24 04:17:01 -0300

    fix(button): Fixed Typescript error when using Next.js Link in the button `as` prop (#1244)
    * fix(button): correctly infer type from `as` prop
    
    `as` prop was throwing TS errors when using Next Link component. Now this correctly infers the type
    from any component passed to the `as` prop
    
    fix #1002 fix #1107
    
    * fix(dropdown-item): applied the generic types to DropdownItem and also wraps it with a forwardRef
    
    * docs(button): removed now-unnecessary @ts-expect-error comments
    
    * fix(button): replaced use of old genericForwardRef

--- src/components/Button/ButtonBase.tsx
+++ src/components/Button/ButtonBase.tsx
@@ -1,18 +1,19 @@
-import { createElement, type ComponentPropsWithoutRef, type ElementType, type ForwardedRef } from 'react';
-import genericForwardRef from '../../helpers/generic-forward-ref';
+import { createElement, type ComponentPropsWithoutRef, type ElementType, type ForwardedRef, forwardRef } from 'react';
 
 export type ButtonBaseProps<T extends ElementType = 'button'> = {
   as?: T;
   href?: string;
 } & ComponentPropsWithoutRef<T>;
 
-const ButtonBaseComponent = <T extends ElementType = 'button'>(
-  { children, as: Component, href, type = 'button', ...props }: ButtonBaseProps<T>,
-  ref: ForwardedRef<T>,
-) => {
-  const BaseComponent = Component || (href ? 'a' : 'button');
+export const ButtonBase = forwardRef(
+  <T extends ElementType = 'button'>(
+    { children, as: Component, href, type = 'button', ...props }: ButtonBaseProps<T>,
+    ref: ForwardedRef<T>,
+  ) => {
+    const BaseComponent = Component || (href ? 'a' : 'button');
 
-  return createElement(BaseComponent, { ref, href, type, ...props }, children);
-};
+    return createElement(BaseComponent, { ref, href, type, ...props }, children);
+  },
+);
 
-export const ButtonBase = genericForwardRef(ButtonBaseComponent);
+ButtonBase.displayName = 'ButtonBaseComponent';
