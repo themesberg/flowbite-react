f70a289729745211a3fe41d921550d85ce3d7740
SutuSebastian <sebastian.sutu@stainless-code.com>
2024-02-07 16:02:19 +0200

    fix(lib): merge-deep test

--- packages/ui/src/helpers/merge-deep.spec.ts
+++ packages/ui/src/helpers/merge-deep.spec.ts
@@ -8,7 +8,7 @@ describe('Helpers / Merge deep', () => {
 
     const result = mergeDeep(target, source);
 
-    expect(result).to.deep.equal({ foo: 'bar' });
+    expect(result).toEqual({ foo: 'bar' });
   });
 
   it('should merge keys that do not exist in source', () => {
@@ -17,7 +17,7 @@ describe('Helpers / Merge deep', () => {
 
     const result = mergeDeep(target, source);
 
-    expect(result).to.deep.equal({ foo: 'bar' });
+    expect(result).toEqual({ foo: 'bar' });
   });
 
   it('should override target key if source key is identical', () => {
@@ -26,7 +26,7 @@ describe('Helpers / Merge deep', () => {
 
     const result = mergeDeep(target, source);
 
-    expect(result).to.deep.equal({ foo: { bar: 'foobar' } });
+    expect(result).toEqual({ foo: { bar: 'foobar' } });
   });
 
   it('should merge keys and do not mutate target and source', () => {
@@ -42,14 +42,14 @@ describe('Helpers / Merge deep', () => {
 
     const result = mergeDeep(target, source);
 
-    expect(result).to.deep.equal({ foo: { a: 1, b: { c: 3, d: 3, f: { g: 3 } } }, baz: 5, bar: { a: 1 } });
+    expect(result).toEqual({ foo: { a: 1, b: { c: 3, d: 3, f: { g: 3 } } }, baz: 5, bar: { a: 1 } });
 
-    expect(target).to.deep.equal({
+    expect(target).toEqual({
       foo: { a: 1, b: { c: 2, f: { g: 3 } } },
       baz: 5,
     });
 
-    expect(source).to.deep.equal({
+    expect(source).toEqual({
       foo: { b: { c: 3, d: 3 } },
       bar: { a: 1 },
     });
@@ -58,14 +58,14 @@ describe('Helpers / Merge deep', () => {
     result.baz = 98;
     result.bar.a = 99;
 
-    expect(result).to.deep.equal({ foo: { a: 1, b: { c: 97, d: 3, f: { g: 3 } } }, baz: 98, bar: { a: 99 } });
+    expect(result).toEqual({ foo: { a: 1, b: { c: 97, d: 3, f: { g: 3 } } }, baz: 98, bar: { a: 99 } });
 
-    expect(target).to.deep.equal({
+    expect(target).toEqual({
       foo: { a: 1, b: { c: 2, f: { g: 3 } } },
       baz: 5,
     });
 
-    expect(source).to.deep.equal({
+    expect(source).toEqual({
       foo: { b: { c: 3, d: 3 } },
       bar: { a: 1 },
     });
