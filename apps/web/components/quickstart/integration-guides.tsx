cecf8d068d3472c5e33af5b126d84e8b7ff53651
SutuSebastian <sebastian.sutu@stainless-code.com>
2024-03-02 11:17:34 +0200

    apps(web): small style fixes

--- apps/web/components/quickstart/integration-guides.tsx
+++ apps/web/components/quickstart/integration-guides.tsx
@@ -8,14 +8,15 @@ interface Guide {
   logo: string;
   /** Invert colors on dark mode */
   invert?: boolean;
+  className?: string;
 }
 
 const GUIDES: Guide[] = [
-  { name: "Next.js", slug: "/docs/guides/next-js", logo: "next-js.svg" },
+  { name: "Next.js", slug: "/docs/guides/next-js", logo: "next-js.svg", className: "p-2" },
   { name: "Remix", slug: "/docs/guides/remix", logo: "remix.svg", invert: true },
   { name: "Astro", slug: "/docs/guides/astro", logo: "astro.svg", invert: true },
   { name: "Gatsby", slug: "/docs/guides/gatsby", logo: "gatsby.svg" },
-  { name: "RedwoodJS", slug: "/docs/guides/redwood-js", logo: "redwood-js.svg" },
+  { name: "RedwoodJS", slug: "/docs/guides/redwood-js", logo: "redwood-js.svg", className: "p-2" },
   { name: "Laravel", slug: "/docs/guides/laravel", logo: "laravel.svg" },
   { name: "Vite", slug: "/docs/guides/vite", logo: "vite.svg" },
   { name: "Parcel", slug: "/docs/guides/parcel", logo: "parcel.svg" },
@@ -24,7 +25,7 @@ const GUIDES: Guide[] = [
 
 export function IntegrationGuides() {
   return (
-    <div className="mt-8 grid grid-cols-2 gap-6 md:grid-cols-3">
+    <div className="my-8 grid grid-cols-2 gap-6 md:grid-cols-3">
       {GUIDES.map((card) => (
         <GuideCard key={card.name} {...card} />
       ))}
@@ -32,13 +33,19 @@ export function IntegrationGuides() {
   );
 }
 
-function GuideCard({ name, slug, logo, invert }: Guide) {
+function GuideCard({ name, slug, logo, invert, className }: Guide) {
   return (
     <Link
       href={slug}
-      className="flex h-40 flex-col items-center justify-center gap-3 rounded-lg border border-gray-200 text-center shadow-sm hover:shadow-lg dark:border-gray-600 dark:bg-gray-800 dark:hover:shadow-lg-light"
+      className="flex h-40 flex-col items-center justify-center gap-3 rounded-lg border border-gray-200 text-center shadow-sm hover:border-gray-300 hover:shadow-md dark:border-gray-600 dark:bg-gray-800 dark:hover:border-gray-500 dark:hover:shadow-lg-light"
     >
-      <Image src={`/logos/${logo}`} alt={name} width={64} height={64} className={twMerge(invert && "dark:invert")} />
+      <Image
+        src={`/logos/${logo}`}
+        alt={name}
+        width={64}
+        height={64}
+        className={twMerge(invert && "dark:invert", className)}
+      />
       <span className="font-medium text-gray-800 dark:text-gray-300">{name}</span>
     </Link>
   );
