57c1f80ad4e67462764af65bc06216ba4cc78ca9
Dhaval Vira <dhavalveera@gmail.com>
2024-03-16 11:23:10 +0530

    added more Test Cases for BottomNavigation

--- src/components/BottomNavigation/BottomNavigation.spec.tsx
+++ src/components/BottomNavigation/BottomNavigation.spec.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { BottomNavigation } from './BottomNavigation';
+import { BottomNavigationItem } from './BottomNavigationItem';
 
 describe.concurrent('BottomNavigation', async () => {
   it('BottomNavigation should have "data-testid=flowbite-bottom-navigation" in the document', async () => {
@@ -14,11 +15,7 @@ describe.concurrent('BottomNavigation', async () => {
   });
 
   it('BottomNavigation.Item should have "data-testid=flowbite-bottom-nav-item" in the document', async () => {
-    render(
-      <BottomNavigation>
-        <BottomNavigation.Item label="Hme" />
-      </BottomNavigation>,
-    );
+    render(<BottomNavigationItem label="Home" />);
 
     expect(bottomNavItemTestId()).toBeInTheDocument();
   });
@@ -26,7 +23,7 @@ describe.concurrent('BottomNavigation', async () => {
   it('BottomNavigation.Item should have "button" in the document', async () => {
     render(
       <BottomNavigation>
-        <BottomNavigation.Item label="Hme" />
+        <BottomNavigation.Item label="Home" />
       </BottomNavigation>,
     );
 
@@ -34,8 +31,15 @@ describe.concurrent('BottomNavigation', async () => {
       expect(button).toBeInTheDocument();
     });
   });
+
+  it('BottomNavigation.Item should have "Home" in the document', async () => {
+    render(<BottomNavigationItem label="Home" />);
+
+    expect(homeTextInButton()).toBeInTheDocument();
+  });
 });
 
 const bottomNavTestId = () => screen.getByTestId('flowbite-bottom-navigation');
 const bottomNavItemTestId = () => screen.getByTestId('flowbite-bottom-nav-item');
 const bottomNavItemButton = () => screen.getAllByRole('button');
+const homeTextInButton = () => screen.getByText('Home');
