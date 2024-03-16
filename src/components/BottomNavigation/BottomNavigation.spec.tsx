3cac538527cb042937d747d235f4b18f66af7a6c
Dhaval Vira <dhavalveera@gmail.com>
2024-03-16 13:38:46 +0530

    added more test-cases

--- src/components/BottomNavigation/BottomNavigation.spec.tsx
+++ src/components/BottomNavigation/BottomNavigation.spec.tsx
@@ -1,31 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { BottomNavigation } from './BottomNavigation';
-import { BottomNavigationItem } from './BottomNavigationItem';
 
 describe.concurrent('BottomNavigation', async () => {
   it('BottomNavigation should have "data-testid=flowbite-bottom-navigation" in the document', async () => {
-    render(
-      <BottomNavigation>
-        <BottomNavigation.Item label="Hme" />
-      </BottomNavigation>,
-    );
+    render(<TestBottomNavigation />);
 
-    expect(bottomNavTestId()).toBeInTheDocument();
+    bottomNavTestId().forEach((bottomNavTestId) => {
+      expect(bottomNavTestId).toBeInTheDocument();
+    });
   });
 
   it('BottomNavigation.Item should have "data-testid=flowbite-bottom-nav-item" in the document', async () => {
-    render(<BottomNavigationItem label="Home" />);
+    render(<TestBottomNavigation />);
 
-    expect(bottomNavItemTestId()).toBeInTheDocument();
+    bottomNavItemTestId().forEach((bottomNavItemTest) => {
+      expect(bottomNavItemTest).toBeInTheDocument();
+    });
   });
 
   it('BottomNavigation.Item should have "button" in the document', async () => {
-    render(
-      <BottomNavigation>
-        <BottomNavigation.Item label="Home" />
-      </BottomNavigation>,
-    );
+    render(<TestBottomNavigation />);
 
     bottomNavItemButton().forEach((button) => {
       expect(button).toBeInTheDocument();
@@ -33,13 +28,44 @@ describe.concurrent('BottomNavigation', async () => {
   });
 
   it('BottomNavigation.Item should have "Home" in the document', async () => {
-    render(<BottomNavigationItem label="Home" />);
+    render(<TestBottomNavigation />);
 
-    expect(homeTextInButton()).toBeInTheDocument();
+    homeTextInButton().forEach((homeText) => {
+      expect(homeText).toBeInTheDocument();
+    });
+  });
+
+  it('should have "attribute of type = button" in the document', async () => {
+    render(<TestBottomNavigation />);
+
+    bottomNavItemTestId().forEach((buttonType) => {
+      expect(buttonType).toHaveAttribute('type', 'button');
+    });
+  });
+
+  it('tooltip in document', async () => {
+    render(<TestBottomNavigation />);
+
+    expect(tooltips()).toBeInTheDocument();
   });
 });
 
-const bottomNavTestId = () => screen.getByTestId('flowbite-bottom-navigation');
-const bottomNavItemTestId = () => screen.getByTestId('flowbite-bottom-nav-item');
+const bottomNavTestId = () => screen.getAllByTestId('flowbite-bottom-navigation');
+const bottomNavItemTestId = () => screen.getAllByTestId('flowbite-bottom-nav-item');
 const bottomNavItemButton = () => screen.getAllByRole('button');
-const homeTextInButton = () => screen.getByText('Home');
+const homeTextInButton = () => screen.getAllByText('Home');
+const tooltips = () => screen.getByTestId('flowbite-tooltip');
+
+const TestBottomNavigation = (): JSX.Element => {
+  return (
+    <div>
+      <BottomNavigation>
+        <BottomNavigation.Item label="Home" />
+      </BottomNavigation>
+
+      <BottomNavigation>
+        <BottomNavigation.Item label="Home" showTooltip />
+      </BottomNavigation>
+    </div>
+  );
+};
