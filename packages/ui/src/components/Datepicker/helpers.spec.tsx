439263ba1ad3916a34e9d91f8c1f9b3345cf8f92
Dhaval Vira <dhavalveera@gmail.com>
2024-03-25 15:21:13 +0530

    DatePicker helpers test case issue fixed

--- packages/ui/src/components/Datepicker/helpers.spec.tsx
+++ packages/ui/src/components/Datepicker/helpers.spec.tsx
@@ -154,20 +154,20 @@ describe("addYears", () => {
 describe("getFormattedDate", () => {
   it("returns the formatted date string using the default options", () => {
     const date = new Date(2023, 0, 15); // January 15th, 2023
-    const formattedDate = getFormattedDate("en", date);
+    const formattedDate = getFormattedDate("en", date, 'dd-MMM-yyyy');
     expect(formattedDate).toBe("January 15, 2023");
   });
 
   it("returns the formatted date string using the specified options", () => {
     const date = new Date(2023, 0, 15); // January 15th, 2023
     const options: Intl.DateTimeFormatOptions = { month: "short", year: "numeric" };
-    const formattedDate = getFormattedDate("en", date, options);
+    const formattedDate = getFormattedDate("en", date, 'dd-MMM-yyyy', options);
     expect(formattedDate).toBe("Jan 2023");
   });
 
   it("returns the formatted date string using the specified language", () => {
     const date = new Date(2023, 0, 15); // January 15th, 2023
-    const formattedDate = getFormattedDate("pt-BR", date);
+    const formattedDate = getFormattedDate("pt-BR", date, 'dd-MMM-yyyy');
     expect(formattedDate).toBe("15 de janeiro de 2023");
   });
 });
