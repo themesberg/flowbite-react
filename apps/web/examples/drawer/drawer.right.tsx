7ddef820d98a3740fd8bfd9265ab4c357a160d25
Sutu Sebastian <41998826+SutuSebastian@users.noreply.github.com>
2024-04-17 13:27:13 +0300

    docs(drawer) - examples cleanup (#1374)
    chore: docs(drawer) - examples cleanup

--- apps/web/examples/drawer/drawer.right.tsx
+++ apps/web/examples/drawer/drawer.right.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { Button, Drawer, DrawerHeader, DrawerItems, theme } from "flowbite-react";
+import { Button, Drawer } from "flowbite-react";
 import { useState } from "react";
-import { twMerge } from "tailwind-merge";
 import type { CodeData } from "~/components/code-demo";
 
 const code = `
@@ -11,7 +10,7 @@ const code = `
 import { Button, Drawer } from "flowbite-react";
 import { useState } from "react";
 
-function Component() {
+export function Component() {
   const [isOpen, setIsOpen] = useState(true);
 
   const handleClose = () => setIsOpen(false);
@@ -67,11 +66,8 @@ function Component() {
   );
 }
 `;
-const codeRSC = `
-import { Button, Drawer, DrawerHeader, DrawerItems } from "flowbite-react";
-import { useState } from "react";
 
-function Component() {
+export function Component() {
   const [isOpen, setIsOpen] = useState(true);
 
   const handleClose = () => setIsOpen(false);
@@ -82,8 +78,8 @@ function Component() {
         <Button onClick={() => setIsOpen(true)}>Show right drawer</Button>
       </div>
       <Drawer open={isOpen} onClose={handleClose} position="right">
-        <DrawerHeader title="Drawer" />
-        <DrawerItems>
+        <Drawer.Header title="Drawer" />
+        <Drawer.Items>
           <p className="mb-6 text-sm text-gray-500 dark:text-gray-400">
             Supercharge your hiring by taking advantage of our&nbsp;
             <a href="#" className="text-cyan-600 underline hover:no-underline dark:text-cyan-500">
@@ -121,82 +117,9 @@ function Component() {
               </svg>
             </a>
           </div>
-        </DrawerItems>
+        </Drawer.Items>
       </Drawer>
     </>
-  );
-}
-`;
-
-function Component() {
-  const [isOpen, setIsOpen] = useState(true);
-
-  const handleClose = () => setIsOpen(false);
-
-  return (
-    <div className="relative z-50 -m-5 w-auto overflow-hidden">
-      <div className="flex min-h-[50vh] items-center justify-center">
-        <Button onClick={() => setIsOpen(true)}>Show right drawer</Button>
-      </div>
-      <Drawer
-        open={isOpen}
-        onClose={handleClose}
-        position="right"
-        theme={{
-          root: {
-            base: twMerge(theme.drawer.root.base, "absolute"),
-            backdrop: twMerge(theme.drawer.root.backdrop, "absolute"),
-            position: {
-              right: {
-                on: twMerge(theme.drawer.root.position.right.on, "h-full"),
-                off: twMerge(theme.drawer.root.position.right.off, "h-full"),
-              },
-            },
-          },
-        }}
-      >
-        <DrawerHeader title="Drawer" />
-        <DrawerItems>
-          <p className="mb-6 text-sm text-gray-500 dark:text-gray-400">
-            Supercharge your hiring by taking advantage of our&nbsp;
-            <a href="#" className="text-cyan-600 underline hover:no-underline dark:text-cyan-500">
-              limited-time sale
-            </a>
-            &nbsp;for Flowbite Docs + Job Board. Unlimited access to over 190K top-ranked candidates and the #1 design
-            job board.
-          </p>
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-            <a
-              href="#"
-              className="rounded-lg border border-gray-200 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-cyan-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
-            >
-              Learn more
-            </a>
-            <a
-              href="#"
-              className="inline-flex items-center rounded-lg bg-cyan-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-            >
-              Get access&nbsp;
-              <svg
-                className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
-          </div>
-        </DrawerItems>
-      </Drawer>
-    </div>
   );
 }
 
@@ -208,12 +131,8 @@ export const right: CodeData = {
       language: "tsx",
       code,
     },
-    {
-      fileName: "server",
-      language: "tsx",
-      code: codeRSC,
-    },
   ],
   githubSlug: "drawer/drawer.right.tsx",
   component: <Component />,
+  iframe: 600,
 };
