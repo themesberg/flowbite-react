3b86e882f110954dec9c5b2a93335e94604521ab
SutuSebastian <sebiitv@gmail.com>
2024-04-17 11:03:59 +0300

    chore: export all entities

--- apps/web/examples/megaMenu/index.ts
+++ apps/web/examples/megaMenu/index.ts
@@ -1,5 +1,5 @@
-export { root } from "./megaMenu.root";
-export { icons } from "./megaMenu.icons";
 export { fullWidth } from "./megaMenu.fullWidth";
 export { fullWidthCTA } from "./megaMenu.fullWidthCTA";
 export { fullWidthImage } from "./megaMenu.fullWidthImage";
+export { icons } from "./megaMenu.icons";
+export { root } from "./megaMenu.root";
