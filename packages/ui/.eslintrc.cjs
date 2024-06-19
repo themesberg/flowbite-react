c0a2da64aec0fd3e8d4701ed2bba9bc1db17232e
SutuSebastian <sebiitv@gmail.com>
2024-06-19 13:08:40 +0300

    up vitest lint

--- packages/ui/.eslintrc.cjs
+++ packages/ui/.eslintrc.cjs
@@ -10,7 +10,6 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
-    "plugin:vitest/recommended",
     "plugin:storybook/recommended",
     "plugin:tailwindcss/recommended",
     "prettier",
