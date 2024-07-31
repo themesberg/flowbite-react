01d3c190b86123e6c79193d2d26f1a2f5bee2607
Eshkhuvvatoff <163532601+EshquvvatovAsliddin@users.noreply.github.com>
2024-08-01 00:11:42 +0500

    Update tabs.root.tsx

--- apps/web/examples/tabs/tabs.root.tsx
+++ apps/web/examples/tabs/tabs.root.tsx
@@ -44,22 +44,52 @@ export function Component() {
 export function Component() {
   return (
     <Tabs aria-label="Default tabs" variant="default">
-      <Tabs.Item active title="Profile" icon={HiUserCircle}>
+      <Tabs.Item
+        active
+        title={
+          <span className="flex items-center">
+            <HiUserCircle className="mr-2" />
+            Profile
+          </span>
+        }
+        className="border-b-2 border-[#FD9339] pb-2" // Active holat uchun maxsus uslub
+      >
         This is <span className="font-medium text-gray-800 dark:text-white">Profile tab's associated content</span>.
         Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
         control the content visibility and styling.
       </Tabs.Item>
-      <Tabs.Item title="Dashboard" icon={MdDashboard}>
+      <Tabs.Item
+        title={
+          <span className="flex items-center">
+            <MdDashboard className="mr-2" />
+            Dashboard
+          </span>
+        }
+      >
         This is <span className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</span>.
         Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
         control the content visibility and styling.
       </Tabs.Item>
-      <Tabs.Item title="Settings" icon={HiAdjustments}>
+      <Tabs.Item
+        title={
+          <span className="flex items-center">
+            <HiAdjustments className="mr-2" />
+            Settings
+          </span>
+        }
+      >
         This is <span className="font-medium text-gray-800 dark:text-white">Settings tab's associated content</span>.
         Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
         control the content visibility and styling.
       </Tabs.Item>
-      <Tabs.Item title="Contacts" icon={HiClipboardList}>
+      <Tabs.Item
+        title={
+          <span className="flex items-center">
+            <HiClipboardList className="mr-2" />
+            Contacts
+          </span>
+        }
+      >
         This is <span className="font-medium text-gray-800 dark:text-white">Contacts tab's associated content</span>.
         Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
         control the content visibility and styling.
