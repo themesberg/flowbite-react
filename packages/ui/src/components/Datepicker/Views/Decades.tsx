ac1d098c24b7c41ee4e5a9baac3c5e974a534dbc
Dias, Diego <diasd@ae.com>
2024-08-21 07:04:37 -0300

    feat: ts compiling

--- packages/ui/src/components/Datepicker/Views/Decades.tsx
+++ packages/ui/src/components/Datepicker/Views/Decades.tsx
@@ -33,7 +33,7 @@ export const DatepickerViewsDecades: FC<DatepickerViewsDecadesProps> = ({ theme:
         const firstDate = new Date(year, 0, 1);
         const lastDate = addYears(firstDate, 9);
 
-        const isSelected = isDateInDecade(selectedDate, year);
+        const isSelected = selectedDate && isDateInDecade(selectedDate, year);
         const isDisabled = !isDateInRange(firstDate, minDate, maxDate) && !isDateInRange(lastDate, minDate, maxDate);
 
         return (
