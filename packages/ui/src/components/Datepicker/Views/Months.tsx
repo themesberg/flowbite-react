628dc14967a89fec3f7ce434aefd9486b179cb8f
Dias, Diego <diasd@ae.com>
2024-08-21 07:23:38 -0300

    feat: prettier

--- packages/ui/src/components/Datepicker/Views/Months.tsx
+++ packages/ui/src/components/Datepicker/Views/Months.tsx
@@ -2,7 +2,7 @@ import type { FC } from "react";
 import { twMerge } from "tailwind-merge";
 import { mergeDeep } from "../../../helpers/merge-deep";
 import { useDatePickerContext } from "../DatepickerContext";
-import { getFormattedDate, isDateInRange, Views, isDateEqual } from "../helpers";
+import { getFormattedDate, isDateEqual, isDateInRange, Views } from "../helpers";
 
 export interface FlowbiteDatepickerViewsMonthsTheme {
   items: {
