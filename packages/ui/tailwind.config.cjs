7781fbd33079a82a27808fe12b0721b0271288d9
SutuSebastian <sebiitv@gmail.com>
2024-10-15 11:30:21 +0300

    fix theme extend values

--- packages/ui/tailwind.config.cjs
+++ packages/ui/tailwind.config.cjs
@@ -1,8 +1,10 @@
+const { theme } = require("./src/tailwind/theme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: theme,
   },
   plugins: [],
 };
