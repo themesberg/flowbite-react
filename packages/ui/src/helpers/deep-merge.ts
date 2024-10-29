de5450b6da167b5887e3eba8da2e80f689a155e0
SutuSebastian <sebiitv@gmail.com>
2024-10-29 14:29:06 +0200

    resolve `deepMergeStrings` unique values

--- packages/ui/src/helpers/deep-merge.ts
+++ packages/ui/src/helpers/deep-merge.ts
@@ -4,10 +4,24 @@ export function deepMergeStrings(merge: (values: string[]) => string) {
   return deepmergeCustom({
     mergeOthers: (values, utils) => {
       if (values.some((value) => typeof value === "string")) {
-        return merge([...new Set(values.filter((value) => typeof value === "string"))]);
+        const strings = values.filter((value) => typeof value === "string");
+        const stringMap = new Set<string>();
+        const uniqueStrings: string[] = [];
+
+        for (const string of strings) {
+          const parts = [...new Set(string.split(/\s+/))];
+
+          uniqueStrings.push(parts.filter((part) => !stringMap.has(part)).join(" "));
+
+          for (const part of parts) {
+            stringMap.add(part);
+          }
+        }
+
+        return merge(uniqueStrings);
       }
 
-      return utils.defaultMergeFunctions.mergeOthers(values);
+      return utils.actions.defaultMerge;
     },
   });
 }
