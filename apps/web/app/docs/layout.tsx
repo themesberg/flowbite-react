f134288f8f6c076c1bd36b6cbbd8b4ffab437e50
SutuSebastian <sebiitv@gmail.com>
2024-12-29 18:22:47 +0200

    fix docs navbar

--- apps/web/app/docs/layout.tsx
+++ apps/web/app/docs/layout.tsx
@@ -46,7 +46,6 @@ export default function DocsLayout({ children }: PropsWithChildren) {
 function DocsNavbar({ isCollapsed, setCollapsed }: DocsLayoutState) {
   return (
     <Navbar
-      fluid
       theme={{
         root: {
           base: "sticky top-0 z-[60] mx-auto flex w-full items-center justify-between border-b border-gray-200 bg-white text-gray-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400",
@@ -55,6 +54,8 @@ function DocsNavbar({ isCollapsed, setCollapsed }: DocsLayoutState) {
           },
         },
       }}
+      applyTheme="replace"
+      fluid
     >
       <div className="flex items-center">
         {isCollapsed ? (
