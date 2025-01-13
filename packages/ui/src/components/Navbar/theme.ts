afe3016230a49fb0fa3f3f7ee35b83f10025fb48
SutuSebastian <sebiitv@gmail.com>
2025-01-13 16:02:45 +0200

    refactor(Navbar): update link active styles to use primary instead of cyan

--- packages/ui/src/components/Navbar/theme.ts
+++ packages/ui/src/components/Navbar/theme.ts
@@ -34,8 +34,8 @@ export const navbarTheme: NavbarTheme = createTheme({
   link: {
     base: "block py-2 pl-3 pr-4 md:p-0",
     active: {
-      on: "bg-cyan-700 text-white md:bg-transparent md:text-cyan-700 dark:text-white",
-      off: "border-b border-gray-100 text-gray-700 hover:bg-gray-50 md:border-0 md:hover:bg-transparent md:hover:text-cyan-700 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-white",
+      on: "bg-primary-700 text-white md:bg-transparent md:text-primary-700 dark:text-white",
+      off: "border-b border-gray-100 text-gray-700 hover:bg-gray-50 md:border-0 md:hover:bg-transparent md:hover:text-primary-700 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-white",
     },
     disabled: {
       on: "text-gray-400 hover:cursor-not-allowed dark:text-gray-600",
