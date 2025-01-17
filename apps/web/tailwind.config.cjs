2ccea8df4dab1be69822e360abb41ead2d5567b5
SutuSebastian <sebiitv@gmail.com>
2025-01-17 16:17:43 +0200

    refactor(tailwind.config): remove unused fade-in keyframes and animation

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -28,15 +28,6 @@ module.exports = {
       maxWidth: {
         "8xl": "90rem",
       },
-      keyframes: {
-        fadeIn: {
-          "0%": { opacity: "0" },
-          "100%": { opacity: "1" },
-        },
-      },
-      animation: {
-        "fade-in": "fadeIn 200ms ease-in-out",
-      },
       boxShadow: {
         "lg-light": "0 10px 15px -3px rgba(255, 255, 255, 0.1), 0 4px 6px -2px rgba(255, 255, 255, 0.05)",
       },
