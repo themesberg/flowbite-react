edd45148e8364ba00b0e1888b75753a944dd10c5
SutuSebastian <sebiitv@gmail.com>
2025-01-18 15:57:42 +0200

    refactor: set default generic type for createTheme function

--- packages/ui/src/helpers/create-theme.ts
+++ packages/ui/src/helpers/create-theme.ts
@@ -1,6 +1,8 @@
+import type { CustomFlowbiteTheme } from "../components/Flowbite/FlowbiteTheme";
+
 /**
  * Used to apply `tailwindcss` intellisense.
  */
-export function createTheme<T>(input: T) {
+export function createTheme<T = CustomFlowbiteTheme>(input: T) {
   return input;
 }
