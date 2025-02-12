b4c17f5388dd6bc6fc4f99159976edefcaf1b9ad
SutuSebastian <sebiitv@gmail.com>
2025-02-12 13:36:43 +0200

    chore(ui): conditional `tailwind-merge` for tailwind v3 and v4

--- packages/ui/src/helpers/tailwind-merge.ts
+++ packages/ui/src/helpers/tailwind-merge.ts
@@ -1,19 +1,22 @@
 import { extendTailwindMerge, type ClassNameValue } from "tailwind-merge";
+import { extendTailwindMerge as extendTailwindMerge_v2 } from "tailwind-merge-v2";
 import { getPrefix } from "../store";
+import { getTailwindVersion } from "./get-tailwind-version";
 
 const cache = new Map<string | undefined, ReturnType<typeof extendTailwindMerge>>();
 
 export function twMerge(...classLists: ClassNameValue[]): string {
   const prefix = getPrefix();
+  const version = getTailwindVersion();
 
-  const cacheKey = prefix;
+  const cacheKey = `${prefix}.${version}`;
   const cacheValue = cache.get(cacheKey);
 
   if (cacheValue) {
     return cacheValue(...classLists);
   }
 
-  const twMergeFn = extendTailwindMerge({
+  const twMergeFn = (version === 3 ? extendTailwindMerge_v2 : extendTailwindMerge)({
     extend: {
       classGroups: {
         "bg-image": ["bg-arrow-down-icon", "bg-check-icon", "bg-dash-icon", "bg-dot-icon"],
