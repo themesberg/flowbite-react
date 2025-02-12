4ce178d2c1011c775695ee9259932d1ef602fc5c
SutuSebastian <sebiitv@gmail.com>
2025-02-12 11:24:42 +0200

    fix parcel plugin reporter

--- packages/ui/src/plugin/parcel.ts
+++ packages/ui/src/plugin/parcel.ts
@@ -2,11 +2,12 @@ import { Reporter } from "@parcel/plugin";
 import { build, dev } from "../cli";
 
 export default new Reporter({
-  async report({ event }) {
-    if (event.type === "buildStart") {
+  async report({ event, options }) {
+    if (event.type === "buildStart" && options.env.NODE_ENV === "production") {
       await build();
     }
     if (event.type === "watchStart") {
+      await build();
       await dev();
     }
   },
