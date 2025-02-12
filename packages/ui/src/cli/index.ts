ed2b196f04434fa184676bb012cfcac0b61a0605
SutuSebastian <sebiitv@gmail.com>
2025-02-12 13:09:41 +0200

    fix `setupPluginParcel` since plugin registering is very strict (naming convention)

--- packages/ui/src/cli/index.ts
+++ packages/ui/src/cli/index.ts
@@ -645,14 +645,61 @@ export async function setupPluginNextjs(configPath: string) {
 export async function setupPluginParcel(configPath: string) {
   try {
     const content = await fs.readFile(configPath, "utf-8");
-    const parsedContent: { reporters?: string[] } = JSON.parse(content);
+    const parsedContent: { extends?: string[] } = JSON.parse(content);
+
+    const parcelConfigDir = path.join(outputDir, "parcel-config");
+    const parcelReporterFile = "parcel-reporter.cjs";
+
+    // setup `.flowbite-react/parcel-config` directory
+    try {
+      await fs.access(parcelConfigDir);
+    } catch {
+      console.log(`Creating ${parcelConfigDir} directory...`);
+      await fs.mkdir(parcelConfigDir, { recursive: true });
+    }
+
+    // setup `.flowbite-react/parcel-config/index.json` file
+    const parcelConfigFilePath = path.join(parcelConfigDir, "index.json");
+    const parcelConfigFileContent = {
+      reporters: [`./${parcelReporterFile}`, "..."],
+    };
+
+    try {
+      const fileContent = await fs.readFile(parcelConfigFilePath, "utf-8");
+      const parsedContent: { reporters?: string[] } = JSON.parse(fileContent);
+
+      if (!parsedContent.reporters?.includes(parcelConfigFileContent.reporters[0])) {
+        console.log(`Updating ${parcelConfigFilePath} file...`);
+        await fs.writeFile(parcelConfigFilePath, JSON.stringify(parcelConfigFileContent, null, 2), "utf-8");
+      }
+    } catch {
+      console.log(`Creating ${parcelConfigFilePath} file...`);
+      await fs.writeFile(parcelConfigFilePath, JSON.stringify(parcelConfigFileContent, null, 2), "utf-8");
+    }
+
+    // setup `.flowbite-react/parcel-config/parcel-reporter.cjs` file
     const pluginImportPath = path.join(bundlerPluginPath, "parcel");
+    const parcelReporterFileContent = `module.exports = require("${pluginImportPath}");`;
+    const parcelReporterFilePath = path.join(parcelConfigDir, parcelReporterFile);
 
-    if (!parsedContent.reporters?.includes(pluginImportPath)) {
-      parsedContent.reporters ||= [];
-      parsedContent.reporters.push(pluginImportPath);
+    try {
+      const fileContent = await fs.readFile(parcelReporterFilePath, "utf-8");
 
-      console.log(`Updating ${configPath} with ${pluginImportPath} plugin...`);
+      if (!fileContent.includes(parcelReporterFileContent)) {
+        console.log(`Updating ${parcelReporterFilePath} file...`);
+        await fs.writeFile(parcelReporterFilePath, parcelReporterFileContent, "utf-8");
+      }
+    } catch {
+      console.log(`Creating ${parcelReporterFilePath} file...`);
+      await fs.writeFile(parcelReporterFilePath, parcelReporterFileContent, "utf-8");
+    }
+
+    // setup `.parcelrc` config file
+    if (!parsedContent.extends?.includes(parcelConfigFilePath)) {
+      parsedContent.extends ||= [];
+      parsedContent.extends.push(parcelConfigFilePath);
+
+      console.log(`Updating ${configPath} with ${parcelConfigFilePath} config...`);
       await fs.writeFile(configPath, JSON.stringify(parsedContent, null, 2), "utf-8");
     }
   } catch (error) {
