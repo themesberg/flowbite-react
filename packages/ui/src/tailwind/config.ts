cad4a2f2f5088255694c69c1b87b5603bdfa8763
SutuSebastian <sebiitv@gmail.com>
2025-02-13 17:47:45 +0200

    fix: prettier `importOrder` rules + format files

--- packages/ui/src/tailwind/config.ts
+++ packages/ui/src/tailwind/config.ts
@@ -1,6 +1,5 @@
-import { theme } from "./theme";
-
 import type { Config } from "tailwindcss";
+import { theme } from "./theme";
 
 export const config: Partial<Config> = {
   theme: {
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,5 +3,13 @@ module.exports = {
   plugins: ["prettier-plugin-packagejson", "@ianvs/prettier-plugin-sort-imports"],
   printWidth: 120,
   // sort-imports
-  importOrder: ["<BUILTIN_MODULES>", "<THIRD_PARTY_MODULES>", "^~/(.*)$", "^[.]", "", "^(?!.*[.]css$)[./].*$", ".css$"],
+  importOrder: [
+    "<BUILTIN_MODULES>",
+    "<THIRD_PARTY_MODULES>",
+    "^~/(?!.*\.(?:css|module\.css)$).*",
+    "^[.]",
+    "",
+    "^[.](?!.*\\.(?:css|module\\.css)$).*$",
+    ".*\\.css$",
+  ],
 };
