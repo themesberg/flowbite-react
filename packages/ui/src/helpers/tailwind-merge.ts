d46b0304e28777ff1dde8fe6f23837dfb857c293
SutuSebastian <sebiitv@gmail.com>
2025-02-14 14:35:38 +0200

    fix confusing import resolution due to monorepo

--- packages/ui/src/helpers/tailwind-merge.ts
+++ packages/ui/src/helpers/tailwind-merge.ts
@@ -1,9 +1,9 @@
-import { extendTailwindMerge, type ClassNameValue } from "tailwind-merge";
 import { extendTailwindMerge as extendTailwindMerge_v2 } from "tailwind-merge-v2";
+import { extendTailwindMerge as extendTailwindMerge_v3, type ClassNameValue } from "tailwind-merge-v3";
 import { getPrefix } from "../store";
 import { getTailwindVersion } from "./get-tailwind-version";
 
-const cache = new Map<string | undefined, ReturnType<typeof extendTailwindMerge>>();
+const cache = new Map<string | undefined, ReturnType<typeof extendTailwindMerge_v3>>();
 
 export function twMerge(...classLists: ClassNameValue[]): string {
   const prefix = getPrefix();
@@ -16,7 +16,7 @@ export function twMerge(...classLists: ClassNameValue[]): string {
     return cacheValue(...classLists);
   }
 
-  const twMergeFn = (version === 3 ? extendTailwindMerge_v2 : extendTailwindMerge)({
+  const twMergeFn = (version === 3 ? extendTailwindMerge_v2 : extendTailwindMerge_v3)({
     extend: {
       classGroups: {
         "bg-image": ["bg-arrow-down-icon", "bg-check-icon", "bg-dash-icon", "bg-dot-icon"],
