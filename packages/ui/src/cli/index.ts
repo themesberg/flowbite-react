ee3cc2fc8df085c7294ca5d3350a684c206d3d38
SutuSebastian <sebiitv@gmail.com>
2025-02-16 13:38:38 +0200

    fix(plugin ~ modernjs): patch `tsconfig.json` due to `moduleResolution: "node"` which does not work with our `package.json` entries config

--- packages/ui/src/cli/index.ts
+++ packages/ui/src/cli/index.ts
@@ -634,6 +634,34 @@ export async function setupPluginModernjs(configPath: string) {
     pluginInvocation: bundlerPluginInvocation,
     pluginName: bundlerPluginName,
   });
+
+  // TODO: remove when `node/node10` moduleResolution is fixed in the build process
+  // update `tsconfig.json` compilerOptions `module` and `moduleResolution`
+  const tsConfigFile = "tsconfig.json";
+
+  try {
+    const content = await fs.readFile(tsConfigFile, "utf-8");
+    const parsedContent: { compilerOptions?: Record<string, unknown> } = JSON.parse(content);
+
+    const defaultTsConfig = {
+      module: "ESNext",
+      moduleResolution: "bundler",
+    };
+
+    if (
+      parsedContent.compilerOptions?.module !== defaultTsConfig.module ||
+      parsedContent.compilerOptions?.moduleResolution !== defaultTsConfig.moduleResolution
+    ) {
+      parsedContent.compilerOptions ||= {};
+      parsedContent.compilerOptions.module = "ESNext";
+      parsedContent.compilerOptions.moduleResolution = "bundler";
+
+      console.log(`Updating ${tsConfigFile} file with flowbite-react configuration...`);
+      await fs.writeFile(tsConfigFile, JSON.stringify(parsedContent, null, 2), "utf-8");
+    }
+  } catch (error) {
+    console.error(`Failed to update ${tsConfigFile} file...`, error);
+  }
 }
 
 export async function setupPluginNextjs(configPath: string) {
