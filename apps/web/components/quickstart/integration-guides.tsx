effe13a48fd3c1aa72f44ac130e28bf4c8f94323
SutuSebastian <sebiitv@gmail.com>
2025-03-05 16:13:33 +0200

    feat(docs): expand integration guides

--- apps/web/components/quickstart/integration-guides.tsx
+++ apps/web/components/quickstart/integration-guides.tsx
@@ -12,15 +12,23 @@ interface Guide {
 }
 
 const GUIDES: Guide[] = [
-  { name: "Next.js", slug: "/docs/guides/next-js", logo: "next-js.svg", className: "p-2" },
-  { name: "Remix", slug: "/docs/guides/remix", logo: "remix.svg", invert: true },
-  { name: "Astro", slug: "/docs/guides/astro", logo: "astro.svg", invert: true },
-  { name: "Gatsby", slug: "/docs/guides/gatsby", logo: "gatsby.svg" },
   { name: "AdonisJS", slug: "/docs/guides/adonis-js", logo: "adonis-js.svg", className: "p-2", invert: true },
-  { name: "RedwoodJS", slug: "/docs/guides/redwood-js", logo: "redwood-js.svg", className: "p-2" },
+  { name: "Astro", slug: "/docs/guides/astro", logo: "astro.svg", invert: true },
+  { name: "Farm", slug: "/docs/guides/farm", logo: "farm.svg" },
+  { name: "Gatsby", slug: "/docs/guides/gatsby", logo: "gatsby.svg" },
   { name: "Laravel", slug: "/docs/guides/laravel", logo: "laravel.svg" },
-  { name: "Vite", slug: "/docs/guides/vite", logo: "vite.svg" },
+  { name: "Modern.js", slug: "/docs/guides/modern-js", logo: "modern-js.svg" },
+  { name: "Next.js", slug: "/docs/guides/next-js", logo: "next-js.svg", className: "p-2" },
   { name: "Parcel", slug: "/docs/guides/parcel", logo: "parcel.svg" },
+  { name: "React Router", slug: "/docs/guides/react-router", logo: "react-router.svg" },
+  { name: "RedwoodJS", slug: "/docs/guides/redwood-js", logo: "redwood-js.svg", className: "p-2" },
+  { name: "Remix", slug: "/docs/guides/remix", logo: "remix.svg", invert: true },
+  { name: "RSBuild", slug: "/docs/guides/rsbuild", logo: "rsbuild.svg" },
+  { name: "RSPack", slug: "/docs/guides/rspack", logo: "rspack.svg" },
+  { name: "TanStack Router", slug: "/docs/guides/tanstack-router", logo: "tanstack.svg" },
+  { name: "TanStack Start", slug: "/docs/guides/tanstack-start", logo: "tanstack.svg" },
+  { name: "Vite", slug: "/docs/guides/vite", logo: "vite.svg" },
+  { name: "Webpack", slug: "/docs/guides/webpack", logo: "webpack.svg" },
 ];
 
 export function IntegrationGuides() {
