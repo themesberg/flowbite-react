1eea6d293126d0a356b61c081bcb7fdcfeea159e
Marvin Fette <fettemarvin@gmail.com>
2025-07-11 12:56:37 +0200

    feat: Update the Datepicker documentation

---
+++ apps/web/examples/datepicker/datepicker.filter.tsx
@@ -0,0 +1,41 @@
+import { Datepicker, Views } from "flowbite-react";
+import type { CodeData } from "~/components/code-demo";
+
+const code = `
+import { Datepicker } from "flowbite-react";
+
+const filterFn = (date: Date, view: Views) => {
+  if (view === Views.Days) {
+    const day = date.getDay();
+    return day >= 1 && day <= 5;
+  }
+  return true;
+};
+
+export function Component() {
+  return <Datepicker filterDate={filterFn} />;
+}
+`;
+
+const filterFn = (date: Date, view: Views) => {
+  if (view === Views.Days) {
+    const day = date.getDay();
+    return day >= 1 && day <= 5;
+  }
+  return true;
+};
+
+export function Component() {
+  return <Datepicker filterDate={filterFn} />;
+}
+
+export const filter: CodeData = {
+  type: "single",
+  code: {
+    fileName: "index",
+    language: "tsx",
+    code,
+  },
+  githubSlug: "datepicker/datepicker.filter.tsx",
+  component: <Component />,
+};
