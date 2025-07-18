5d7879298239f360ded4f9194f545a32ab50722a
Sutu Sebastian <sebiitv@gmail.com>
2025-07-18 13:40:29 +0300

    feat(install): enhance package installation logic to upgrade to latest version if below 0.11.x

--- packages/ui/src/cli/commands/install.ts
+++ packages/ui/src/cli/commands/install.ts
@@ -11,12 +11,6 @@ export async function installPackage() {
 
   try {
     const packageJson = await getPackageJson();
-
-    if (packageJson.dependencies?.[packageName] || packageJson.devDependencies?.[packageName]) {
-      // TODO: prompt to bump the version to latest
-      return;
-    }
-
     let pm = await detect();
 
     if (!pm) {
@@ -25,10 +19,26 @@ export async function installPackage() {
 
     pm ??= { agent: "npm", name: "npm" };
 
+    const currentVersion = packageJson.dependencies?.[packageName] || packageJson.devDependencies?.[packageName];
+    if (currentVersion) {
+      const currentMajorMinor = currentVersion.split(".").slice(0, 2).join(".");
+
+      // Upgrade to latest version if below 0.11.x since 0.11.x is the version with the new engine and CLI
+      if (currentMajorMinor.localeCompare("0.11", undefined, { numeric: true }) < 0) {
+        console.log(
+          "The current version of flowbite-react is below 0.11.x, which is the version with the new engine and CLI.",
+        );
+        const { command = "", args } = resolveCommand(pm.agent, "add", [`${packageName}@latest`]) ?? {};
+        console.log(`Updating ${packageName} to latest version using ${pm.name}...`);
+        await execCommand(command, args);
+      }
+      return;
+    }
+
     const { command = "", args } = resolveCommand(pm.agent, "add", [packageName]) ?? {};
 
     console.log(`Installing ${packageName} using ${pm.name}...`);
-    execCommand(command, args);
+    await execCommand(command, args);
   } catch (error) {
     console.error(`Failed to install ${packageName}:`, error);
   }
