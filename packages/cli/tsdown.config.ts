0f526aacd25cfa9029a9f3bc8e4160fe98afc9ec
Sutu Sebastian <sebiitv@gmail.com>
2026-02-09 12:58:47 +0200

    fix(cli): update bin path to use relative path and add fixedExtension option in tsdown config

--- packages/cli/tsdown.config.ts
+++ packages/cli/tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   clean: true,
   entry: ["src/index.ts"],
+  fixedExtension: false,
   format: ["esm"],
   minify: true,
   target: "esnext",
